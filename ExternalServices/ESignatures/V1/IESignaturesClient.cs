4bba41b684dae034c51c8cda91f937f90cefa022
tomas-kucera <tomas.kucera@mpss.cz>
2022-01-23 14:47:50 +0100

    eSignatures

---
+++ ExternalServices/ESignatures/V1/IESignaturesClient.cs
@@ -0,0 +1,14 @@
+﻿
+using ExternalServices.ESignatures.V1.ESignaturesWrapper;
+
+namespace ExternalServices.ESignatures.V1
+{
+    public interface IESignaturesClient
+    {
+        /// <summary>
+        /// Popis . . . .
+        /// </summary>
+        Task<IServiceCallResult> PrepareDocument(PrepareDocumentRequest request, string org);
+
+    }
+}
