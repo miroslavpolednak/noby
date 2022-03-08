bb26faaf06d89d28908f8399b75b6f4dc5eecd25
tomas-kucera <tomas.kucera@mpss.cz>
2022-03-08 15:19:49 +0100

    Codebook [Channels]

---
+++ DomainServices/CodebookService/Endpoints/Channels/ChannelsHandler.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using DomainServices.CodebookService.Contracts;
+using DomainServices.CodebookService.Contracts.Endpoints.Channels;
+
+namespace DomainServices.CodebookService.Endpoints.Channels
+{
+    public class ChannelsHandler
+        : IRequestHandler<ChannelsRequest, List<GenericCodebookItem>>
+    {
+        public async Task<List<GenericCodebookItem>> Handle(ChannelsRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (_cache.Exists(_cacheKey))
+                {
+                    _logger.LogDebug("Found Channels in cache");
+
+                    return await _cache.GetAllAsync<GenericCodebookItem>(_cacheKey);
+                }
+                else
+                {
+                    _logger.LogDebug("Reading Channels from database");
+
+                    await using (var connection = _connectionProvider.Create())
+                    {
+                        await connection.OpenAsync();
+                        var result = (await connection.QueryAsync<GenericCodebookItem>("SELECT KOD 'Id', TEXT 'Name', CASE WHEN SYSDATETIME() BETWEEN[PLATNOST_OD] AND ISNULL([PLATNOST_DO], '9999-12-31') THEN 1 ELSE 0 END 'IsValid' FROM [SBR].[CIS_ALT_KANALY] WHERE KOD > 0 ORDER BY KOD ASC")).ToList();
+                        
+                        await _cache.SetAllAsync(_cacheKey, result);
+
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+
+        private readonly CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> _connectionProvider;
+        private readonly ILogger<ChannelsHandler> _logger;
+        private readonly CIS.Infrastructure.Caching.IGlobalCache<ISharedInMemoryCache> _cache;
+
+        public ChannelsHandler(
+            CIS.Infrastructure.Caching.IGlobalCache<ISharedInMemoryCache> cache,
+            CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> connectionProvider, 
+            ILogger<ChannelsHandler> logger)
+        {
+            _cache = cache;
+            _logger = logger;
+            _connectionProvider = connectionProvider;
+        }
+
+        private const string _cacheKey = "Channels";
+    }
+}
