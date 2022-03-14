1b51bbbe60fa6517b5b1b65e264ccdd77aa225ac
tomas-kucera <tomas.kucera@mpss.cz>
2022-03-14 10:55:18 +0100

    Codebook (PostCodes)

---
+++ DomainServices/CodebookService/Contracts/Endpoints/PostCodes/ICodebookService.PostCodes.cs
@@ -0,0 +1,10 @@
+﻿using DomainServices.CodebookService.Contracts.Endpoints.PostCodes;
+using ProtoBuf.Grpc;
+
+namespace DomainServices.CodebookService.Contracts;
+
+public partial interface ICodebookService
+{
+    [OperationContract]
+    Task<List<PostCodeItem>> PostCodes(PostCodesRequest request, CallContext context = default);
+}
---
+++ DomainServices/CodebookService/Contracts/Endpoints/PostCodes/PostCodeItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+namespace DomainServices.CodebookService.Contracts.Endpoints.PostCodes;
+
+[DataContract]
+public class PostCodeItem
+{
+    [DataMember(Order = 1)]
+    public string PostCode { get; set; }
+
+    [DataMember(Order = 2)]
+    public string Name { get; set; }
+
+    [DataMember(Order = 3)]
+    public int Disctrict { get; set; }
+
+    [DataMember(Order = 4)]
+    public int Municipality { get; set; }
+}
