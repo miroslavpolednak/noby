a5e0bb6735e5e227d1745d8ef0384b33a1e7fb97
tomas-kucera <tomas.kucera@mpss.cz>
2022-03-29 14:52:39 +0200

    SendToCmp (IdentificationDocument, Address) + IgnoreNullValues

--- DomainServices/SalesArrangementService/Api/Extensions/JsonExtensions.cs
+++ DomainServices/SalesArrangementService/Api/Extensions/JsonExtensions.cs
@@ -6,14 +6,24 @@ internal static class JsonExtensions
     private static readonly string FormatDecimal = "###########0.00";
     private static readonly string FormatDate = "dd.MM.yyyy";
 
-    public static string ToJsonString(this CIS.Infrastructure.gRPC.CisTypes.GrpcDecimal value)
+
+    public static decimal? ToDecimal(this CIS.Infrastructure.gRPC.CisTypes.GrpcDecimal value)
     {
-        return ((decimal)value).ToString(FormatDecimal, CultureInfo.InvariantCulture);
+        return  value == null ? null : (decimal)value;
+    }
+
+    public static string? ToJsonString(this CIS.Infrastructure.gRPC.CisTypes.GrpcDecimal value)
+    {
+        return value.ToDecimal().ToJsonString();
     }
 
     public static string ToJsonString(this int value)
     {
         return value.ToString(CultureInfo.InvariantCulture);
+    }
+    public static string? ToJsonString(this int? value)
+    {
+        return value.HasValue ? value.Value.ToJsonString() : null;
     }
 
     public static string ToJsonString(this decimal value)
@@ -21,9 +31,9 @@ internal static class JsonExtensions
         return value.ToString(FormatDecimal, CultureInfo.InvariantCulture);
     }
 
-    public static string? ToJsonString(this int? value)
+    public static string? ToJsonString(this decimal? value)
     {
-        return value.HasValue ? value.Value.ToString(CultureInfo.InvariantCulture) : null;
+        return value.HasValue ? value.Value.ToJsonString() : null;
     }
 
     public static string ToJsonString(this DateTime value)
@@ -44,7 +54,6 @@ internal static class JsonExtensions
     {
         return value ? "1" : "0";
     }
-
 
     public static string ToCode(this CIS.Infrastructure.gRPC.CisTypes.Identity identity)
     {
