43effba080dfa5bb1c31b1731e679bae20d0e6b7
tomas-kucera <tomas.kucera@mpss.cz>
2022-04-10 16:44:34 +0200

    Codebook ClassficationOfEconomicActivities

---
+++ DomainServices/CodebookService/Contracts/Endpoints/ClassficationOfEconomicActivities/ClassficationOfEconomicActivitiesItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+namespace DomainServices.CodebookService.Contracts.Endpoints.ClassficationOfEconomicActivities;
+
+[DataContract]
+public class ClassficationOfEconomicActivitiesItem
+{
+    [DataMember(Order = 1)]
+    public int Id { get; set; }
+
+    [DataMember(Order = 2)]
+    public string Name { get; set; }
+
+    [DataMember(Order = 3)]
+    [JsonIgnore]
+    public bool IsValid { get; set; }
+}
---
+++ DomainServices/CodebookService/Endpoints/ClassficationOfEconomicActivities/ClassficationOfEconomicActivitiesHandler.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using DomainServices.CodebookService.Contracts.Endpoints.ClassficationOfEconomicActivities;
+
+namespace DomainServices.CodebookService.Endpoints.RealEstateTypes
+{
+    public class ClassficationOfEconomicActivitiesHandler
+        : IRequestHandler<ClassficationOfEconomicActivitiesRequest, List<ClassficationOfEconomicActivitiesItem>>
+    {
+        public async Task<List<ClassficationOfEconomicActivitiesItem>> Handle(ClassficationOfEconomicActivitiesRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (_cache.Exists(_cacheKey))
+                {
+                    _logger.LogDebug("Found RealEstateTypes in cache");
+
+                    return await _cache.GetAllAsync<ClassficationOfEconomicActivitiesItem>(_cacheKey);
+                }
+                else
+                {
+                    _logger.LogDebug("Reading RealEstateTypes from database");
+
+                    await using (var connection = _connectionProvider.Create())
+                    {
+                        await connection.OpenAsync();
+                        var result = (await connection.QueryAsync<ClassficationOfEconomicActivitiesItem>("SELECT KOD 'Id', TEXT 'Name', CASE WHEN SYSDATETIME() BETWEEN[PLATNOST_OD] AND ISNULL([PLATNOST_DO], '9999-12-31') THEN 1 ELSE 0 END 'IsValid' FROM [SBR].[CIS_OKEC] ORDER BY KOD ASC")).ToList();
+
+                        await _cache.SetAllAsync(_cacheKey, result);
+
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+
+        private readonly CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> _connectionProvider;
+        private readonly ILogger<RealEstateTypesHandler> _logger;
+        private readonly CIS.Infrastructure.Caching.IGlobalCache<ISharedInMemoryCache> _cache;
+
+        public ClassficationOfEconomicActivitiesHandler(
+            CIS.Infrastructure.Caching.IGlobalCache<ISharedInMemoryCache> cache,
+            CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> connectionProvider,
+            ILogger<RealEstateTypesHandler> logger)
+        {
+            _cache = cache;
+            _logger = logger;
+            _connectionProvider = connectionProvider;
+        }
+
+        private const string _cacheKey = "ClassficationOfEconomicActivities";
+    }
+}
