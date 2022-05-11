6cc8e0fc3f904b8d2fde9002206e0b9622a5b962
tomas-kucera <tomas.kucera@mpss.cz>
2022-05-11 23:43:19 +0200

    Codebook (PropertySettlements)

---
+++ DomainServices/CodebookService/Contracts/Endpoints/PropertySettlements/PropertySettlementItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DomainServices.CodebookService.Contracts.Endpoints.PropertySettlements
+{
+    [DataContract]
+    public class PropertySettlementItem
+    {
+        [DataMember(Order = 1)]
+        public int Id { get; set; }
+
+        [DataMember(Order = 2)]
+        [JsonIgnore]
+        public string Code { get; set; }
+
+        [DataMember(Order = 3)]
+        public string Name { get; set; }
+
+        [DataMember(Order = 4)]
+        [JsonIgnore]
+        public string NameEng { get; set; }
+
+        [DataMember(Order = 5)]
+        [JsonIgnore]
+        public bool IsValid { get; set; }
+    }
+}
---
+++ DomainServices/CodebookService/Endpoints/PropertySettlements/PropertySettlementsHandler.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using DomainServices.CodebookService.Contracts;
+using DomainServices.CodebookService.Contracts.Endpoints.PropertySettlements;
+
+namespace DomainServices.CodebookService.Endpoints.PropertySettlements;
+
+public class PropertySettlementsHandler
+    : IRequestHandler<PropertySettlementsRequest, List<PropertySettlementItem>>
+{
+    public async Task<List<PropertySettlementItem>> Handle(PropertySettlementsRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await FastMemoryCache.GetOrCreate<PropertySettlementItem>(nameof(PropertySettlementsHandler), async () =>
+                await _connectionProvider.ExecuteDapperRawSqlToList<PropertySettlementItem>(_sqlQuery, cancellationToken)
+            );
+        }
+        catch (Exception ex)
+        {
+            _logger.GeneralException(ex);
+            throw;
+        }
+    }
+
+    const string _sqlQuery = @"SELECT KOD 'Id', CODE 'Code', TEXT_CZE 'Name', TEXT_ENG 'NameEng', CASE WHEN SYSDATETIME() BETWEEN[PLATNOST_OD] AND ISNULL([PLATNOST_DO], '9999-12-31') THEN 1 ELSE 0 END 'IsValid' 
+                               FROM [SBR].[CIS_VYPORADANI_MAJETKU] ORDER BY KOD ASC";
+
+    private readonly CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> _connectionProvider;
+    private readonly ILogger<PropertySettlementsHandler> _logger;
+
+    public PropertySettlementsHandler(
+        CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> connectionProvider, 
+        ILogger<PropertySettlementsHandler> logger)
+    {
+        _logger = logger;
+        _connectionProvider = connectionProvider;
+    }
+}
