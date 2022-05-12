65d9c02219e515b3b522322961a7546889b3dc03
tomas-kucera <tomas.kucera@mpss.cz>
2022-05-12 12:58:24 +0200

    Codebook (ProofTypes)

---
+++ DomainServices/CodebookService/Contracts/Endpoints/ProofTypes/ICodebookService.ProofTypes.cs
@@ -0,0 +1,10 @@
+﻿using ProtoBuf.Grpc;
+
+namespace DomainServices.CodebookService.Contracts
+{
+    public partial interface ICodebookService
+    {
+        [OperationContract]
+        Task<List<Endpoints.ProofTypes.ProofTypeItem>> ProofTypes(Endpoints.ProofTypes.ProofTypesRequest request, CallContext context = default);
+    }
+}
