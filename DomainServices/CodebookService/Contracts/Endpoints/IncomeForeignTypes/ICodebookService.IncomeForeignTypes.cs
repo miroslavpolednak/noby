35e7e1d3994ae6b19e52b23f2360773e9149eb60
tomas-kucera <tomas.kucera@mpss.cz>
2022-05-12 10:55:15 +0200

    Codebook (IncomeForeignTypes)

---
+++ DomainServices/CodebookService/Contracts/Endpoints/IncomeForeignTypes/ICodebookService.IncomeForeignTypes.cs
@@ -0,0 +1,10 @@
+﻿using ProtoBuf.Grpc;
+
+namespace DomainServices.CodebookService.Contracts
+{
+    public partial interface ICodebookService
+    {
+        [OperationContract]
+        Task<List<Endpoints.IncomeForeignTypes.IncomeForeignTypeItem>> IncomeForeignTypes(Endpoints.IncomeForeignTypes.IncomeForeignTypesRequest request, CallContext context = default);
+    }
+}
