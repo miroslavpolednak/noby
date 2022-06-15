f3cf7f4c30068d7dd5386edb73db2b823d751a4e
tomas-kucera <tomas.kucera@mpss.cz>
2022-06-15 20:20:28 +0200

    CaseSvc (GetTaskList - demock)

--- DomainServices/CaseService/Api/Extensions/CaseExtensions.cs
+++ DomainServices/CaseService/Api/Extensions/CaseExtensions.cs
@@ -4,26 +4,18 @@ namespace DomainServices.CaseService.Api;
 
 internal static class CaseExtensions
 {
-    //private static readonly string[] EasTaskMandatoryFields = new string[] {"ukol_id", "ukol_sada", "ukol_typ", "ukol_nazov", "ukol_dat_start_proces", "ukol_stav_poz"};
-
     public static WorkflowTask ToWorkflowTask(this ExternalServices.EasSimulationHT.V6.EasSimulationHTWrapper.WFS_FindItem easTask)
     {
         var dict = easTask.task.ToDictionary(i => i.mtdt_def, i => i.mtdt_val);
-
-        //TODO: remove mocks
-
-        var mockTypeId = 1;
-        var mockCreatedOn = DateTime.Now;
-        var mockStateId = 0;
 
         var task = new WorkflowTask
         {
             TaskId = int.Parse(dict["ukol_id"]),
             TaskProcessId = int.Parse(dict["ukol_sada"]),
-            TypeId = dict.ContainsKey("ukol_typ") ? int.Parse(dict["ukol_typ"]) : mockTypeId,
+            TypeId = int.Parse(dict["ukol_typ"]),
             Name = dict["ukol_nazov"],
-            CreatedOn = dict.ContainsKey("ukol_dat_start_proces") ? DateTime.Parse(dict["ukol_dat_start_proces"]) : mockCreatedOn,
-            StateId = dict.ContainsKey("ukol_stav_poz") ? int.Parse(dict["ukol_stav_poz"]) : mockStateId,
+            CreatedOn = DateTime.Parse(dict["ukol_dat_start_proces"]),
+            StateId = int.Parse(dict["ukol_stav_poz"]),
         };
 
         return task;
@@ -38,5 +30,4 @@ internal static class CaseExtensions
 
         };
     }
-
 }
