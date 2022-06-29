41aa1621dc14e387c3a7e9f8fb88a0a11dc16e1f
filip-tuma <filip@filiptuma.com>
2022-06-29 14:13:02 +0200

    no message

--- CIS/Infrastructure.Telemetry/Middlewares/LoggerCisUserMiddleware.cs
+++ CIS/Infrastructure.Telemetry/Middlewares/LoggerCisUserMiddleware.cs
@@ -1,7 +1,6 @@
 ﻿using CIS.Core.Security;
 using Microsoft.AspNetCore.Http;
 using Serilog.Context;
-using System.Diagnostics;
 
 namespace CIS.Infrastructure.Telemetry.Middlewares;
 
@@ -10,8 +9,6 @@ namespace CIS.Infrastructure.Telemetry.Middlewares;
 /// </summary>
 internal sealed class LoggerCisUserMiddleware
 {
-    const string ContextUserBaggageKey = "MpPartyId";
-
     private readonly RequestDelegate _next;
 
     public LoggerCisUserMiddleware(RequestDelegate next)
@@ -21,27 +18,15 @@ internal sealed class LoggerCisUserMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        // pokud se posila v ramci headeru requestu
-        if (int.TryParse(Activity.Current?.Baggage.FirstOrDefault(b => b.Key == ContextUserBaggageKey).Value, out int id))
+        var userAccessor = context.RequestServices.GetService(typeof(ICurrentUserAccessor)) as ICurrentUserAccessor;
+        if (userAccessor is not null && userAccessor.IsAuthenticated)
         {
-            using (LogContext.PushProperty("CisUserId", id))
+            using (LogContext.PushProperty("CisUserId", userAccessor.User!.Id))
             {
                 await _next.Invoke(context);
             }
         }
-        // jinak pokud je v httpContextu
         else
-        {
-            var userAccessor = context.RequestServices.GetService(typeof(ICurrentUserAccessor)) as ICurrentUserAccessor;
-            if (userAccessor is not null && userAccessor.IsAuthenticated)
-            {
-                using (LogContext.PushProperty("CisUserId", userAccessor.User!.Id))
-                {
-                    await _next.Invoke(context);
-                }
-            }
-            else
-                await _next.Invoke(context);
-        }
+            await _next.Invoke(context);
     }
 }
