15ed284d1b0699792b292a0b40c686595398c253
filip-tuma <filip@filiptuma.com>
2022-07-07 17:13:36 +0200

    no message

--- DomainServices/RiskIntegrationService/Contracts/CreditWorthiness/CreditLiability.cs
+++ DomainServices/RiskIntegrationService/Contracts/CreditWorthiness/CreditLiability.cs
@@ -7,16 +7,16 @@ public class CreditLiability
     public int LiabilityType { get; set; }
 
     [DataMember(Order = 2)]
-    public double? Limit { get; set; }
+    public decimal? Limit { get; set; }
 
     [DataMember(Order = 3)]
-    public double? AmountConsolidated { get; set; }
+    public decimal? AmountConsolidated { get; set; }
 
     [DataMember(Order = 4)]
-    public double? Installment { get; set; }
+    public decimal? Installment { get; set; }
 
     [DataMember(Order = 5)]
-    public double? InstallmentConsolidated { get; set; }
+    public decimal? InstallmentConsolidated { get; set; }
 
     [DataMember(Order = 6)]
     public bool OutHomeCompanyFlag { get; set; }
