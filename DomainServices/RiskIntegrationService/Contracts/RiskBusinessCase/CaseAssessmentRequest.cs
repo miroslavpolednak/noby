b41819128f2e844ae55488e24cd54c3ff531191d
Martin Heimlich <martin.heimlich@mpss.cz>
2022-07-12 13:32:20 +0200

    no message

--- DomainServices/RiskIntegrationService/Contracts/RiskBusinessCase/CaseAssessmentRequest.cs
+++ DomainServices/RiskIntegrationService/Contracts/RiskBusinessCase/CaseAssessmentRequest.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DomainServices.RiskIntegrationService.Contracts.RiskBusinessCase;
 
-namespace DomainServices.RiskIntegrationService.Contracts.RiskBusinessCase;
-
+[ProtoContract]
 public class CaseAssessmentRequest
+    : IRequest<CaseAssessmentResponse>, CIS.Core.Validation.IValidatableRequest
 {
+    [ProtoMember(1)]
+    public SystemId? LoanApplicationIdMp { get; set; }
+
+    [ProtoMember(2)]
+    public string? LoanApplicationDataVersion { get; set; }
+
+    [ProtoMember(3)]
+    public string? ItChannel { get; set; }
+
+    [ProtoMember(4)]
+    public string? ItChannelPrevious { get; set; }
+
+    [ProtoMember(5)]
+    public string? AssessmentMode { get; set; }
+
+    [ProtoMember(6)]
+    public string? GrantingProcedureCode { get; set; }
+
+    [ProtoMember(7)]
+    public bool SelfApprovalRequired { get; set; }
+
+    [ProtoMember(8)]
+    public bool SystemApprovalRequired { get; set; }
+
+    [ProtoMember(9)]
+    public List<LoanApplicationException>? LoanApplicationException { get; set; }
+
+    [ProtoMember(10)]
+    public string? ExceptionHighestApprovalLevel { get; set; }
+
+    [ProtoMember(11)]
+    public List<string>? Expand { get; set; }
 }
---
+++ DomainServices/RiskIntegrationService/Contracts/RiskBusinessCase/LoanApplicationException.cs
@@ -0,0 +1,14 @@
+﻿namespace DomainServices.RiskIntegrationService.Contracts.RiskBusinessCase;
+
+
+[ProtoContract]
+#pragma warning disable CA1711 // Identifiers should not have incorrect suffix
+public sealed class LoanApplicationException
+#pragma warning restore CA1711 // Identifiers should not have incorrect suffix
+{
+    [ProtoMember(1)]
+    public string? Arm { get; set; }
+
+    [ProtoMember(2)]
+    public string? ReasonCode { get; set; }
+}
