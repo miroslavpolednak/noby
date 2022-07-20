d2af79ae8cdfdf3a6e14407fa41fc85d01ce72e1
filip-tuma <filip@filiptuma.com>
2022-07-20 20:12:30 +0200

    no message

---
+++ FOMS/FOMS.Api/Endpoints/Customer/Identify/IdentifyRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FOMS.Api.Endpoints.Customer.Identify;
+
+public class IdentifyRequest
+    : IRequest
+{
+    public CIS.Foms.Types.CustomerIdentity? CustomerIdentity { get; set; }
+
+    [JsonIgnore]
+    internal int CustomerOnSAId;
+
+    internal IdentifyRequest InfuseId(int customerOnSAId)
+    {
+        this.CustomerOnSAId = customerOnSAId;
+        return this;
+    }
+}
---
+++ FOMS/FOMS.Api/Endpoints/Customer/Identify/IdentifyRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace FOMS.Api.Endpoints.Customer.Identify;
+
+public class IdentifyRequestValidator
+    : AbstractValidator<IdentifyRequest>
+{
+    public IdentifyRequestValidator()
+    {
+        RuleFor(t => t.CustomerIdentity)
+            .Cascade(CascadeMode.Stop)
+            .NotNull()
+            .ChildRules(x =>
+            {
+                x.RuleFor(t => t.Id)
+                    .GreaterThan(0);
+                x.RuleFor(t => t.Scheme)
+                    .IsInEnum()
+                    .Must(t => (int)t > 0);
+            });
+    }
+}
