7e042bbd24ece88e78d7eb4e5c0a2073080e9551
filip-tuma <filip@filiptuma.com>
2022-07-22 12:36:27 +0200

    no message

--- DomainServices/CodebookService/Contracts/Endpoints/DeveloperSearch/DeveloperSearchItem.cs
+++ DomainServices/CodebookService/Contracts/Endpoints/DeveloperSearch/DeveloperSearchItem.cs
@@ -3,18 +3,33 @@
 [DataContract]
 public class DeveloperSearchItem
 {
+    /// <summary>
+    /// ID developera
+    /// </summary>
     [DataMember(Order = 1)]
     public int DeveloperId { get; set; }
 
+    /// <summary>
+    /// ID developerského projektu
+    /// </summary>
     [DataMember(Order = 2)]
     public int DeveloperProjectId { get; set; }
 
+    /// <summary>
+    /// Jméno developera
+    /// </summary>
     [DataMember(Order = 3)]
     public string DeveloperName { get; set; }
 
+    /// <summary>
+    /// Jméno developerského projektu
+    /// </summary>
     [DataMember(Order = 4)]
     public string DeveloperProjectName { get; set; }
 
+    /// <summary>
+    /// ICO/RČ developera
+    /// </summary>
     [DataMember(Order = 5)]
     public string DeveloperCIN { get; set; }
 }
