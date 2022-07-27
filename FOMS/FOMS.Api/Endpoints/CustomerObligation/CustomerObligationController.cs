bb01b0ecb6308c3dc2b07686da8203a0bf1a3661
filip-tuma <filip@filiptuma.com>
2022-07-27 21:12:53 +0200

    no message

--- FOMS/FOMS.Api/Endpoints/CustomerObligation/CustomerObligationController.cs
+++ FOMS/FOMS.Api/Endpoints/CustomerObligation/CustomerObligationController.cs
@@ -7,65 +7,62 @@ namespace FOMS.Api.Endpoints.CustomerObligation;
 public class CustomerObligationController : ControllerBase
 {
     /// <summary>
-    /// Smazani zavazku customera
+    /// Smazání závazku customera
     /// </summary>
     /// <remarks>
     /// <i>DS:</i> SalesArrangementService/DeleteObligation
     /// </remarks>
     /// <param name="customerOnSAId">ID customera</param>
-    /// <param name="obligationId">ID zavazku ke smazani</param>
+    /// <param name="obligationId">ID závazku ke smazani</param>
     [HttpDelete("{customerOnSAId:int}/obligation/{obligationId:int}")]
-    [SwaggerOperation(Tags = new[] { "UC: Zavazky" })]
+    [SwaggerOperation(Tags = new[] { "UC: Závazek" })]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task Delete([FromRoute] int customerOnSAId, [FromRoute] int obligationId, CancellationToken cancellationToken)
         => await _mediator.Send(new DeleteObligation.DeleteObligationRequest(customerOnSAId, obligationId), cancellationToken);
 
     /// <summary>
-    /// Detail zavazku customera
+    /// Detail závazku customera
     /// </summary>
     /// <remarks>
-    /// <strong>CustomerObligation.GetDetail</strong><br/>
     /// <i>DS:</i> SalesArrangementService/GetObligation
     /// </remarks>
     /// <param name="customerOnSAId">ID customera</param>
-    /// <param name="obligationId">ID zavazku</param>
+    /// <param name="obligationId">ID závazku</param>
     /// <returns>
     /// <see cref="Dto.ObligationFullDto"/>
     /// </returns>
     [HttpGet("{customerOnSAId:int}/obligation/{obligationId:int}")]
     [Produces("application/json")]
-    [SwaggerOperation(Tags = new[] { "UC: Zavazky" })]
+    [SwaggerOperation(Tags = new[] { "UC: Závazek" })]
     [ProducesResponseType(typeof(Dto.ObligationFullDto), StatusCodes.Status200OK)]
     public async Task<Dto.ObligationFullDto> GetDetail([FromRoute] int customerOnSAId, [FromRoute] int obligationId, CancellationToken cancellationToken)
         => await _mediator.Send(new GetObligation.GetObligationRequest(customerOnSAId, obligationId), cancellationToken);
 
     /// <summary>
-    /// Update detailu zavazku customera
+    /// Update detailu závazku customera
     /// </summary>
     /// <remarks>
-    /// <strong>CustomerObligation.Update</strong><br/>
     /// <i>DS:</i> SalesArrangementService/UpdateObligation
     /// </remarks>
     /// <param name="customerOnSAId">ID customera</param>
-    /// <param name="obligationId">ID zavazku</param>
+    /// <param name="obligationId">ID závazku</param>
     [HttpPut("{customerOnSAId:int}/obligation/{obligationId:int}")]
     [Consumes("application/json")]
-    [SwaggerOperation(Tags = new[] { "UC: Zavazky" })]
+    [SwaggerOperation(Tags = new[] { "UC: Závazek" })]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task Update([FromRoute] int customerOnSAId, [FromRoute] int obligationId, [FromBody] UpdateObligation.UpdateObligationRequest? request, CancellationToken cancellationToken)
         => await _mediator.Send(request?.InfuseId(customerOnSAId, obligationId) ?? throw new CisArgumentNullException(ErrorCodes.PayloadIsEmpty, "Payload is empty", nameof(request)), cancellationToken);
 
     /// <summary>
-    /// Vytvoreni zavazku customera
+    /// Vytvoření závazku customera
     /// </summary>
     /// <remarks>
-    /// <strong>CustomerObligation.Create</strong><br/>
     /// <i>DS:</i> SalesArrangementService/CreateObligation
     /// </remarks>
     /// <param name="customerOnSAId">ID customera</param>
     [HttpPost("{customerOnSAId:int}/obligation")]
     [Consumes("application/json")]
-    [SwaggerOperation(Tags = new[] { "UC: Zavazky" })]
+    [SwaggerOperation(Tags = new[] { "UC: Závazek" })]
     [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
     public async Task<int> Create([FromRoute] int customerOnSAId, [FromBody] CreateObligation.CreateObligationRequest? request, CancellationToken cancellationToken)
         => await _mediator.Send(request?.InfuseId(customerOnSAId) ?? throw new CisArgumentNullException(ErrorCodes.PayloadIsEmpty, "Payload is empty", nameof(request)), cancellationToken);
--- FOMS/FOMS.Api/Endpoints/CustomerObligation/_Shared/ObligationCreditorDto.cs
+++ FOMS/FOMS.Api/Endpoints/CustomerObligation/_Shared/ObligationCreditorDto.cs
@@ -2,9 +2,18 @@
 
 public class ObligationCreditorDto
 {
+    /// <summary>
+    /// Id věřitele z číselníku bank <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=455007953">BankCode (CIS_KODY_BANK)</a>
+    /// </summary>
     public int? CreditorId { get; set; }
 
+    /// <summary>
+    /// Jméno věřitele z číselníku bank <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=455007953">BankCode (CIS_KODY_BANK)</a> nebo jméno věřitele zadané uživatelem
+    /// </summary>
     public string? Name { get; set; }
 
+    /// <summary>
+    /// True pokud je veřitel externí (=JPÚ - jiný peněžní ústav)<br />False pokud KB/MP (dáno vždy produktem)<br />null pokud je nedefinovaný
+    /// </summary>
     public bool? IsExternal { get; set; }
 }
