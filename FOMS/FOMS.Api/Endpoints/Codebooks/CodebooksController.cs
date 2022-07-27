0a41770609bf0f0821268ec2fad0dd64b11ccc52
Frantisek Fojtik <frantisek.fojtik@mpss.cz>
2022-07-27 17:09:35 +0200

    links to CFL

--- FOMS/FOMS.Api/Endpoints/Codebooks/CodebooksController.cs
+++ FOMS/FOMS.Api/Endpoints/Codebooks/CodebooksController.cs
@@ -23,43 +23,43 @@ public class CodebooksController : ControllerBase
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=392877756">ContactTypes</a>
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=405522415">Countries</a>
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=424127746">Currencies</a>
-    /// - CustomerProfiles
+    /// - <a href="https://wiki.kb.cz/display/HT/CustomerProfile">CustomerProfiles</a> - zatím není na FE API implementováno
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=421386916">CustomerRoles</a>
-    /// - BankCodes
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=455007953">BankCodes</a> - zatím není na FE API implementováno
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=438046695">Developers</a>
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=438046776">DeveloperProjects</a>
-    /// - DrawingDurations
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=450580207">DrawingDurations</a> - zatím není na FE API implementováno
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=419562802">EducationLevels</a>
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=430216233">EmploymentTypes</a>
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=438049777">Fees</a>
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=413645701">FixedRatePeriods</a>
     /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=405524365">Genders</a>
-    /// - HouseholdTypes
-    /// - IdentificationDocumentTypes
-    /// - IncomeMainTypes
-    /// - IncomeForeignTypes
-    /// - IncomeOtherTypes
-    /// - JobTypes
-    /// - LegalCapacities
-    /// - LoanPurposes
-    /// - LoanKinds
+    /// - <a href="https://wiki.kb.cz/display/HT/HouseholdType+-+MOCK">HouseholdTypes</a> - zatím není na FE API implementováno
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=392871926">IdentificationDocumentTypes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=426158089">IncomeMainTypes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=430216188">IncomeForeignTypes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=440881532">IncomeOtherTypes</a> - zatím není na FE API implementováno
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=430216289">JobTypes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=424118208">LegalCapacities</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=413109663">LoanPurposes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=413108848">LoanKinds</a>
     /// - Mandants
-    /// - MaritalStatuses
-    /// - ObligationCorrectionTypes
-    /// - ObligationTypes
-    /// - PaymentDays
-    /// - PostCodes
-    /// - ProductTypes
-    /// - PropertySettlements
-    /// - RealEstateTypes
-    /// - RealEstatePurchaseTypes
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=417279748">MaritalStatuses</a>
+    /// -  <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=440890324">ObligationCorrectionTypes</a> - zatím není na FE API implementováno
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=426150084">ObligationTypes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=438046178">PaymentDays</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=421389753">PostCodes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=392883923">ProductTypes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=413648025">PropertySettlements</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=413632253">RealEstateTypes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=413632953">RealEstatePurchaseTypes</a>
     /// - SalesArrangementStates
-    /// - SalesArrangementTypes
-    /// - SignatureTypes
-    /// - WorkflowTaskCategories
-    /// - WorkflowTaskStates
-    /// - WorkflowTaskTypes
-    /// - WorkSectors
+    /// - <a href="https://wiki.kb.cz/display/HT/SalesArrangementType">SalesArrangementTypes</a>
+    /// - <a href="https://wiki.kb.cz/display/HT/SignatureType">SignatureTypes</a>
+    /// - <a href="https://wiki.kb.cz/display/HT/WorkflowTaskCategory">WorkflowTaskCategories</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=440871662">WorkflowTaskStates</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=440879561">WorkflowTaskTypes</a>
+    /// - <a href="https://wiki.kb.cz/pages/viewpage.action?pageId=430216261">WorkSectors</a>
     /// </remarks>
     /// <param name="codebookTypes">Kody pozadovanych ciselniku oddelene carkou. Nazvy NEjsou case-sensitive. Example: q=productTypes,actionCodesSavings</param>
     /// <returns>Kolekce vyzadanych ciselniku.</returns>
