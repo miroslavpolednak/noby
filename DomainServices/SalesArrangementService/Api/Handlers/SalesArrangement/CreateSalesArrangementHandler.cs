e6453bb09754b015f2af73e77e8bd1377b870a9d
filip-tuma <filip@filiptuma.com>
2022-07-28 14:29:41 +0200

    no message

--- DomainServices/SalesArrangementService/Api/Handlers/SalesArrangement/CreateSalesArrangementHandler.cs
+++ DomainServices/SalesArrangementService/Api/Handlers/SalesArrangement/CreateSalesArrangementHandler.cs
@@ -25,7 +25,7 @@ internal class CreateSalesArrangementHandler
             ContractNumber = request.Request.ContractNumber,
             EaCode = request.Request.EaCode,
             LoanToCost = request.Request.LoanToCost,
-            ChannelId = 1 //TODO jak ziskat ChannelId? Z instance uzivatele? Az bude pripravena xxvvss...
+            ChannelId = 4 //TODO jak ziskat ChannelId? Z instance uzivatele? Az bude pripravena xxvvss...
         };
 
         // get default SA state
