c642ee14f0822aa4d8cd81feb2f29782e29679f1
tomas-kucera <tomas.kucera@mpss.cz>
2022-08-02 12:23:51 +0200

    Codebook (Mandants)

--- DomainServices/CodebookService/Contracts/Endpoints/Mandants/MandantsItem.cs
+++ DomainServices/CodebookService/Contracts/Endpoints/Mandants/MandantsItem.cs
@@ -1,19 +1,18 @@
-﻿using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
-
+﻿
 namespace DomainServices.CodebookService.Contracts.Endpoints.Mandants
 {
     [DataContract]
     public class MandantsItem
     {
+
         [DataMember(Order = 1)]
-        public string Code { get; set; }
+        public int Id { get; set; }
 
         [DataMember(Order = 2)]
-        public string Name { get; set; }
+        public string Code { get; set; }
 
         [DataMember(Order = 3)]
-        [JsonIgnore]
-        public int StarbuildId { get; set; }
+        public string Name { get; set; }
+
     }
 }
