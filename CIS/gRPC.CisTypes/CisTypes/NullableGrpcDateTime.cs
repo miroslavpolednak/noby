a5189d672b37028c08c6d965c353ee127bad3dc1
filip-tuma <filip@filiptuma.com>
2022-08-16 21:10:08 +0200

    no message

---
+++ CIS/gRPC.CisTypes/CisTypes/NullableGrpcDateTime.cs
@@ -0,0 +1,24 @@
+﻿namespace CIS.Infrastructure.gRPC.CisTypes;
+
+public sealed partial class NullableGrpcDateTime
+{
+    public NullableGrpcDateTime(int year, int month, int day, int hours, int minutes, int seconds, int nanos)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+        Hours = hours;
+        Minutes = minutes;
+        Seconds = seconds;
+        Nanos = nanos;
+    }
+
+    public static implicit operator NullableGrpcDateTime?(DateTime? value)
+    {
+        if (value == null) return default(NullableGrpcDateTime);
+        return new NullableGrpcDateTime(value.Value.Year, value.Value.Month, value.Value.Day, value.Value.Hour, value.Value.Minute, value.Value.Second, value.Value.Millisecond);
+    }
+
+    public static implicit operator DateTime?(NullableGrpcDateTime? grpcDate)
+        => grpcDate == null ? default(DateTime?) : new DateTime(grpcDate.Year, grpcDate.Month, grpcDate.Day, grpcDate.Hours, grpcDate.Minutes, grpcDate.Seconds, grpcDate.Nanos);
+}
