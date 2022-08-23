b8b58272a83adb952885db0f76373ecf72946c60
tomas-kucera <tomas.kucera@mpss.cz>
2022-08-23 11:54:31 +0200

    Codebook (DrawingTypes)

---
+++ CIS/Foms.Types/Enums/DrawingTypes.cs
@@ -0,0 +1,24 @@
+﻿using CIS.Core.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+
+namespace CIS.Foms.Enums;
+
+[DataContract]
+public enum DrawingTypes : byte
+{
+    [Display(Name = "unknown")]
+    [EnumMember]
+    Unknown = 0,
+
+    [Display(Name = "Postupné")]
+    [EnumMember]
+    [CisStarbuildId(1)]
+    Gradual = 1,
+
+    [Display(Name = "Jednorázové")]
+    [EnumMember]
+    [CisStarbuildId(0)]
+    Disposable = 2,
+}
