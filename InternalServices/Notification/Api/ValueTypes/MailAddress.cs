da388e493b62f0f6ff6a0d4ffe2fc47afa9ea254
filip-tuma <filip@filiptuma.com>
2022-09-09 17:12:26 +0200

    no message

--- InternalServices/Notification/Api/ValueTypes/MailAddress.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace CIS.InternalServices.Notification
-{
-    public sealed class MailAddress : System.Net.Mail.MailAddress
-    {
-        public MailAddress(string address) : base(address) { }
-    }
-}
