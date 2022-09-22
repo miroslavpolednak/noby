1818c33b674acdc74636064f6289a78d7fa61f67
David Martinek <david.martinek@mpss.cz>
2022-09-22 23:21:07 +0200

    DocumentGeneratorService - infrastructure

---
+++ InternalServices/DocumentGeneratorService/Api/Services/DocumentGeneratorService.cs
@@ -0,0 +1,12 @@
+﻿using CIS.InternalServices.DocumentGeneratorService.Contracts;
+using Grpc.Core;
+
+namespace CIS.InternalServices.DocumentGeneratorService.Api.Services;
+
+internal class DocumentGeneratorService : Contracts.V1.DocumentGeneratorService.DocumentGeneratorServiceBase
+{
+    public override Task<Document> GenerateDocument(GenerateDocumentRequest request, ServerCallContext context)
+    {
+        return base.GenerateDocument(request, context);
+    }
+}
