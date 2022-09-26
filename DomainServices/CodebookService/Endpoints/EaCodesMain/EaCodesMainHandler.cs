7c7ebab9158b33e24bd85177e4a21901eac5416f
tomas-kucera <tomas.kucera@mpss.cz>
2022-09-26 12:06:55 +0200

    [HFICH-1773] Codebook EaCodesMain

--- DomainServices/CodebookService/Endpoints/EaCodesMain/EaCodesMainHandler.cs
+++ DomainServices/CodebookService/Endpoints/EaCodesMain/EaCodesMainHandler.cs
@@ -8,15 +8,15 @@ public class EaCodesMainHandler
 
     #region Construction
 
-    private readonly CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> _connectionProvider;
+    private readonly CIS.Core.Data.IConnectionProvider _connectionProviderCodebooks;
     private readonly ILogger<EaCodesMainHandler> _logger;
 
     public EaCodesMainHandler(
-        CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> connectionProvider,
+        CIS.Core.Data.IConnectionProvider connectionProviderCodebooks,
         ILogger<EaCodesMainHandler> logger)
     {
         _logger = logger;
-        _connectionProvider = connectionProvider;
+        _connectionProviderCodebooks = connectionProviderCodebooks;
     }
 
     #endregion
@@ -25,7 +25,7 @@ public class EaCodesMainHandler
     // dotaz na codebook do SB
     const string _sql = @"SELECT KOD 'Id', POPIS 'Name', popis_klient 'DescriptionForClient', KATEGORIE 'Category', DRUH_KB 'KindKb', viditelnost_ps_kb_prodejni_sit_kb 'IsVisibleForKb', viditelnost_pro_vlozeni_noby 'IsInsertingAllowedNoby'
                           , CASE WHEN SYSDATETIME() BETWEEN[PLATNOST_OD] AND ISNULL([PLATNOST_DO], '9999-12-31') THEN 1 ELSE 0 END 'IsValid' 
-                          FROM [SBR].[EA_CIS_EACODEMAIN] ORDER BY kod ASC";
+                          FROM [dbo].[EA_CIS_EACODEMAIN] ORDER BY kod ASC";
 
     public async Task<List<EaCodeMainItem>> Handle(EaCodesMainRequest request, CancellationToken cancellationToken)
     {
@@ -34,7 +34,7 @@ public class EaCodesMainHandler
             return await FastMemoryCache.GetOrCreate<EaCodeMainItem>(nameof(EaCodesMainHandler), async () =>
             {
                 // load codebook items
-                return await _connectionProvider.ExecuteDapperRawSqlToList<EaCodeMainItem>(_sql, cancellationToken);
+                return await _connectionProviderCodebooks.ExecuteDapperRawSqlToList<EaCodeMainItem>(_sql, cancellationToken);
             });
         }
         catch (Exception ex)
