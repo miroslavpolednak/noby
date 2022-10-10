f20bf6f85f8e4e4219b6990f77cdef5beb4a80d3
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2022-10-10 21:07:07 +0200

    wip: s3 integration skeleton

---
+++ Playgrounds/NotificationService/Console_ClientS3/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("run");
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false, true)
+    .Build();
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IConfiguration>(configuration)
+    .AddDefaultAWSOptions(configuration.GetAWSOptions())
+    .AddAWSService<IAmazonS3>()
+    .BuildServiceProvider();
+    
+var s3Client = serviceProvider.GetRequiredService<IAmazonS3>();
+var buckets = await s3Client.ListBucketsAsync();
+
+Console.WriteLine("end");
