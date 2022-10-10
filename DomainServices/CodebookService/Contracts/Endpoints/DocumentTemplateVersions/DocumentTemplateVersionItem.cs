d63bfc8415b001f219cdde350848c2431d4f22de
tomas-kucera <tomas.kucera@mpss.cz>
2022-10-10 13:31:40 +0200

    [HFICH-2618] Codebook DocumentTemplateVersions

---
+++ DomainServices/CodebookService/Contracts/Endpoints/DocumentTemplateVersions/DocumentTemplateVersionItem.cs
@@ -0,0 +1,14 @@
+﻿namespace DomainServices.CodebookService.Contracts.Endpoints.DocumentTemplateVersions;
+
+[DataContract]
+public class DocumentTemplateVersionItem
+{
+    [DataMember(Order = 1)]
+    public int Id { get; set; }
+
+    [DataMember(Order = 2)]
+    public int DocumentTemplateTypeId { get; set; }
+
+    [DataMember(Order = 3)]
+    public string DocumentVersion { get; set; }
+}
---
+++ DomainServices/CodebookService/Endpoints/DocumentTemplateVersions/DocumentTemplateVersionsHandler.cs
@@ -0,0 +1,22 @@
+﻿using DomainServices.CodebookService.Contracts.Endpoints.DocumentTemplateVersions;
+using DomainServices.CodebookService.Contracts.Endpoints.RepaymentScheduleTypes;
+
+namespace DomainServices.CodebookService.Endpoints.DocumentTemplateVersions;
+
+internal class DocumentTemplateVersionsHandler
+    : IRequestHandler<DocumentTemplateVersionsRequest, List<DocumentTemplateVersionItem>>
+{
+    public Task<List<DocumentTemplateVersionItem>> Handle(DocumentTemplateVersionsRequest request, CancellationToken cancellationToken)
+    {
+        // TODO: Redirect to real data source!
+        var ids = new List<int> { 1, 2, 3, 4, 5, 6 };
+        return Task.FromResult(
+            ids.Select(i => new DocumentTemplateVersionItem
+            {
+                Id = i,
+                DocumentTemplateTypeId = i,
+                DocumentVersion = "001A"
+            }).ToList()
+         );
+    }
+}
