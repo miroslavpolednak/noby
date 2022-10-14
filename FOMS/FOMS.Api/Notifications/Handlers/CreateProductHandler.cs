e43a3f993059bae3dbf4386750faf18c29180f67
filip-tuma <filip@filiptuma.com>
2022-10-14 10:41:21 +0200

    no message

--- FOMS/FOMS.Api/Notifications/Handlers/CreateProductHandler.cs
+++ FOMS/FOMS.Api/Notifications/Handlers/CreateProductHandler.cs
@@ -12,33 +12,37 @@ internal class CreateProductHandler
     public async Task Handle(MainCustomerUpdatedNotification notification, CancellationToken cancellationToken)
     {
         long? mpId = notification.CustomerIdentifiers?.FirstOrDefault(t => t.IdentityScheme == CIS.Infrastructure.gRPC.CisTypes.Identity.Types.IdentitySchemes.Mp)?.IdentityId;
-        if (!mpId.HasValue) return; // nema modre ID, nezajima me
+        if (!mpId.HasValue)
+        {
+            _logger.LogInformation($"CreateProductHandler for CaseId #{notification.CaseId} not proceeding / missing MP ID");
+            return; // nema modre ID, nezajima me
+        }
 
         try
         {
+            // proc to tady bylo?
             await _productService.GetMortgage(notification.CaseId, cancellationToken);
-
-            // detail SA
-            var saInstance = ServiceCallResult.ResolveAndThrowIfError<_SA.SalesArrangement>(await _salesArrangementService.GetSalesArrangement(notification.SalesArrangementId, cancellationToken));
-            if (!saInstance.OfferId.HasValue)
-                throw new CisValidationException($"SalesArrangement #{notification.SalesArrangementId} is not bound to Offer");
-
-            // detail offer
-            var offerInstance = ServiceCallResult.ResolveAndThrowIfError<DomainServices.OfferService.Contracts.GetMortgageOfferResponse>(await _offerService.GetMortgageOffer(saInstance.OfferId.Value, cancellationToken));
-
-            var request = new _Product.CreateMortgageRequest
-            {
-                CaseId = notification.CaseId,
-                Mortgage = offerInstance.ToDomainServiceRequest(mpId.Value)
-            };
-            var result = ServiceCallResult.ResolveAndThrowIfError<_Product.ProductIdReqRes>(await _productService.CreateMortgage(request, cancellationToken));
-
-            _logger.EntityCreated(nameof(_Product.Product), result.ProductId);
-        }
-        catch
-        {
             _logger.LogInformation($"Product already exist for CaseId #{notification.CaseId}");
+            return;
         }
+        catch { }
+
+        // detail SA
+        var saInstance = ServiceCallResult.ResolveAndThrowIfError<_SA.SalesArrangement>(await _salesArrangementService.GetSalesArrangement(notification.SalesArrangementId, cancellationToken));
+        if (!saInstance.OfferId.HasValue)
+            throw new CisValidationException($"SalesArrangement #{notification.SalesArrangementId} is not bound to Offer");
+
+        // detail offer
+        var offerInstance = ServiceCallResult.ResolveAndThrowIfError<DomainServices.OfferService.Contracts.GetMortgageOfferResponse>(await _offerService.GetMortgageOffer(saInstance.OfferId.Value, cancellationToken));
+
+        var request = new _Product.CreateMortgageRequest
+        {
+            CaseId = notification.CaseId,
+            Mortgage = offerInstance.ToDomainServiceRequest(mpId.Value)
+        };
+        var result = ServiceCallResult.ResolveAndThrowIfError<_Product.ProductIdReqRes>(await _productService.CreateMortgage(request, cancellationToken));
+
+        _logger.EntityCreated(nameof(_Product.Product), result.ProductId);
     }
 
     private readonly IOfferServiceAbstraction _offerService;
