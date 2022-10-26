40fee8aa2270aa20ad510f49cdd30e7eef285cc9
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2022-10-26 12:04:30 +0200

    wip: s3 client proxy

--- Playgrounds/NotificationService/Console_ClientS3/Program.cs
+++ Playgrounds/NotificationService/Console_ClientS3/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net;
 using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Microsoft.Extensions.Configuration;
@@ -14,13 +16,23 @@ var configuration = new ConfigurationBuilder()
 
 var awsOptions = configuration.GetAWSOptions("AWS");
 
+var config = new AmazonS3Config()
+{
+    ServiceURL = "https://ecs-s3.sos.kb.cz:9021",
+    ForcePathStyle = true,
+    ProxyHost = "wproxy.kb.cz",
+    ProxyPort = 8080,
+    ProxyCredentials = CredentialCache.DefaultCredentials
+};
+
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IConfiguration>(configuration)
     .AddDefaultAWSOptions(awsOptions)
     .AddAWSService<IAmazonS3>()
     .BuildServiceProvider();
-    
-var s3Client = serviceProvider.GetRequiredService<IAmazonS3>();
+
+var s3Client = new AmazonS3Client(new BasicAWSCredentials("AKIAC2FA54725D65F6FF", "J0EvmiuUN0iVQAtod/lEc9dGIsJNECBJvA5AeWwJ"), config);
+// var s3Client = serviceProvider.GetRequiredService<IAmazonS3>();
 var response = await s3Client.ListBucketsAsync();
 var bucket = response.Buckets.FirstOrDefault(b => b.BucketName == "b-s3-mcs");
 
