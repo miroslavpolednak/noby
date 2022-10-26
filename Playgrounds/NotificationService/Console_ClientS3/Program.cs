5c2e9975eb9c635f44c6125b275e46f85e50405f
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2022-10-26 11:58:17 +0200

    wip: s3 client

--- Playgrounds/NotificationService/Console_ClientS3/Program.cs
+++ Playgrounds/NotificationService/Console_ClientS3/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Amazon.Extensions.NETCore.Setup;
 using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,14 +12,41 @@ var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", false, true)
     .Build();
 
+var awsOptions = configuration.GetAWSOptions("AWS");
+
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IConfiguration>(configuration)
-    .AddDefaultAWSOptions(configuration.GetAWSOptions())
+    .AddDefaultAWSOptions(awsOptions)
     .AddAWSService<IAmazonS3>()
     .BuildServiceProvider();
     
 var s3Client = serviceProvider.GetRequiredService<IAmazonS3>();
 var response = await s3Client.ListBucketsAsync();
 var bucket = response.Buckets.FirstOrDefault(b => b.BucketName == "b-s3-mcs");
+
+// var listRequest = new ListObjectsRequest
+// {
+//     BucketName = "b-s3-mcs"
+// };
+//
+// var bucketObjects = await s3Client.ListObjectsAsync(listRequest);
+
+var key = Guid.NewGuid().ToString();
+var putRequest = new PutObjectRequest
+{
+    BucketName = "b-s3-mcs",
+    Key = key,
+    ContentBody = "Content"
+};
+
+var putResponse = await s3Client.PutObjectAsync(putRequest);
+
+var getRequest = new GetObjectRequest()
+{
+    BucketName = "b-s3-mcs",
+    Key = key
+};
+
+var getResponse = await s3Client.GetObjectAsync(getRequest);
 
 Console.WriteLine("end");
