938b87f5a8018f8ac0eebb73141e14a8b84880a2
tomas-kucera <tomas.kucera@mpss.cz>
2022-10-31 13:38:19 +0100

    [HFICH-2968] CodebookSvc (SigningMethodsForNaturalPerson)

---
+++ DomainServices/CodebookService/Contracts/Endpoints/SigningMethodsForNaturalPerson/ICodebookService.SigningMethodsForNaturalPerson.cs
@@ -0,0 +1,11 @@
+﻿using DomainServices.CodebookService.Contracts.Endpoints.SigningMethodsForNaturalPerson;
+using ProtoBuf.Grpc;
+
+namespace DomainServices.CodebookService.Contracts
+{
+    public partial interface ICodebookService
+    {
+        [OperationContract]
+        Task<List<SigningMethodsForNaturalPersonItem>> SigningMethodsForNaturalPerson(SigningMethodsForNaturalPersonRequest request, CallContext context = default);
+    }
+}
