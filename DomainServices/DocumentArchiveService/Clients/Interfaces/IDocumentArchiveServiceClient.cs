2e0ee462cb7b9f510812d05e47907fe19288fcf6
filip-tuma <filip@filiptuma.com>
2022-11-03 21:31:42 +0100

    DocumentARchiveService update

---
+++ DomainServices/DocumentArchiveService/Clients/Interfaces/IDocumentArchiveServiceClient.cs
@@ -0,0 +1,13 @@
+﻿namespace DomainServices.DocumentArchiveService.Clients;
+
+public interface IDocumentArchiveServiceClient
+{
+    /// <summary>
+    /// Metoda slouží k vygenerování ID dokumentu které se následně použije pro uložení dokumentu do eArchivu a pro registraci do ESCUDO.
+    /// </summary>
+    /// <returns><see cref="string"/> DocumentId</returns>
+    /// <exception cref="CIS.Core.Exceptions.CisArgumentException">Code: 14009; EnvironmentName hodnota není z enum (DEV, FAT, SIT, UAT, PREPROD, EDU, PROD)</exception>
+    /// <exception cref="CIS.Core.Exceptions.CisArgumentException">Code: 14010; EnvironmentIndex není jednociferné číslo</exception>
+    /// <exception cref="CIS.Core.Exceptions.CisServiceUnavailableException">Service unavailable</exception>
+    Task<IServiceCallResult> GenerateDocumentId(Contracts.EnvironmentNames environmentName, int? environmentIndex = 0, CancellationToken cancellationToken = default(CancellationToken));
+}
