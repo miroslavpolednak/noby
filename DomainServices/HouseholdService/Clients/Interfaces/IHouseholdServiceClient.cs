c551e9e4687825b4163488dc977b2221165ff4c1
filip-tuma <filip@filiptuma.com>
2022-11-16 20:09:52 +0100

    Household svc logging fix

--- DomainServices/HouseholdService/Clients/Interfaces/IHouseholdServiceClient.cs
+++ DomainServices/HouseholdService/Clients/Interfaces/IHouseholdServiceClient.cs
@@ -8,7 +8,7 @@ public interface IHouseholdServiceClient
     /// Vytvoreni nove domacnosti
     /// </summary>
     /// <returns><see cref="SuccessfulServiceCallResult{}"/> of type <see cref="int" /> (HouseholdId)</returns>
-    /// <exception cref="CIS.Core.Exceptions.CisArgumentException">Code: 16023; HouseholdTypeId does not exist.</exception>
+    /// <exception cref="CIS.Core.Exceptions.CisNotFoundException">Code: 16023; HouseholdTypeId does not exist.</exception>
     /// <exception cref="CIS.Core.Exceptions.CisArgumentException">Code: 16000; Sales arrangement ID does not exist.</exception>
     /// <exception cref="CIS.Core.Exceptions.CisServiceUnavailableException">SalesArrangement service unavailable</exception>
     Task<IServiceCallResult> CreateHousehold(CreateHouseholdRequest request, CancellationToken cancellationToken = default(CancellationToken));
@@ -17,7 +17,7 @@ public interface IHouseholdServiceClient
     /// Smazani domacnosti
     /// </summary>
     /// <returns><see cref="SuccessfulServiceCallResult"/></returns>
-    /// <exception cref="CIS.Core.Exceptions.CisArgumentException">Code: 16022; Household ID does not exist.</exception>
+    /// <exception cref="CIS.Core.Exceptions.CisNotFoundException">Code: 16022; Household ID does not exist.</exception>
     /// <exception cref="CIS.Core.Exceptions.CisServiceUnavailableException">SalesArrangement service unavailable</exception>
     Task<IServiceCallResult> DeleteHousehold(int householdId, CancellationToken cancellationToken = default(CancellationToken));
 
@@ -25,7 +25,7 @@ public interface IHouseholdServiceClient
     /// Vraci instanci pozadovane domacnosti
     /// </summary>
     /// <returns><see cref="SuccessfulServiceCallResult{}"/> of type <see cref="Household" /></returns>
-    /// <exception cref="CIS.Core.Exceptions.CisArgumentException">Code: 16022; Household ID does not exist.</exception>
+    /// <exception cref="CIS.Core.Exceptions.CisNotFoundException">Code: 16022; Household ID does not exist.</exception>
     /// <exception cref="CIS.Core.Exceptions.CisServiceUnavailableException">SalesArrangement service unavailable</exception>
     Task<IServiceCallResult> GetHousehold(int householdId, CancellationToken cancellationToken = default(CancellationToken));
 
@@ -40,7 +40,7 @@ public interface IHouseholdServiceClient
     /// Update dat o domacnosti
     /// </summary>
     /// <returns><see cref="SuccessfulServiceCallResult"/></returns>
-    /// <exception cref="CIS.Core.Exceptions.CisArgumentException">Code: 16022; Household ID does not exist.</exception>
+    /// <exception cref="CIS.Core.Exceptions.CisNotFoundException">Code: 16022; Household ID does not exist.</exception>
     /// <exception cref="CIS.Core.Exceptions.CisServiceUnavailableException">SalesArrangement service unavailable</exception>
     Task<IServiceCallResult> UpdateHousehold(UpdateHouseholdRequest request, CancellationToken cancellationToken = default(CancellationToken));
 
@@ -48,8 +48,8 @@ public interface IHouseholdServiceClient
     /// Nalinkovani customeru na Household
     /// </summary>
     /// <returns><see cref="SuccessfulServiceCallResult"/></returns>
-    /// <exception cref="CIS.Core.Exceptions.CisArgumentException">Code: 16020; CustomerOnSA ID {CustomerOnSAId} does not exist.</exception>
-    /// <exception cref="CIS.Core.Exceptions.CisArgumentException">Code: 16022; Household ID {HouseholdId} does not exist.</exception>
+    /// <exception cref="CIS.Core.Exceptions.CisNotFoundException">Code: 16020; CustomerOnSA ID {CustomerOnSAId} does not exist.</exception>
+    /// <exception cref="CIS.Core.Exceptions.CisNotFoundException">Code: 16022; Household ID {HouseholdId} does not exist.</exception>
     /// <exception cref="CIS.Core.Exceptions.CisServiceUnavailableException">SalesArrangement service unavailable</exception>
     Task<IServiceCallResult> LinkCustomerOnSAToHousehold(int householdId, int? customerOnSAId1, int? customerOnSAId2, CancellationToken cancellationToken = default(CancellationToken));
 }
