d9fcab49fb6e088aa27c968dfb21ec316e18a4cf
Martin Heimlich <martin.heimlich@mpss.cz>
2022-11-30 15:35:39 +0100

    no message

--- DomainServices/RiskIntegrationService/Api/Endpoints/CreditWorthiness/V2/SimpleCalculate/SimpleCalculateRequestExtensions.cs
+++ DomainServices/RiskIntegrationService/Api/Endpoints/CreditWorthiness/V2/SimpleCalculate/SimpleCalculateRequestExtensions.cs
@@ -1,11 +1,64 @@
 ﻿using _V2 = DomainServices.RiskIntegrationService.Contracts.CreditWorthiness.V2;
 using _C4M = DomainServices.RiskIntegrationService.Api.Clients.CreditWorthiness.V1.Contracts;
-
+using DomainServices.RiskIntegrationService.Contracts.CreditWorthiness.V2;
+using System.Runtime.CompilerServices;
 
 namespace DomainServices.RiskIntegrationService.Api.Endpoints.CreditWorthiness.V2.SimpleCalculate;
 
 internal static class SimpleCalculateRequestExtensions
 {
+    public static CreditWorthinessCalculateRequest ToFullRequest(this CreditWorthinessSimpleCalculateRequest request)
+    {
+        var customer = new CreditWorthinessCustomer()
+        {
+            PrimaryCustomerId = request.PrimaryCustomerId,
+            Incomes = new List<CreditWorthinessIncome>
+            {
+                new() { IncomeTypeId = 4, Amount = request.TotalMonthlyIncome.GetValueOrDefault() }
+            },
+            Obligations = new List<CreditWorthinessObligation>
+            {
+                new()
+                {
+                    Amount = request.ObligationsSummary?.AuthorizedOverdraftsAmount ?? 0,
+                    ObligationTypeId = 4,
+                    IsObligationCreditorExternal = true
+                },
+                new()
+                {
+                    Amount = request.ObligationsSummary?.CreditCardsAmount ?? 0,
+                    ObligationTypeId = 3,
+                    IsObligationCreditorExternal = true
+                },
+                new()
+                {
+                    Installment = request.ObligationsSummary?.LoansInstallmentsAmount ?? 0,
+                    ObligationTypeId = 2,
+                    IsObligationCreditorExternal = true
+                },
+            }
+        };
+
+        return new CreditWorthinessCalculateRequest()
+        {
+            ResourceProcessId = request.ResourceProcessId,
+            UserIdentity = request.UserIdentity,
+            Product = request.Product,
+            Households = new List<CreditWorthinessHousehold>() {
+                new()
+                {
+                    ChildrenOverTenYearsCount = request.ChildrenCount,
+                    Customers = new List<CreditWorthinessCustomer>(){ customer },
+                    ExpensesSummary = new Contracts.Shared.V1.ExpensesSummary
+                    {
+                        Rent = request.ExpensesSummary?.Rent,
+                        Other = request.ExpensesSummary?.Other
+                    }
+                }
+            }
+        };
+    }
+
     public static List<_C4M.ExpensesSummary> ToC4M(this _V2.CreditWorthinessSimpleExpensesSummary expenses)
         => new()
         {
