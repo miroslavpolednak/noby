d37383fe88e96033394ed4cf2dfa874cd330835b
filip-tuma <filip@filiptuma.com>
2022-11-30 18:02:15 +0100

    no message

--- CIS/Infrastructure.ExternalServicesHelpers/Helpers.cs
+++ CIS/Infrastructure.ExternalServicesHelpers/Helpers.cs
@@ -8,6 +8,12 @@ public static class Helpers
     public static async Task<string?> SafeReadAsStringAsync(this HttpResponseMessage? response, CancellationToken cancellationToken = default(CancellationToken))
         => response?.Content == null ? null : await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
 
+    /// <summary>
+    /// Na základě TClient.GetVersion() získá instanci konfigurace z appsettings.json.
+    /// </summary>
+    /// <typeparam name="TClient">Proxy klient</typeparam>
+    /// <param name="serviceName">Název služby třetí strany</param>
+    /// <returns>Konfiguraci proxy</returns>
     public static ExternalServiceConfiguration<TClient> GetExternalServiceBaseConfiguration<TClient>(this WebApplicationBuilder builder, string serviceName)
         where TClient : class, IExternalServiceClient
     {
