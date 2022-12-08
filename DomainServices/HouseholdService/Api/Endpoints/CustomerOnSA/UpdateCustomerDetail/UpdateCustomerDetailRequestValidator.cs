9084698d301fa99200211c3bb23d32b638d58fb9
filip-tuma <filip@filiptuma.com>
2022-12-08 14:19:13 +0100

    no message

---
+++ DomainServices/HouseholdService/Api/Endpoints/CustomerOnSA/UpdateCustomerDetail/UpdateCustomerDetailRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using DomainServices.HouseholdService.Contracts;
+using FluentValidation;
+
+namespace DomainServices.HouseholdService.Api.Endpoints.CustomerOnSA.UpdateCustomerDetail;
+
+internal class UpdateCustomerDetailRequestValidator
+    : AbstractValidator<UpdateCustomerDetailRequest>
+{
+    public UpdateCustomerDetailRequestValidator()
+    {
+        RuleFor(t => t.CustomerOnSAId)
+            .GreaterThan(0)
+            .WithMessage("CustomerOnSAId must be > 0").WithErrorCode("16024");
+    }
+}
