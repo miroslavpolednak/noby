a65c00c8df689634268a946a40e23b903ed2f6d4
tomas-kucera <tomas.kucera@mpss.cz>
2022-12-13 15:55:20 +0100

    [HFICH-3475] LegalCapacityRestrictionTypes

---
+++ DomainServices/CodebookService/Contracts/Endpoints/LegalCapacityRestrictionTypes/ICodebookService.LegalCapacityRestrictionTypes.cs
@@ -0,0 +1,10 @@
+﻿using DomainServices.CodebookService.Contracts.Endpoints.LegalCapacityRestrictionTypes;
+using ProtoBuf.Grpc;
+
+namespace DomainServices.CodebookService.Contracts;
+
+public partial interface ICodebookService
+{
+    [OperationContract]
+    Task<List<LegalCapacityRestrictionTypeItem>> LegalCapacityRestrictionTypes(LegalCapacityRestrictionTypesRequest request, CallContext context = default);
+}
--- DomainServices/CodebookService/Contracts/Endpoints/LegalCapacityRestrictionTypes/LegalCapacityRestrictionTypeItem.cs
+++ DomainServices/CodebookService/Contracts/Endpoints/LegalCapacityRestrictionTypes/LegalCapacityRestrictionTypeItem.cs
@@ -1,16 +1,16 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace DomainServices.CodebookService.Contracts.Endpoints.LegalCapacities;
+namespace DomainServices.CodebookService.Contracts.Endpoints.LegalCapacityRestrictionTypes;
 
 [DataContract]
-public class LegalCapacityItem
+public class LegalCapacityRestrictionTypeItem
 {   
     [DataMember(Order = 1)]
     public int Id { get; set; }
 
 
     [DataMember(Order = 2)]
-    public CIS.Foms.Enums.LegalCapacities Code { get; set; }
+    public CIS.Foms.Enums.LegalCapacityRestrictions Code { get; set; }
 
     [DataMember(Order = 3)]
     public string Name { get; set; }
