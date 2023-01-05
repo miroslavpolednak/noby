9f757b850a05585b2b1db69c1308b46c4d720921
filip-tuma <filip@filiptuma.com>
2023-01-05 11:47:52 +0100

    CS fix

--- DomainServices/CustomerService/DomainServices.CustomerService.ExternalServices.Common/Extensions.cs
+++ DomainServices/CustomerService/DomainServices.CustomerService.ExternalServices.Common/Extensions.cs
@@ -2,9 +2,6 @@
 
 public static class Extensions
 {
-    public static string ToQuery(this bool? value)
-        => !value.GetValueOrDefault(false) ? "false" : "true";
-
     public static string ToQuery(this bool value)
         => value ? "true" : "false";
 }
