a05e1010617f90d1aa893c689e60c4618dcff34d
tomas-kucera <tomas.kucera@mpss.cz>
2023-01-19 14:34:13 +0100

    DocumentArchiveSvc - deploy (noby-temp-storage-cleaner.ps1)

---
+++ DomainServices/DocumentArchiveService/_Deploy/noby-temp-storage-cleaner.ps1
@@ -0,0 +1,10 @@
+﻿$limit = (Get-Date).AddDays(-3)
+# $limit = (Get-Date).AddMinutes(-1)
+
+
+$path = "D:\www\noby_temp_storage"
+
+# Delete files older than the $limit.
+Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+
+return 0
