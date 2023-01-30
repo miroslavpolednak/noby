3afe74143275328323c7ef8f26db19670174635d
tomas-kucera <tomas.kucera@mpss.cz>
2023-01-30 09:20:52 +0100

    Tests (Case)

--- Tests/BE/business/case/Case.py
+++ Tests/BE/business/case/Case.py
@@ -1,44 +1,14 @@
 from ..base.Base import Base
-
 from ..offer.Offer import Offer
-
 from .Household import Household
 from .Parameters import Parameters
-# from .Developer import Developer
-# from .MarketingActions import MarketingActions
-# from .Insurance import Insurance
-# from .Fee import Fee
 
 from common import Convertor
 
 DISPATCHES = {
             'offer': lambda value: Offer.from_json(value),
-            # 'product_type_id': lambda value: Convertor.to_int(value),
-            # 'loan_kind_id': lambda value: Convertor.to_int(value),
-            # 'loan_amount': lambda value: Convertor.to_decimal(value),
-            # 'loan_duration': lambda value: Convertor.to_int(value),
-            # 'fixed_rate_period': lambda value: Convertor.to_int(value),
-            # 'collateral_amount': lambda value: Convertor.to_decimal(value),
-            # 'payment_day': lambda value: Convertor.to_int(value),
-            # 'statement_type_id': lambda value: Convertor.to_int(value),
-            # 'is_employee_bonus_requested': lambda value: Convertor.to_bool(value),
-            # 'expected_date_of_drawing': lambda value: Convertor.to_date(value),
-            # 'with_guarantee': lambda value: Convertor.to_bool(value),
-            # 'financial_resources_own': lambda value: Convertor.to_decimal(value),
-            # 'financial_resources_other': lambda value: Convertor.to_decimal(value),
-            # 'drawing_type_id': lambda value: Convertor.to_int(value),
-            # 'drawing_duration_id': lambda value: Convertor.to_int(value),
-            # 'interest_rate_discount': lambda value: Convertor.to_decimal(value),
-            # 'interest_rate_discount_toggle': lambda value: Convertor.to_bool(value),
-            # 'resource_process_id': lambda value: Convertor.to_guid(value),
-
             'households': lambda value: Household.from_json_list(value),
             'parameters': lambda value: Parameters.from_json(value),
-
-            # 'developer': lambda value: Developer.from_json(value),
-            # 'risk_life_insurance': lambda value: Insurance.from_json(value),
-            # 'real_estate_insurance': lambda value: Insurance.from_json(value),
-            # 'fees': lambda value: Fee.from_json_list(value),
         }
 
 class Case(Base):
@@ -65,11 +35,7 @@ class Case(Base):
        return dict()
 
 
-
-# ?????:
-
 # --------------------------------------------------------------------------------------------
-
 # {
 #     "households": [
 #         {
@@ -157,7 +123,4 @@ class Case(Base):
 #         "agentConsentWithElCom": false
 #     }
 # }
-        
 # --------------------------------------------------------------------------------------------
-
-
