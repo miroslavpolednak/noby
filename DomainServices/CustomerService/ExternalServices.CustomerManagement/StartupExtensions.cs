b92ff052c341d7d72c5171a9a9f5e73bde8d251e
filip-tuma <filip@filiptuma.com>
2023-02-03 14:58:43 +0100

    no message

--- DomainServices/CustomerService/ExternalServices.CustomerManagement/StartupExtensions.cs
+++ DomainServices/CustomerService/ExternalServices.CustomerManagement/StartupExtensions.cs
@@ -29,7 +29,7 @@ public static class StartupExtensions
                 builder
                     .AddExternalServiceRestClient<CustomerManagement.V1.ICustomerManagementClient, CustomerManagement.V1.RealCustomerManagementClient>()
                     .AddExternalServicesKbHeaders("CUSTOMER_SERVICE")
-                    .AddExternalServicesKbPartyHeaders()
+                    //.AddExternalServicesKbPartyHeaders()
                     .AddExternalServicesErrorHandling(StartupExtensions.ServiceName);
                 break;
 
