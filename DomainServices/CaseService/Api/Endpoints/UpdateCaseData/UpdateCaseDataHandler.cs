02b0d500c4726733215eb40c6d2a3a142b15154f
filip-tuma <filip@filiptuma.com>
2023-02-09 14:21:25 +0100

    no message

--- DomainServices/CaseService/Api/Endpoints/UpdateCaseData/UpdateCaseDataHandler.cs
+++ DomainServices/CaseService/Api/Endpoints/UpdateCaseData/UpdateCaseDataHandler.cs
@@ -16,6 +16,8 @@ internal sealed class UpdateCaseDataHandler
         if (!(await _codebookService.ProductTypes(cancellation)).Any(t => t.Id == request.Data.ProductTypeId))
             throw new CisNotFoundException(13014, nameof(request.Data.ProductTypeId), request.Data.ProductTypeId);
 
+        var bonusChanged = entity.IsEmployeeBonusRequested != request.Data.IsEmployeeBonusRequested;
+
         // ulozit do DB
         entity.ContractNumber = request.Data.ContractNumber;
         entity.TargetAmount = request.Data.TargetAmount;
@@ -24,16 +26,32 @@ internal sealed class UpdateCaseDataHandler
 
         await _dbContext.SaveChangesAsync(cancellation);
 
+        // pokud se zmenil IsEmployeeBonusRequested, zavolat EAS
+        if (bonusChanged)
+        {
+            await _mediator.Publish(new Notifications.CaseStateChangedNotification
+            {
+                CaseId = request.CaseId,
+                CaseStateId = entity.State,
+                ClientName = $"{entity.FirstNameNaturalPerson} {entity.Name}",
+                ProductTypeId = request.Data.ProductTypeId,
+                CaseOwnerUserId = entity.OwnerUserId
+            }, cancellation);
+        }
+
         return new Google.Protobuf.WellKnownTypes.Empty();
     }
 
+    private readonly IMediator _mediator;
     private readonly CodebookService.Clients.ICodebookServiceClients _codebookService;
     private readonly CaseServiceDbContext _dbContext;
 
     public UpdateCaseDataHandler(
+        IMediator mediator,
         CodebookService.Clients.ICodebookServiceClients codebookService,
         CaseServiceDbContext dbContext)
     {
+        _mediator = mediator;
         _codebookService = codebookService;
         _dbContext = dbContext;
     }
