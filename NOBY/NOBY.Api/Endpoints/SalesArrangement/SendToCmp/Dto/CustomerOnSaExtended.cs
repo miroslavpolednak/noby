347d9df4265710088c81d147414f82b9cb2254b0
David Martínek <david.martinek@mpss.cz>
2023-02-13 17:22:07 +0100

    FE API - SA SendToCmp

---
+++ NOBY/NOBY.Api/Endpoints/SalesArrangement/SendToCmp/Dto/CustomerOnSaExtended.cs
@@ -0,0 +1,13 @@
+﻿using CIS.Infrastructure.gRPC.CisTypes;
+using DomainServices.HouseholdService.Contracts;
+
+namespace NOBY.Api.Endpoints.SalesArrangement.SendToCmp.Dto;
+
+internal class CustomerOnSaExtended
+{
+    public required Identity IdentityMp { get; init; }
+
+    public required Identity IdentityKb { get; init; }
+
+    public required CustomerOnSA Customer { get; init; }
+}
