cb0547629f1258fbc0491f45efca60f6a76b5055
David Martínek <david.martinek@mpss.cz>
2023-02-14 10:56:54 +0100

    Removed DataAggregator Library

--- InternalServices/DataAggregator/Library/Configuration/EasForm/EasFormConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CIS.InternalServices.DataAggregator.Configuration.EasForm;
-
-internal class EasFormConfiguration
-{
-    public required InputConfig InputConfig { get; init; }
-
-    public required IReadOnlyCollection<EasFormSourceField> SourceFields { get; init; }
-}
