500387dea3d0b26b66904abb5ea2010b9568475e
Polednak Miroslav <miroslav.polednak@mpss.cz>
2023-02-15 16:08:02 +0100

    Text edited

--- CIS/Infrastructure.gRPC/Extensions/ByteStringExtensions.cs
+++ CIS/Infrastructure.gRPC/Extensions/ByteStringExtensions.cs
@@ -5,7 +5,7 @@ namespace CIS.Infrastructure.gRPC;
 public static class ByteStringExtensions
 {
     /// <summary>
-    /// This copy method is memory efficient, there is no data copy (ToArray do copy of data).  
+    /// This method is memory efficient, there is no data copy (ToArray do copy of data).  
     /// Byte array instance should only be passed to methods which treat the array contents as read-only.
     /// </summary>
     public static byte[] ToArrayUnsafe(this ByteString bytesString)
