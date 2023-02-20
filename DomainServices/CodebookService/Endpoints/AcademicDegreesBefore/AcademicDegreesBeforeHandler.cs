9428aaf73c9e26f6a15a974f0019388fed03cefb
tomas-kucera <tomas.kucera@mpss.cz>
2023-02-20 10:54:47 +0100

    [HFICH-4609] CodebookSvc - AcademicDegreesBefore

--- DomainServices/CodebookService/Endpoints/AcademicDegreesBefore/AcademicDegreesBeforeHandler.cs
+++ DomainServices/CodebookService/Endpoints/AcademicDegreesBefore/AcademicDegreesBeforeHandler.cs
@@ -2,32 +2,45 @@
 using DomainServices.CodebookService.Contracts;
 using DomainServices.CodebookService.Contracts.Endpoints.AcademicDegreesBefore;
 
-namespace DomainServices.CodebookService.Endpoints.AcademicDegreesBefore
+
+namespace DomainServices.CodebookService.Endpoints.AcademicDegreesBefore;
+
+public class AcademicDegreesBeforeHandler
+    : IRequestHandler<AcademicDegreesBeforeRequest, List<GenericCodebookItem>>
 {
-    public class AcademicDegreesBeforeHandler
-        : IRequestHandler<AcademicDegreesBeforeRequest, List<GenericCodebookItem>>
+    //public async Task<List<GenericCodebookItem>> Handle(AcademicDegreesBeforeRequest request, CancellationToken cancellationToken)
+    //{
+    //    return await FastMemoryCache.GetOrCreate<GenericCodebookItem>(nameof(AcademicDegreesBeforeHandler), async () =>
+    //    {
+    //        await using (var connection = _connectionProvider.Create())
+    //        {
+    //            await connection.OpenAsync();
+    //            return (await connection.QueryAsync<GenericCodebookItem>("SELECT KOD 'Id', TEXT 'Name', CAST(1 as bit) 'IsValid' FROM [SBR].[CIS_TITULY] ORDER BY TEXT ASC")).ToList();
+    //        }
+    //    });
+    //}
+
+    // [HFICH-4609] číselník titulů je nově zamockován, starbuild číselník je pro nás nepoužitelný
+
+    public Task<List<GenericCodebookItem>> Handle(AcademicDegreesBeforeRequest request, CancellationToken cancellationToken)
     {
-        public async Task<List<GenericCodebookItem>> Handle(AcademicDegreesBeforeRequest request, CancellationToken cancellationToken)
+        return Task.FromResult(new List<GenericCodebookItem>
         {
-            return await FastMemoryCache.GetOrCreate<GenericCodebookItem>(nameof(AcademicDegreesBeforeHandler), async () =>
-            {
-                await using (var connection = _connectionProvider.Create())
-                {
-                    await connection.OpenAsync();
-                    return (await connection.QueryAsync<GenericCodebookItem>("SELECT KOD 'Id', TEXT 'Name', CAST(1 as bit) 'IsValid' FROM [SBR].[CIS_TITULY] ORDER BY TEXT ASC")).ToList();
-                }
-            });
-        }
+            new GenericCodebookItem() { Id = 0, Name = "Neuvedeno", IsValid = true},
+            new GenericCodebookItem() { Id = 3, Name = "Dr.", IsValid = true},
+            new GenericCodebookItem() { Id = 7, Name = "JUDr.", IsValid = true},
+            new GenericCodebookItem() { Id = 8, Name = "MUDr.", IsValid = true},
+        });
+    }
 
-        private readonly CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> _connectionProvider;
-        private readonly ILogger<AcademicDegreesBeforeHandler> _logger;
+    private readonly CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> _connectionProvider;
+    private readonly ILogger<AcademicDegreesBeforeHandler> _logger;
 
-        public AcademicDegreesBeforeHandler(
-            CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> connectionProvider, 
-            ILogger<AcademicDegreesBeforeHandler> logger)
-        {
-            _logger = logger;
-            _connectionProvider = connectionProvider;
-        }
+    public AcademicDegreesBeforeHandler(
+        CIS.Core.Data.IConnectionProvider<IXxdDapperConnectionProvider> connectionProvider, 
+        ILogger<AcademicDegreesBeforeHandler> logger)
+    {
+        _logger = logger;
+        _connectionProvider = connectionProvider;
     }
 }
