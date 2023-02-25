8c4f2fdb8d788729e94fd2f091d72f54204586e1
David Martínek <david.martinek@mpss.cz>
2023-02-25 16:58:32 +0100

    FE API - product SwaggerResponse

--- NOBY/NOBY.Api/Endpoints/Product/ProductController.cs
+++ NOBY/NOBY.Api/Endpoints/Product/ProductController.cs
@@ -34,10 +34,10 @@ public class ProductController : ControllerBase
     /// <returns>Seznam závazků na produktu</returns>
     [HttpGet("{caseId:long}/obligations")]
     [Produces("application/json")]
-    [SwaggerOperation(Tags = new[] { "Produkt" })]
     [ProducesResponseType(typeof(List<ProductObligation>), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [SwaggerResponse(StatusCodes.Status204NoContent)]
+    [SwaggerResponse(StatusCodes.Status404NotFound)]
+    [SwaggerOperation(Tags = new[] { "Produkt" })]
     public async Task<IActionResult> GetProductObligations([FromRoute] long caseId, CancellationToken cancellationToken)
     {
         var items = await _mediator.Send(new GetProductObligationList.GetProductObligationListRequest(caseId), cancellationToken);
