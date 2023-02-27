5711ec672e00dfa1ac722976f535561a95f18082
Mikel Marek EX <992474q@vsskb.cz>
2023-02-27 10:33:24 +0100

    json for test sms template

---
+++ Tests/backend/pytest/tests/notification_service/json/request/sms_template.py
@@ -0,0 +1,27 @@
+
+json_req_sms_full_template= \
+{
+  "phoneNumber": "00420607115686",
+  "processingPriority": 1,
+  "type": "TESTING",
+  "placeholders": [
+    {
+      "key": "date_from",
+      "value": "dneska"
+    },
+    {
+      "key": "date_to",
+      "value": "nekonecna a jeste dal"
+    },
+{
+      "key": "count",
+      "value": "1000000"
+    }
+  ],
+  "identifier": {
+    "identity": "992474q",
+    "identityScheme": "2"
+  },
+  "customId": "123546",
+  "documentId": "789456"
+}
