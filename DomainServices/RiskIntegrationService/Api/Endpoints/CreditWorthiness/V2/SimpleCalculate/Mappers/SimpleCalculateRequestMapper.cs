a757b9a067c40a360bc9b93917da3e300f35852a
Martin Heimlich <martin.heimlich@mpss.cz>
2023-03-08 15:06:25 +0100

    no message

--- DomainServices/RiskIntegrationService/Api/Endpoints/CreditWorthiness/V2/SimpleCalculate/Mappers/SimpleCalculateRequestMapper.cs
+++ DomainServices/RiskIntegrationService/Api/Endpoints/CreditWorthiness/V2/SimpleCalculate/Mappers/SimpleCalculateRequestMapper.cs
@@ -30,7 +30,7 @@ internal sealed class SimpleCalculateRequestMapper
         if (request.UserIdentity is not null)
         {
             var userInstance = await _xxvConnectionProvider.GetC4mUserInfo(request.UserIdentity, cancellation);
-            if (Helpers.IsDealerSchema(request.UserIdentity!.IdentityScheme))
+            if (Helpers.IsDealerSchema(userInstance.DealerCompanyId))
                 requestModel.LoanApplicationDealer = _C4M.C4mUserInfoDataExtensions.ToC4mDealer(userInstance, request.UserIdentity);
             else
                 requestModel.KbGroupPerson = _C4M.C4mUserInfoDataExtensions.ToC4mPerson(userInstance, request.UserIdentity);
@@ -63,7 +63,7 @@ internal sealed class SimpleCalculateRequestMapper
         if (request.UserIdentity is not null)
         {
             var userInstance = await _xxvConnectionProvider.GetC4mUserInfo(request.UserIdentity, cancellation);
-            if (Helpers.IsDealerSchema(request.UserIdentity!.IdentityScheme))
+            if (Helpers.IsDealerSchema(userInstance.DealerCompanyId))
                 requestModel.LoanApplicationDealer = _C4M.C4mUserInfoDataExtensions.ToC4mDealer(userInstance, request.UserIdentity);
             else
                 requestModel.KbGroupPerson = _C4M.C4mUserInfoDataExtensions.ToC4mPerson(userInstance, request.UserIdentity);
