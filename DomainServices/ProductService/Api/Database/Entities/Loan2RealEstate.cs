a95b9e1567b814959035ba8467770732c0f7712d
filip-tuma <filip@filiptuma.com>
2023-03-09 21:46:04 +0100

    no message

---
+++ DomainServices/ProductService/Api/Database/Entities/Loan2RealEstate.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainServices.ProductService.Api.Database.Entities;
+
+[Table("UverNemovitost", Schema = "dbo")]
+internal sealed class Loan2RealEstate
+{
+    public int UverId { get; set; }
+
+    public int NemovitostId { get; set; }
+
+    public int UcelKod { get; set; }
+}
