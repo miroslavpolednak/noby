8bdf7b8e6a23b2d826bd6f51203a4f6586826df5
filip-tuma <filip@filiptuma.com>
2023-03-14 14:16:57 +0100

    HFICH-4724

---
+++ DomainServices/ProductService/Api/Database/Entities/Collateral.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainServices.ProductService.Api.Database.Entities;
+
+[Table("Nemovitost", Schema = "dbo")]
+internal sealed class Collateral
+{
+    [Key]
+    public long Id { get; set; }
+
+    public long UverId { get; set; }
+
+    public long NemovitostId { get; set; }
+}
