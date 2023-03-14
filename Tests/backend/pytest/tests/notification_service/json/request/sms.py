e11b31f83cb5da7dca510a0d91a89ed61c249982
Mikel Marek EX <992474q@vsskb.cz>
2023-03-14 11:49:51 +0100

    pridani testu pro security s uzivateli

--- Tests/backend/pytest/tests/notification_service/json/request/sms.py
+++ Tests/backend/pytest/tests/notification_service/json/request/sms.py
@@ -20,3 +20,19 @@ json_req_sms_basic_full= \
   "customId": "CustomID_876",
   "documentId": "DocumentID_876"
 }
+
+json_req_sms_basic_epsy = \
+{
+  "phoneNumber":"+420607115686",
+  "processingPriority": 1,
+  "type": "TEST_EPSY",
+  "text": "Testovací sms text epsy"
+}
+
+json_req_sms_basic_insg = \
+{
+  "phoneNumber":"+420607115686",
+  "processingPriority": 1,
+  "type": "TEST_INSG",
+  "text": "Testovací sms text insg"
+}
--- Tests/backend/pytest/tests/notification_service/tests/sms.py
+++ Tests/backend/pytest/tests/notification_service/tests/sms.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
-from ..json.request.sms import json_req_sms_basic, json_req_sms_basic_full
+from ..json.request.sms import json_req_sms_basic, json_req_sms_basic_full, json_req_sms_basic_epsy, \
+    json_req_sms_basic_insg
 
 
 @pytest.mark.parametrize("auth", ["XX_INSG_RMT_USR_TEST", "XX_EPSY_RMT_USR_TEST", "XX_SB_RMT_USR_TEST"], indirect=True)
@@ -25,3 +26,25 @@ def test_sms_basic(ns_url, auth_params, auth, json_data):
     resp = resp.json()
     print(resp)
     # chybi dodelat asserty do databaze
+
+
+#pro testy zabezpeceni, jake sms jsou mozne odespilat pres urcite uzivatele - pouzita vnorena parametrizace
+@pytest.mark.parametrize("auth, json_data",
+                         [
+                            ("XX_EPSY_RMT_USR_TEST", json_req_sms_basic_epsy),
+                            ("XX_INSG_RMT_USR_TEST", json_req_sms_basic_insg)
+
+], indirect=["auth"])
+def test_sms_basic_security(ns_url, auth_params, auth, json_data):
+    username = auth[0]
+    password = auth[1]
+    session = requests.session()
+    resp = session.post(
+        ns_url["url"] + "/v1/notification/sms",
+        json=json_data,
+        auth=(username, password),
+        verify=False
+    )
+    resp = resp.json()
+    print(resp)
+    assert resp["status"] == 200
