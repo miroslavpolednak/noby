bfb3c6a4874e1e48314be0a75f705296a9ecd7f2
filip-tuma <filip@filiptuma.com>
2023-03-20 11:10:09 +0100

    no message

--- DomainServices/HouseholdService/Clients/Helpers.cs
+++ DomainServices/HouseholdService/Clients/Helpers.cs
@@ -7,7 +7,7 @@ public static class Helpers
     /// </summary>
     public static bool AreCustomersPartners(int? maritalStatusId1, int? maritalStatusId2)
     {
-        if (!maritalStatusId2.HasValue) return false;
+        if (!maritalStatusId1.HasValue || !maritalStatusId2.HasValue) return false;
         return !((maritalStatusId1.GetValueOrDefault() == 2 && maritalStatusId2.GetValueOrDefault() == 2)
             || (maritalStatusId1.GetValueOrDefault() == 6 && maritalStatusId2.GetValueOrDefault() == 6));
     }
