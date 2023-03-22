f50fa20a97858bc7d2f7f7e67e4f3a7531e11b6e
filip-tuma <filip@filiptuma.com>
2023-03-22 21:21:06 +0100

    no message

--- InternalServices/ServiceDiscovery/Api/Extensions/HealthCheckExtensions.cs
+++ InternalServices/ServiceDiscovery/Api/Extensions/HealthCheckExtensions.cs
@@ -3,11 +3,21 @@ using CIS.InternalServices.ServiceDiscovery.Api.Common;
 using Grpc.Health.V1;
 using Grpc.Net.Client;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace CIS.InternalServices.ServiceDiscovery.Api;
 
+internal class HealthCheckItem
+{
+    public string ServiceName { get; set; } = string.Empty;
+    public HealthCheckResponse.Types.ServingStatus Status { get; set; }
+    public long Elapsed { get; set; }
+}
+
 internal static class HealthCheckExtensions
 {
+    private const int _deadlineSeconds = 5;
+
     public static WebApplication MapGlobalHealthChecks(this WebApplication app)
     {
         app.MapGet(CIS.Core.CisGlobalConstants.CisHealthCheckEndpointUrl, async ([FromServices] ServicesMemoryCache cache, [FromServices] ICisEnvironmentConfiguration environmentConfiguration, CancellationToken cancellationToken) =>
@@ -16,15 +26,40 @@ internal static class HealthCheckExtensions
             var foundServices = (await cache.GetServices(new(environmentConfiguration.EnvironmentName), cancellationToken))
                 .Where(t => t.ServiceType == Contracts.ServiceTypes.Grpc && t.ServiceName != environmentConfiguration.DefaultApplicationKey)
                 .ToArray();
+            var result = new List<HealthCheckItem>(foundServices.Length);
 
             foreach (var service in foundServices)
             {
+                HealthCheckItem check = new()
+                {
+                    ServiceName = service.ServiceName,
+                    Status = HealthCheckResponse.Types.ServingStatus.Unknown
+                };
+
                 var channel = GrpcChannel.ForAddress(service.ServiceUrl);
                 var client = new Health.HealthClient(channel);
 
-                var response = await client.CheckAsync(new HealthCheckRequest(), cancellationToken: cancellationToken);
-                var status = response.Status;
+                var timer = new Stopwatch();
+                try
+                {
+                    var response = await client.CheckAsync(
+                        new HealthCheckRequest(), 
+                        deadline: DateTime.UtcNow.AddSeconds(_deadlineSeconds),
+                        cancellationToken: cancellationToken);
+                    check.Status = response.Status;
+                }
+                finally
+                {
+                    timer.Stop();
+
+                    check.Status = HealthCheckResponse.Types.ServingStatus.NotServing;
+                    check.Elapsed = timer.ElapsedMilliseconds;
+                }
+
+                result.Add(check);
             }
+
+            return result;
         });
 
         return app;
