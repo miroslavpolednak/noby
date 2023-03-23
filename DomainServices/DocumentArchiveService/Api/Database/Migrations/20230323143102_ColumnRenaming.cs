999dbe3f2263ad39e8ea2f6cb963f24ceca9ca13
Polednak Miroslav <miroslav.polednak@mpss.cz>
2023-03-23 15:36:02 +0100

    Added script for column renaming

---
+++ DomainServices/DocumentArchiveService/Api/Database/Migrations/20230323143102_ColumnRenaming.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DomainServices.DocumentArchiveService.Api.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnRenaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DATUM_PRIJETI",
+                table: "DocumentInterface",
+                newName: "CREATED_ON");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CREATED_ON",
+                table: "DocumentInterface",
+                newName: "DATUM_PRIJETI");
+        }
+    }
+}
