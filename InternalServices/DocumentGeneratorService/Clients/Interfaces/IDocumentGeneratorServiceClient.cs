6cbd947cb5c8f056b7ea014427637db669c53fa8
David Martínek <david.martinek@mpss.cz>
2023-03-27 21:40:04 +0200

    DataAggre3gator docs + fixed migration script

--- InternalServices/DocumentGeneratorService/Clients/Interfaces/IDocumentGeneratorServiceClient.cs
+++ InternalServices/DocumentGeneratorService/Clients/Interfaces/IDocumentGeneratorServiceClient.cs
@@ -4,5 +4,13 @@ namespace CIS.InternalServices.DocumentGeneratorService.Clients;
 
 public interface IDocumentGeneratorServiceClient
 {
+    /// <summary>
+    /// Vygenerování a naplnění PDF dle zadaných vstupních dat.
+    /// </summary>
+    /// <remarks>
+    /// Metoda naplní požadované PDF (podle typu, verze a varianty) daty, které příjdou na vstupu.
+    /// Data se získávají přes službu <a href="https://wiki.kb.cz/display/HT/DataAggregatorService">DataAggregator</a> metodou <a href="https://wiki.kb.cz/display/HT/getDocumentData">GetDocumentData</a>.
+    /// </remarks>
+    /// <returns></returns>
     Task<Document> GenerateDocument(GenerateDocumentRequest request, CancellationToken cancellationToken = default);
 }
