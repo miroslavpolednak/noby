b88238ebe92f73cb88330296c32188f246fec0da
filip-tuma <filip@filiptuma.com>
2023-04-03 11:41:48 +0200

    no message

--- DomainServices/CaseService/Api/Endpoints/GetCaseDetail/GetCaseDetailHandler.cs
+++ DomainServices/CaseService/Api/Endpoints/GetCaseDetail/GetCaseDetailHandler.cs
@@ -13,54 +13,6 @@ internal sealed class GetCaseDetailHandler
     /// </summary>
     public async Task<Case> Handle(GetCaseDetailRequest request, CancellationToken cancellation)
     {
-        var message = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.MainLoanProcessChanged
-        {
-            state = cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.ProcessStateEnum.COMPLETED,
-            eventId = "a",
-            currentTask = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.mainloanprocesschanged.CurrentTask
-            {
-                id = "b",
-                name = "c",
-                type = 1
-            },
-            @case = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.mainloanprocesschanged.Case
-            {
-                caseId = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.mainloanprocesschanged.CaseId
-                {
-                    id = "1"
-                },
-                mortgageInstance = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.mainloanprocesschanged.MortgageInstance
-                {
-                    Starbuild = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.mainloanprocesschanged.StarbuildInstanceId
-                    {
-                        id = "a"
-                    }
-                }
-            },
-            type = 1,
-            occurredOn = DateTime.Now,
-            id = Guid.NewGuid().ToString("N"),
-            name = "d",
-            processData = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.mainloanprocesschanged.ProcessData
-            {
-                @private = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.mainloanprocesschanged.BusinessObject
-                {
-                    mainLoanProcessData = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.mainloanprocesschanged.MainLoanProcessData
-                    {
-                        processPhase = new cz.mpss.api.starbuild.mortgage.workflow.processevents.v1.mainloanprocesschanged.ProcessPhase
-                        {
-                            code = 1,
-                            name = "a"
-                        }
-                    }
-                }
-            }
-        };
-
-        await _producer.Produce(message, cancellation);
-
-        return new Case();
-
         // vytahnout Case z DB
         return await _dbContext.Cases
             .Where(t => t.CaseId == request.CaseId)
@@ -71,11 +23,9 @@ internal sealed class GetCaseDetailHandler
     }
 
     private readonly CaseServiceDbContext _dbContext;
-    private readonly ITopicProducer<IMarker1> _producer;
 
-    public GetCaseDetailHandler(CaseServiceDbContext dbContext, ITopicProducer<IMarker1> producer)
+    public GetCaseDetailHandler(CaseServiceDbContext dbContext)
     {
-        _producer = producer;
         _dbContext = dbContext;
     }
 }
