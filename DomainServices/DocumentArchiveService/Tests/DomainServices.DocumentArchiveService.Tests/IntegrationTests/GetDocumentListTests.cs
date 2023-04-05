3525f4e8eed7952ca4c008d7179397bf95904d1f
Polednak Miroslav <miroslav.polednak@mpss.cz>
2023-04-05 11:38:20 +0200

    Added test 4

--- DomainServices/DocumentArchiveService/Tests/DomainServices.DocumentArchiveService.Tests/IntegrationTests/GetDocumentListTests.cs
+++ DomainServices/DocumentArchiveService/Tests/DomainServices.DocumentArchiveService.Tests/IntegrationTests/GetDocumentListTests.cs
@@ -8,13 +8,24 @@ public class GetDocumentListTests : IntegrationTestBase
     {
     }
 
-
     [Fact]
     public async Task GetDocumentList_PassInvalidParametrs_ShouldReturnValidationExp()
     {
         var client = CreateClient();
-        var response = await client.GetDocumentListAsync(new() { UserLogin="Test"}, default);
-        //ToDo
-        //await action.Should().ThrowAsync<InvalidOperationException>();
+        Func<Task> act = async () =>
+        {
+            await client.GetDocumentListAsync(new() { UserLogin = "Test" }, default);
+        };
+        // ToDo this shoud be different kind of exp, but there is a bug with logging 
+        await act.Should().ThrowAsync<ArgumentOutOfRangeException>().WithMessage("Index was out of range. Must be non-negative and less than the size of the collection. (Parameter 'index')");
+    }
+
+    [Fact]
+    public async Task GetDocumentList_PassCorrectParameters_ShouldReturnValues()
+    {
+        var client = CreateClient();
+        var result = await client.GetDocumentListAsync(new() {CaseId =123, UserLogin = "Test" }, default);
+        result.Should().NotBeNull();
+        
     }
 }
