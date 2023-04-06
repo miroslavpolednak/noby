a798e0805badd8d7909cd754117b91608e3dc7b3
filip-tuma <filip@filiptuma.com>
2023-04-06 22:07:53 +0200

    no message

---
+++ DomainServices/CodebookService/DatabaseMigrations/00006-HFICH-5335_CustomerChange3602.sql
@@ -0,0 +1,4 @@
+INSERT INTO [dbo].[SalesArrangementType] (Id, [Name], SalesArrangementCategory) VALUES 
+	(10, N'Údaje o přistupujícím k dluhu (3602)', 2),
+	(11, N'Žádost o přidání spoludlužníka (3602)', 2),
+	(12, N'Údaje o zůstávajícím v dluhu (3602)', 2);
