75aaf9f6f21b28f0bac5b3d383fc80c9cc829b0f
filip-tuma <filip@filiptuma.com>
2023-04-11 19:03:50 +0200

    UpdateCustomers fix

--- NOBY/NOBY.Api/Endpoints/Household/UpdateCustomers/UpdateCustomersValidator.cs
+++ NOBY/NOBY.Api/Endpoints/Household/UpdateCustomers/UpdateCustomersValidator.cs
@@ -2,7 +2,7 @@
 
 namespace NOBY.Api.Endpoints.Household.UpdateCustomers;
 
-internal abstract class UpdateCustomersValidator
+internal sealed class UpdateCustomersValidator
     : AbstractValidator<UpdateCustomersRequest>
 {
     public UpdateCustomersValidator()
@@ -16,9 +16,19 @@ internal abstract class UpdateCustomersValidator
             When(t => t.Customer1!.Identity is not null && t.Customer2!.Identity is not null, () =>
             {
                 RuleFor(x => x.Customer1!.Identity!.Id)
-                .NotEqual(x => x.Customer2!.Identity!.Id)
-                .WithMessage("KBID most not be equal");
+                    .NotEqual(x => x.Customer2!.Identity!.Id)
+                    .WithMessage("KBID most not be equal");
             });
         });
+
+        RuleFor(t => t.Customer1!.CustomerOnSAId)
+            .Must((r, id) => (r.Customer1!.Identity?.Id ?? 0) == 0 || id.GetValueOrDefault() > 0)
+            .WithMessage("CustomerOnSAId1 can not be empty while Identity is set")
+            .When(t => t.Customer1 is not null);
+
+        RuleFor(t => t.Customer2!.CustomerOnSAId)
+            .Must((r, id) => (r.Customer2!.Identity?.Id ?? 0) == 0 || id.GetValueOrDefault() > 0)
+            .WithMessage("CustomerOnSAId2 can not be empty while Identity is set")
+            .When(t => t.Customer2 is not null);
     }
 }
