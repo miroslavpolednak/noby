f906bbd25a3372633478e2352b7ef9775dd49dca
David Martínek <david.martinek@mpss.cz>
2023-04-11 19:07:14 +0200

    SbWebApi - added FindTasks endpoints

---
+++ DomainServices/CaseService/ExternalServices.SbWebApi/Dto/FindTasks/FindByCaseIdRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainServices.CaseService.ExternalServices.SbWebApi.Dto.FindTasks;
+
+public sealed class FindByCaseIdRequest : RequestBase
+{
+    public required long CaseId { get; init; }
+
+    public required string SearchPattern { get; init; }
+
+    public ICollection<int>? TaskStates { get; init; }
+}
