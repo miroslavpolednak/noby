1e0b5d2031f1eafea5dd797078d0ee0037e05c6b
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-04-19 18:41:57 +0200

    wip: get/update task detail handler skeleton

---
+++ NOBY/NOBY.Api/Endpoints/Cases/GetTaskDetail/GetTaskDetailHandler.cs
@@ -0,0 +1,86 @@
+﻿using CIS.Core.Security;
+using DomainServices.CaseService.Clients;
+using DomainServices.DocumentArchiveService.Clients;
+using NOBY.Api.Endpoints.Cases.GetTaskDetail.Dto;
+using NOBY.Api.Endpoints.Cases.GetTaskList.Dto;
+using NOBY.Api.Endpoints.DocumentArchive.GetDocumentList;
+
+namespace NOBY.Api.Endpoints.Cases.GetTaskDetail;
+
+internal sealed class GetTaskDetailHandler : IRequestHandler<GetTaskDetailRequest, GetTaskDetailResponse>
+{
+    private readonly ICaseServiceClient _caseService;
+    private readonly IDocumentArchiveServiceClient _documentArchiveService;
+    private readonly ICurrentUserAccessor _currentUserAccessor;
+
+    public async Task<GetTaskDetailResponse> Handle(GetTaskDetailRequest request, CancellationToken cancellationToken)
+    {
+        var taskList = await _caseService.GetTaskList(request.CaseId, cancellationToken);
+
+        var task = taskList.FirstOrDefault(t => t.TaskId == request.TaskId) 
+                   ?? throw new CisNotFoundException(0, "TODO");
+
+        // TODO
+        
+        var documentListResponse = await _documentArchiveService.GetDocumentList(new DomainServices.DocumentArchiveService.Contracts.GetDocumentListRequest
+        {
+            CaseId = request.CaseId,
+            ContractNumber = "",
+            CreatedOn = DateTime.Now,
+            OrderId = 0,
+            UserLogin = "",
+            AuthorUserLogin = "",
+            FolderDocumentId = "",
+            PledgeAgreementNumber = ""
+        }, cancellationToken);
+        
+        return new GetTaskDetailResponse
+        {
+            TaskDetail = new WorkflowTaskDetail
+            {
+                TaskIdSB = task.TaskIdSb,
+                PerformerLogin = "",
+                PerformerName = "",
+                ProcessNameLong = "",
+                SentToCustomer = false,
+                OrderId = 0,
+                TaskCommunication = new List<TaskCommunicationItem>()
+            },
+            Task = new WorkflowTask
+            {
+                TaskId = task.TaskId,
+                CreatedOn = task.CreatedOn,
+                TaskTypeId = task.TaskTypeId,
+                TaskTypeName = task.TaskTypeName,
+                TaskSubtypeName = task.TaskSubtypeName,
+                ProcessId = task.ProcessId,
+                ProcessNameShort = task.ProcessNameShort,
+                // StateId = taskState.Id,
+                // StateName = taskState.Name,
+                // StateFilter = Enum.Parse<StateFilter>(taskState.Filter, true),
+                // StateIndicator = Enum.Parse<StateIndicator>(taskState.Indicator, true)
+            },
+            Documents = documentListResponse.Metadata
+                .Select(m => new DocumentsMetadata
+                {
+                    DocumentId = m.DocumentId,
+                    Description = m.Description,
+                    CreatedOn = m.CreatedOn,
+                    FileName = m.Filename,
+                    // UploadStatus = m.Status,
+                    EaCodeMainId = m.EaCodeMainId
+                })
+                .ToList()
+        };
+    }
+    
+    public GetTaskDetailHandler(
+        ICaseServiceClient caseService,
+        IDocumentArchiveServiceClient documentArchiveService,
+        ICurrentUserAccessor currentUserAccessor)
+    {
+        _caseService = caseService;
+        _documentArchiveService = documentArchiveService;
+        _currentUserAccessor = currentUserAccessor;
+    }
+}
---
+++ NOBY/NOBY.Api/Endpoints/Cases/UpdateTaskDetail/UpdateTaskDetailHandler.cs
@@ -0,0 +1,73 @@
+﻿using CIS.Core.Security;
+using DomainServices.CaseService.Clients;
+using DomainServices.CaseService.Contracts;
+using DomainServices.DocumentArchiveService.Clients;
+using DomainServices.DocumentArchiveService.Contracts;
+
+namespace NOBY.Api.Endpoints.Cases.UpdateTaskDetail;
+
+internal sealed class UpdateTaskDetailHandler : IRequestHandler<UpdateTaskDetailRequest>
+{
+    private readonly ICaseServiceClient _caseService;
+    private readonly IDocumentArchiveServiceClient _documentArchiveService;
+    private readonly ICurrentUserAccessor _currentUserAccessor;
+    
+    public async Task Handle(UpdateTaskDetailRequest request, CancellationToken cancellationToken)
+    {
+        var caseDetail = await _caseService.GetCaseDetail(request.CaseId, cancellationToken);
+        
+        var taskDetail = await _caseService.GetTaskDetail(request.TaskIdSB, cancellationToken);
+        
+        foreach (var attachment in request.Attachments)
+        {
+            var documentId = await _documentArchiveService.GenerateDocumentId(new GenerateDocumentIdRequest
+            {
+                EnvironmentName = EnvironmentNames.Dev
+            }, cancellationToken);
+            await _documentArchiveService.UploadDocument(new UploadDocumentRequest
+            {
+                Metadata = new DocumentMetadata
+                {
+                    CaseId = request.CaseId,
+                    ContractNumber = caseDetail.Data.ContractNumber,
+                    Completeness = 0,
+                    Description = attachment.Description,
+                    Filename = attachment.FileName,
+                    Priority = "",
+                    Status = "",
+                    CreatedOn = DateTime.Now,
+                    DocumentDirection = "",
+                    DocumentId = documentId,
+                    FolderDocument = "",
+                    FormId = "",
+                    MinorCodes = {  },
+                    OrderId = 0,
+                    SourceSystem = "",
+                    AuthorUserLogin = "",
+                    FolderDocumentId = "",
+                    PledgeAgreementNumber = "",
+                    EaCodeMainId = attachment.EaCodeMainId
+                },
+                NotifyStarBuild = false
+            }, cancellationToken);
+        }
+
+        await _caseService.CompleteTask(new CompleteTaskRequest
+        {
+            TaskIdSb = request.TaskIdSB,
+            TaskUserResponse = request.TaskUserResponse,
+            TaskDocumentIds = {  }
+        }, cancellationToken);
+
+    }
+
+    public UpdateTaskDetailHandler(
+        ICaseServiceClient caseService,
+        IDocumentArchiveServiceClient documentArchiveService,
+        ICurrentUserAccessor currentUserAccessor)
+    {
+        _caseService = caseService;
+        _documentArchiveService = documentArchiveService;
+        _currentUserAccessor = currentUserAccessor;
+    }
+}
