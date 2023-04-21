111478a7526e26dbd22907492d3232ba1ba4856f
Polednak Miroslav <miroslav.polednak@mpss.cz>
2023-04-21 11:51:06 +0200

    Added api endpoint

---
+++ NOBY/NOBY.Api/Endpoints/DocumentArchive/SetDocumentStatusInQueue/SetDocumentStatusInQueueHandler.cs
@@ -0,0 +1,23 @@
+﻿using DomainServices.DocumentArchiveService.Clients;
+
+namespace NOBY.Api.Endpoints.DocumentArchive.SetDocumentStatusInQueue;
+
+public class SetDocumentStatusInQueueHandler : IRequestHandler<SetDocumentStatusInQueueRequest>
+{
+    private readonly IDocumentArchiveServiceClient _client;
+
+    public SetDocumentStatusInQueueHandler(IDocumentArchiveServiceClient client)
+    {
+        _client = client;
+    }
+
+    public async Task Handle(SetDocumentStatusInQueueRequest request, CancellationToken cancellationToken)
+    {
+        await _client.SetDocumentStatusInQueue(new()
+        {
+            EArchivId = request.DocumentId,
+            StatusInQueue = request.StatusId
+        },
+        cancellationToken);
+    }
+}
