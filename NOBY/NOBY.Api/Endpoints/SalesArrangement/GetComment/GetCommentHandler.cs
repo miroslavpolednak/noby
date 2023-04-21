28aa6001aee500d32e3f66a39ae2441998b68997
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-04-21 14:12:52 +0200

    feature: noby.api get/put sales arrangement comment

--- NOBY/NOBY.Api/Endpoints/SalesArrangement/GetComment/GetCommentHandler.cs
+++ NOBY/NOBY.Api/Endpoints/SalesArrangement/GetComment/GetCommentHandler.cs
@@ -1,11 +1,24 @@
-﻿using NOBY.Api.Endpoints.SalesArrangement.Dto;
+﻿using CIS.Foms.Types.Enums;
+using DomainServices.SalesArrangementService.Clients;
+using NOBY.Api.Endpoints.SalesArrangement.Dto;
 
 namespace NOBY.Api.Endpoints.SalesArrangement.GetComment;
 
 internal sealed  class GetCommentHandler : IRequestHandler<GetCommentRequest, Comment>
 {
-    public Task<Comment> Handle(GetCommentRequest request, CancellationToken cancellationToken)
+    private readonly ISalesArrangementServiceClient _salesArrangementService;
+    
+    public async Task<Comment> Handle(GetCommentRequest request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var salesArrangement = await _salesArrangementService.GetSalesArrangement(request.SalesArrangementId, cancellationToken);
+        
+        return (SalesArrangementTypes)salesArrangement.SalesArrangementTypeId != SalesArrangementTypes.Mortgage
+            ? new Comment { Comment = salesArrangement.Mortgage.Comment }
+            : throw new NobyValidationException(90001);
+    }
+    
+    public GetCommentHandler(ISalesArrangementServiceClient salesArrangementService)
+    {
+        _salesArrangementService = salesArrangementService;
     }
 }
--- NOBY/NOBY.Api/Endpoints/SalesArrangement/UpdateComment/UpdateCommentHandler.cs
+++ NOBY/NOBY.Api/Endpoints/SalesArrangement/UpdateComment/UpdateCommentHandler.cs
@@ -1,9 +1,46 @@
-﻿namespace NOBY.Api.Endpoints.SalesArrangement.UpdateComment;
+﻿using CIS.Foms.Types.Enums;
+using DomainServices.SalesArrangementService.Clients;
+using DomainServices.SalesArrangementService.Contracts;
+
+namespace NOBY.Api.Endpoints.SalesArrangement.UpdateComment;
 
 internal sealed  class UpdateCommentHandler : IRequestHandler<UpdateCommentRequest>
 {
-    public Task Handle(UpdateCommentRequest request, CancellationToken cancellationToken)
+    private readonly ISalesArrangementServiceClient _salesArrangementService;
+
+    public async Task Handle(UpdateCommentRequest request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var salesArrangement = await _salesArrangementService.GetSalesArrangement(request.SalesArrangementId, cancellationToken);
+
+        if ((SalesArrangementTypes)salesArrangement.SalesArrangementTypeId != SalesArrangementTypes.Mortgage)
+        {
+            throw new NobyValidationException(90001);
+        }
+        
+        var mortgageParameters = new SalesArrangementParametersMortgage
+        {
+            Agent = salesArrangement.Mortgage.Agent,
+            Comment = request.Comment.Comment ?? string.Empty,
+            IncomeCurrencyCode = salesArrangement.Mortgage.IncomeCurrencyCode,
+            ResidencyCurrencyCode = salesArrangement.Mortgage.ResidencyCurrencyCode,
+            ContractSignatureTypeId = salesArrangement.Mortgage.ContractSignatureTypeId,
+            ExpectedDateOfDrawing = salesArrangement.Mortgage.ExpectedDateOfDrawing,
+            AgentConsentWithElCom = salesArrangement.Mortgage.AgentConsentWithElCom,
+        };
+        
+        mortgageParameters.LoanRealEstates.AddRange(salesArrangement.Mortgage.LoanRealEstates);
+
+        var updateParametersRequest = new UpdateSalesArrangementParametersRequest
+        {
+            SalesArrangementId = request.SalesArrangementId,
+            Mortgage = mortgageParameters
+        };
+
+        await _salesArrangementService.UpdateSalesArrangementParameters(updateParametersRequest, cancellationToken);
+    }
+    
+    public UpdateCommentHandler(ISalesArrangementServiceClient salesArrangementService)
+    {
+        _salesArrangementService = salesArrangementService;
     }
 }
