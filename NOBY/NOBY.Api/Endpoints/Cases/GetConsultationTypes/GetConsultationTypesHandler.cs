90c49902c0d171f2e422318fce50811beaf24b69
filip-tuma <filip@filiptuma.com>
2023-04-24 13:56:49 +0200

    HFICH-5516

--- NOBY/NOBY.Api/Endpoints/Cases/GetConsultationTypes/GetConsultationTypesHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Cases/GetConsultationTypes/GetConsultationTypesHandler.cs
@@ -1,4 +1,5 @@
 ﻿using DomainServices.CaseService.Clients;
+using DomainServices.CodebookService.Clients;
 
 namespace NOBY.Api.Endpoints.Cases.GetConsultationTypes;
 
@@ -11,17 +12,23 @@ internal sealed class GetConsultationTypesHandler
             .Where(t => t.ProcessId == request.ProcessId)
             .ToList();
 
-        return processesList.Select(t => new GetConsultationTypesResponseItem
-        {
-            TaskSubtypeId = 1,
-            taskSubtypeName = ""
-        }).ToList();
+        var matrix = await _codebookService.WorkflowConsultationMatrix(cancellationToken);
+
+        return matrix
+            .Where(t => t.IsValidFor.Any(x => processesList.Any(p => x.ProcessPhaseId == p.ProcessPhaseId && x.ProcessTypeId == p.ProcessTypeId)))
+            .Select(t => new GetConsultationTypesResponseItem
+            {
+                TaskSubtypeId = t.TaskSubtypeId,
+                taskSubtypeName = t.TaskSubtypeName
+            }).ToList();
     }
 
+    private readonly ICodebookServiceClients _codebookService;
     private readonly ICaseServiceClient _caseService;
 
-    public GetConsultationTypesHandler(ICaseServiceClient caseService)
+    public GetConsultationTypesHandler(ICaseServiceClient caseService, ICodebookServiceClients codebookService)
     {
+        _codebookService = codebookService;
         _caseService = caseService;
     }
 }
