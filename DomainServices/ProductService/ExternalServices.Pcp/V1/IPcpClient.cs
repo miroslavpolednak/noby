e52bec31b73b5fc60ba555c8ed1cfa062a6d3036
filip-tuma <filip@filiptuma.com>
2023-05-03 15:24:54 +0200

    no message

---
+++ DomainServices/ProductService/ExternalServices.Pcp/V1/IPcpClient.cs
@@ -0,0 +1,11 @@
+﻿using CIS.Infrastructure.ExternalServicesHelpers;
+
+namespace DomainServices.ProductService.ExternalServices.Pcp.V1;
+
+public interface IPcpClient
+    : IExternalServiceClient
+{
+    Task x();
+
+    const string Version = "V1";
+}
---
+++ DomainServices/ProductService/ExternalServices.Pcp/V1/RealPcpClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainServices.ProductService.ExternalServices.Pcp.V1;
+internal class RealPcpClient
+{
+}
