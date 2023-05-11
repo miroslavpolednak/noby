6aded8f79f9e3c428091438e5664995bd026b7c2
filip-tuma <filip@filiptuma.com>
2023-05-11 17:18:19 +0200

    no message

---
+++ NOBY/NOBY.Api/Endpoints/Users/GetLoggedInUser/GetLoggedInUserHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace NOBY.Api.Endpoints.Users.GetLoggedInUser;
+
+internal sealed class GetLoggedInUserHandler
+    : IRequestHandler<GetLoggedInUserRequest, GetLoggedInUserResponse>
+{
+    public async Task<GetLoggedInUserResponse> Handle(GetLoggedInUserRequest request, CancellationToken cancellationToken)
+    {
+        var userInstance = await _userService.GetUser(_userAccessor.User!.Id, cancellationToken);
+
+        return new GetLoggedInUserResponse
+        {
+            UserId = userInstance.UserId,
+            UserInfo = new GetLoggedInUserResponseUserInfo
+            {
+                FirstName = userInstance.UserInfo.FirstName,
+                LastName = userInstance.UserInfo.LastName,
+                Cin = userInstance.UserInfo.Cin,
+                Cpm = userInstance.UserInfo.Cpm,
+                Icp = userInstance.UserInfo.Icp
+            },
+            UserIdentifiers = userInstance.UserIdentifiers.Select(t => (CIS.Foms.Types.UserIdentity)t).ToList(),
+            UserAttributes = new GetLoggedInUserResponseAttributes
+            {
+                EmailAddress = userInstance.UserAttributes?.Email,
+                PhoneNumber = userInstance.UserAttributes?.PhoneNumber,
+                IsUserVIP = userInstance.UserAttributes?.IsUserVIP ?? false
+            }
+        };
+    }
+
+    private readonly DomainServices.UserService.Clients.IUserServiceClient _userService;
+    private readonly CIS.Core.Security.ICurrentUserAccessor _userAccessor;
+
+    public GetLoggedInUserHandler(
+        CIS.Core.Security.ICurrentUserAccessor userAccessor,
+        DomainServices.UserService.Clients.IUserServiceClient userService)
+    {
+        _userAccessor = userAccessor;
+        _userService = userService;
+    }
+}
