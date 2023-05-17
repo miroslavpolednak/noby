91ce8b8348ef4495a73ee52843da4c3aa25b57d7
filip-tuma <filip@filiptuma.com>
2023-05-17 15:08:46 +0200

    no message

--- DomainServices/CodebookService/Api/Endpoints/CodebookService.cs
+++ DomainServices/CodebookService/Api/Endpoints/CodebookService.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CIS.Core.Data;
+using DomainServices.CodebookService.Api.Database;
+using DomainServices.CodebookService.Contracts.v1;
+using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DomainServices.CodebookService.Api.Endpoints;
 
@@ -6,5 +10,20 @@ namespace DomainServices.CodebookService.Api.Endpoints;
 internal sealed class CodebookService
     : Contracts.v1.CodebookService.CodebookServiceBase
 {
+    public override Task<GenericCodebookItemResponse> AcademicDegreesAfter(Google.Protobuf.WellKnownTypes.Empty request, ServerCallContext context)
+        => _xxd.GetGenericItems("SELECT KOD 'Id', TEXT 'Name', CAST(1 as bit) 'IsValid' FROM [SBR].[CIS_TITULY_ZA] ORDER BY TEXT ASC");
 
+    private readonly IConnectionProvider<IKonsdbDapperConnectionProvider> _konsdb;
+    private readonly IConnectionProvider<IXxdHfDapperConnectionProvider> _xxdhf;
+    private readonly IConnectionProvider<IXxdDapperConnectionProvider> _xxd;
+
+    public CodebookService(
+        IConnectionProvider<IKonsdbDapperConnectionProvider> konsdb, 
+        IConnectionProvider<IXxdHfDapperConnectionProvider> xxdhf, 
+        IConnectionProvider<IXxdDapperConnectionProvider> xxd)
+    {
+        _konsdb = konsdb;
+        _xxdhf = xxdhf;
+        _xxd = xxd;
+    }
 }
---
+++ DomainServices/CodebookService/Api/Extensions/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using CIS.Core.Data;
+using Dapper;
+using DomainServices.CodebookService.Api.Database;
+using DomainServices.CodebookService.Contracts.v1;
+using System.Runtime.CompilerServices;
+
+namespace DomainServices.CodebookService.Api;
+
+internal static class Helpers
+{
+    public static Task<GenericCodebookItemResponse> GetGenericItems(this IConnectionProvider connectionProvider, ReadOnlySpan<char> sqlQuery, [CallerMemberName] string method = "")
+    {
+        GenericCodebookItemResponse response = new();
+        response.Items.AddRange(connectionProvider.GetOrCreate<GenericCodebookItem>(sqlQuery.ToString(), method.AsSpan()));
+        return Task.FromResult(response);
+    }
+
+    public static List<TResponse> GetOrCreate<TResponse>(this IConnectionProvider connectionProvider, string sqlQuery, ReadOnlySpan<char> method)
+        where TResponse : class
+    {
+        return FastMemoryCache.GetOrCreate<TResponse>(method.ToString(), () =>
+        {
+            using var connection = connectionProvider.Create();
+            connection.Open();
+            return connection.Query<TResponse>(sqlQuery).AsList();
+        });
+    }
+}
