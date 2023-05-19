2861fe2140e1e20ad531395a45c6c2e5332b99ae
Martin Heimlich <martin.heimlich@mpss.cz>
2023-05-19 23:10:49 +0200

    no message

---
+++ DomainServices/RiskIntegrationService/ExternalServices.CreditWorthiness/V3/MockCreditWorthinessClient.cs
@@ -0,0 +1,12 @@
+﻿using DomainServices.RiskIntegrationService.ExternalServices.CreditWorthiness.V3.Contracts;
+
+namespace DomainServices.RiskIntegrationService.ExternalServices.CreditWorthiness.V3;
+
+internal sealed class MockCreditWorthinessClient
+    : ICreditWorthinessClient
+{
+    public Task<CreditWorthinessCalculationResponse> Calculate(CreditWorthinessCalculationArguments request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new CreditWorthinessCalculationResponse{});
+    }
+}
