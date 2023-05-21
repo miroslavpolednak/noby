8c7f947e200cc6e8af238e2badc676b7a930b388
David Martínek <david.martinek@mpss.cz>
2023-05-21 17:43:41 +0200

    CS - JSchema fixed case sensitive problem

--- DomainServices/CustomerService/ExternalServices.IdentifiedSubjectBr/V1/Dto/IdentifiedSubjectValidationError.cs
+++ DomainServices/CustomerService/ExternalServices.IdentifiedSubjectBr/V1/Dto/IdentifiedSubjectValidationError.cs
@@ -1,22 +1,30 @@
-﻿namespace DomainServices.CustomerService.ExternalServices.IdentifiedSubjectBr.V1.Dto;
+﻿using Newtonsoft.Json;
+
+namespace DomainServices.CustomerService.ExternalServices.IdentifiedSubjectBr.V1.Dto;
 
 public class IdentifiedSubjectValidationError
 {
-    public required string Message { get; init; }
+    [JsonProperty(PropertyName = "message")]
+    public string? Message { get; init; }
 
+    [JsonProperty(PropertyName = "detail")]
     public required ErrorDetail Detail { get; init; }
 
     public class ErrorDetail
     {
+        [JsonProperty(PropertyName = "violatedConstraints")]
         public ViolatedConstraint[]? ViolatedConstraints { get; init; }
     }
 
     public class ViolatedConstraint
     {
+        [JsonProperty(PropertyName = "attribute")]
         public string? Attribute { get; init; }
 
+        [JsonProperty(PropertyName = "invalidValue", Required = Required.Default)]
         public string? InvalidValue { get; init; }
 
+        [JsonProperty(PropertyName = "message")]
         public string? Message { get; init; }
     }
 }
