799ab1793a320a0a7657f89ae646a4f4ad2eefcf
filip-tuma <filip@filiptuma.com>
2023-05-21 22:58:03 +0200

    no message

--- DomainServices/CodebookService/ClientsGenerator/ClientCallMethodsGenerator.cs
+++ DomainServices/CodebookService/ClientsGenerator/ClientCallMethodsGenerator.cs
@@ -1,16 +1,77 @@
 ﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
 
-namespace DomainServices.CodebookService.ClientsGenerator;
-
-[Generator]
-public class ClientCallMethodsGenerator
-    : IIncrementalGenerator
+namespace DomainServices.CodebookService.ClientsGenerators
 {
-    public void Initialize(IncrementalGeneratorInitializationContext context)
+    [Generator]
+    public sealed class ServiceClientsSourceGenerator : ISourceGenerator
     {
-        throw new Exception("Test exception!");
+        private static string[] _hardcodedCodebooks = new[] { "GetDeveloper", "GetDeveloperProject", "DeveloperSearch", "Reset", "GetOperator" };
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            /*if (!Debugger.IsAttached)
+            {
+                Debugger.Launch();
+            }*/
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // finding Contracts reference assembly Symbols
+            IAssemblySymbol assemblySymbol = context.Compilation.SourceModule.ReferencedAssemblySymbols.First(q => q.Name == "DomainServices.CodebookService.Contracts");
+            var serviceInterface = assemblySymbol.GetTypeByMetadataName("DomainServices.CodebookService.Contracts.ICodebookService");
+            // get all methods in interface
+            var endpoints = serviceInterface.GetMembers()
+                .Where(t => t is IMethodSymbol)
+                .Cast<IMethodSymbol>()
+                .Where(t => !_hardcodedCodebooks.Contains(t.OriginalDefinition.Name))
+                .Select(t => new Endpoint(t.OriginalDefinition.Name, t.OriginalDefinition.ReturnType.ToString(), t.OriginalDefinition.Parameters.Count() == 2 ? t.OriginalDefinition.Parameters[0].Type.ToString() : ""))
+                .ToList();
+
+            var sbImpl = new StringBuilder();
+            sbImpl.AppendLine("namespace DomainServices.CodebookService.Clients;");
+            sbImpl.AppendLine("internal partial class CodebookService {");
+
+            var sbInterface = new StringBuilder();
+            sbInterface.AppendLine("namespace DomainServices.CodebookService.Clients;");
+            sbInterface.AppendLine("public partial interface ICodebookServiceClients {");
+
+            endpoints.ForEach(m =>
+            {
+                // implementation
+                sbImpl.Append($"public async {m.ReturnType} {m.MethodName}(CancellationToken cancellationToken = default(CancellationToken))");
+                sbImpl.AppendLine($" => await _cache.GetOrCreate(\"{m.MethodName}\", async () => await _codebookService.{m.MethodName}(new {m.RequestDtoType}(), cancellationToken));");
+
+                // interface
+                sbInterface.AppendLine($"{m.ReturnType} {m.MethodName}(CancellationToken cancellationToken = default(CancellationToken));");
+            });
+
+            sbImpl.Append("}");
+            sbInterface.Append("}");
+
+            // generate source using targets ...
+            context.AddSource("ICodebookServiceClients_generated.cs", sbInterface.ToString());
+            context.AddSource("CodebookService_generated.cs", sbImpl.ToString());
+        }
     }
 
+    internal class Endpoint
+    {
+        public Endpoint(string methodName, string returnType, string requestDtoType)
+        {
+            MethodName = methodName;
+            ReturnType = returnType;
+            RequestDtoType = requestDtoType;
+            ReturnTypeSync = returnType.Substring(28, returnType.Length - 29);
+        }
+
+        public string MethodName { get; set; }
+        public string ReturnType { get; set; }
+        public string ReturnTypeSync { get; set; }
+        public string RequestDtoType { get; set; }
+    }
 }
