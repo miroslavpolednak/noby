94bc7d289df1fed077ab64d685081bc5af976056
David Martínek <david.martinek@mpss.cz>
2023-05-21 16:25:06 +0200

    CS - IdentifiedSubject - improved http BadRequest handling

---
+++ DomainServices/CustomerService/ExternalServices.IdentifiedSubjectBr/V1/Dto/IdentifiedSubjectValidationError.cs
@@ -0,0 +1,22 @@
+﻿namespace DomainServices.CustomerService.ExternalServices.IdentifiedSubjectBr.V1.Dto;
+
+public class IdentifiedSubjectValidationError
+{
+    public required string Message { get; init; }
+
+    public required ErrorDetail Detail { get; init; }
+
+    public class ErrorDetail
+    {
+        public ViolatedConstraint[]? ViolatedConstraints { get; init; }
+    }
+
+    public class ViolatedConstraint
+    {
+        public string? Attribute { get; init; }
+
+        public string? InvalidValue { get; init; }
+
+        public string? Message { get; init; }
+    }
+}
