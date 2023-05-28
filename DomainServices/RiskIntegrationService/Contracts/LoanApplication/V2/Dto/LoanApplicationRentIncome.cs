c06290e0f2053ad96ada30ed5fffebf2f13b7b6a
Martin Heimlich <martin.heimlich@mpss.cz>
2023-05-28 23:46:32 +0200

    no message

--- DomainServices/RiskIntegrationService/Contracts/LoanApplication/V2/Dto/LoanApplicationRentIncome.cs
+++ DomainServices/RiskIntegrationService/Contracts/LoanApplication/V2/Dto/LoanApplicationRentIncome.cs
@@ -4,11 +4,14 @@
 public sealed class LoanApplicationRentIncome
 {
     [ProtoMember(1)]
-    public bool IsDomicile { get; set; }
+    public Shared.BankAccountDetail? BankAccount { get; set; }
 
     [ProtoMember(2)]
-    public int? ProofTypeId { get; set; }
+    public bool IsDomicile { get; set; }
 
     [ProtoMember(3)]
+    public int? ProofTypeId { get; set; }
+
+    [ProtoMember(4)]
     public Shared.AmountDetail? MonthlyIncomeAmount { get; set; }
 }
