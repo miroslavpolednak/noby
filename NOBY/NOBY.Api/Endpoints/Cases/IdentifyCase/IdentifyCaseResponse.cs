e0226077d76d50ab4ce0d0ae6acd188fd21ee048
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-05-30 15:16:21 +0200

    fix: feapi identifyCase account validation

--- NOBY/NOBY.Api/Endpoints/Cases/IdentifyCase/IdentifyCaseResponse.cs
+++ NOBY/NOBY.Api/Endpoints/Cases/IdentifyCase/IdentifyCaseResponse.cs
@@ -6,6 +6,10 @@ namespace NOBY.Api.Endpoints.Cases.IdentifyCase;
 
 public sealed class IdentifyCaseResponse
 {
+    /// <summary>
+    /// ID obchodního případu
+    /// </summary>
+    /// <example>24777</example>
     public long? CaseId { get; set; }
     
     public WorkflowTask? Task { get; set; }
