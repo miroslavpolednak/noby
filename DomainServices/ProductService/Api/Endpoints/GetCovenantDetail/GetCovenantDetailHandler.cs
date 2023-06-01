38f99492588f42f77580fb9facb91c3f3c522953
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-06-01 14:17:53 +0200

    wip: getCovenantDetail and List skeleton

---
+++ DomainServices/ProductService/Api/Endpoints/GetCovenantDetail/GetCovenantDetailHandler.cs
@@ -0,0 +1,11 @@
+﻿using DomainServices.ProductService.Contracts;
+
+namespace DomainServices.ProductService.Api.Endpoints.GetCovenantDetail;
+
+internal sealed class GetCovenantDetailHandler : IRequestHandler<GetCovenantDetailRequest, GetCovenantDetailResponse>
+{
+    public Task<GetCovenantDetailResponse> Handle(GetCovenantDetailRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DomainServices/ProductService/Api/Endpoints/GetCovenantList/GetCovenantListHandler.cs
@@ -0,0 +1,11 @@
+﻿using DomainServices.ProductService.Contracts;
+
+namespace DomainServices.ProductService.Api.Endpoints.GetCovenantList;
+
+internal sealed class GetCovenantListHandler : IRequestHandler<GetCovenantListRequest, GetCovenantListResponse>
+{
+    public Task<GetCovenantListResponse> Handle(GetCovenantListRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
