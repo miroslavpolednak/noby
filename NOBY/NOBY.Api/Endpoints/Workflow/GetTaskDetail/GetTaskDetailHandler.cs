2d073a5d99db4d459c35b94831298a9ddc3b3add
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-06-05 14:09:51 +0200

    feature: feapi get task detail allowed task types

--- NOBY/NOBY.Api/Endpoints/Workflow/GetTaskDetail/GetTaskDetailHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Workflow/GetTaskDetail/GetTaskDetailHandler.cs
@@ -6,13 +6,19 @@ internal sealed class GetTaskDetailHandler : IRequestHandler<GetTaskDetailReques
 {
     private readonly Infrastructure.Services.WorkflowTask.IWorkflowTaskService _workflowTaskService;
     private readonly ICaseServiceClient _caseService;
-
+    private static int[] _allowedTaskTypeIds = { 1, 2, 3, 6, 7 };
+    
     public async Task<GetTaskDetailResponse> Handle(GetTaskDetailRequest request, CancellationToken cancellationToken)
     {
         var taskList = await _caseService.GetTaskList(request.CaseId, cancellationToken);
         var task = taskList.FirstOrDefault(t => t.TaskId == request.TaskId)
             ?? throw new CisNotFoundException(90001, $"Task {request.TaskId} not found.");
 
+        if (!_allowedTaskTypeIds.Contains(task.TaskTypeId))
+        {
+            throw new CisAuthorizationException();
+        }
+        
         var (taskDto, taskDetailDto, documents) = await _workflowTaskService.GetTaskDetail(request.CaseId, task.TaskIdSb, cancellationToken);
 
         return new GetTaskDetailResponse
