570c6ac6114476f2eda86e1a0aa4e382463f0593
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-06-09 09:15:03 +0200

    feature: identify case - no error when not found

--- NOBY/NOBY.Api/Endpoints/Cases/IdentifyCase/IdentifyCaseHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Cases/IdentifyCase/IdentifyCaseHandler.cs
@@ -28,14 +28,19 @@ internal sealed class IdentifyCaseHandler : IRequestHandler<IdentifyCaseRequest,
         var documentListResponse = await _documentArchiveServiceClient.GetDocumentList(documentListRequest, cancellationToken);
 
         var document = documentListResponse.Metadata.FirstOrDefault();
-        var caseId = document?.CaseId ?? throw new NobyValidationException("CaseId does not exist.");
+        var caseId = document?.CaseId;
 
-        if (!await _caseServiceClient.ValidateCaseId(caseId, false, cancellationToken))
+        if (caseId == null)
+        {
+            return new IdentifyCaseResponse();
+        }
+
+        if (!await _caseServiceClient.ValidateCaseId(caseId.Value, false, cancellationToken))
         {
             return new IdentifyCaseResponse { CaseId = null };
         }
 
-        var taskList = await _caseServiceClient.GetTaskList(caseId, cancellationToken);
+        var taskList = await _caseServiceClient.GetTaskList(caseId.Value, cancellationToken);
         var taskSubList = taskList.Where(t => t.TaskTypeId == 6).ToList();
 
         if (!taskSubList.Any())
@@ -61,7 +66,7 @@ internal sealed class IdentifyCaseHandler : IRequestHandler<IdentifyCaseRequest,
         }
 
         var taskId = taskDetails.First().Key;
-        var taskDetailRequest = new Workflow.GetTaskDetail.GetTaskDetailRequest(caseId, taskId);
+        var taskDetailRequest = new Workflow.GetTaskDetail.GetTaskDetailRequest(caseId.Value, taskId);
         var taskDetailResponse = await _mediator.Send(taskDetailRequest, cancellationToken);
 
         return new IdentifyCaseResponse
@@ -76,9 +81,16 @@ internal sealed class IdentifyCaseHandler : IRequestHandler<IdentifyCaseRequest,
     {
         var paymentAccount = new PaymentAccountObject { Prefix = account.Prefix, AccountNumber = account.Number };
         var request = new GetCaseIdRequest { PaymentAccount = paymentAccount };
-        var response = await _productServiceClient.GetCaseId(request, cancellationToken);
         
-        return await HandleByCaseId(response.CaseId, cancellationToken);
+        try
+        {
+            var response = await _productServiceClient.GetCaseId(request, cancellationToken);
+            return await HandleByCaseId(response.CaseId, cancellationToken);
+        }
+        catch (CisNotFoundException)
+        {
+            return new IdentifyCaseResponse();
+        }
     }
     
     private async Task<IdentifyCaseResponse> HandleByCaseId(long caseId, CancellationToken cancellationToken)
@@ -91,9 +103,15 @@ internal sealed class IdentifyCaseHandler : IRequestHandler<IdentifyCaseRequest,
     {
         var contract = new ContractNumberObject { ContractNumber = contractNumber };
         var request = new GetCaseIdRequest { ContractNumber = contract };
-        var response = await _productServiceClient.GetCaseId(request, cancellationToken);
-
-        return await HandleByCaseId(response.CaseId, cancellationToken);
+        try
+        {
+            var response = await _productServiceClient.GetCaseId(request, cancellationToken);
+            return await HandleByCaseId(response.CaseId, cancellationToken);
+        }
+        catch (CisNotFoundException)
+        {
+            return new IdentifyCaseResponse();
+        }
     }
 
     private readonly IMediator _mediator;
