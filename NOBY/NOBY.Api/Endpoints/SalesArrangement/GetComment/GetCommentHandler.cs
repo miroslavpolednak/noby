842044d4d4ed6bd1d37d9de16f387d9c5dfc5d82
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-06-12 15:01:33 +0200

    refactoring: feapi get/update comment

--- NOBY/NOBY.Api/Endpoints/SalesArrangement/GetComment/GetCommentHandler.cs
+++ NOBY/NOBY.Api/Endpoints/SalesArrangement/GetComment/GetCommentHandler.cs
@@ -1,4 +1,5 @@
-﻿using CIS.Foms.Types.Enums;
+﻿using CIS.Foms.Enums;
+using DomainServices.CodebookService.Clients;
 using DomainServices.SalesArrangementService.Clients;
 using NOBY.Api.Endpoints.SalesArrangement.Dto;
 
@@ -7,18 +8,24 @@ namespace NOBY.Api.Endpoints.SalesArrangement.GetComment;
 internal sealed  class GetCommentHandler : IRequestHandler<GetCommentRequest, Comment>
 {
     private readonly ISalesArrangementServiceClient _salesArrangementService;
+    private readonly ICodebookServiceClient _codebookService;
     
     public async Task<Comment> Handle(GetCommentRequest request, CancellationToken cancellationToken)
     {
         var salesArrangement = await _salesArrangementService.GetSalesArrangement(request.SalesArrangementId, cancellationToken);
+        var salesArrangementTypes = await _codebookService.SalesArrangementTypes(cancellationToken);
+        var salesArrangementType = salesArrangementTypes.Single(t => t.Id == salesArrangement.SalesArrangementTypeId);
         
-        return (SalesArrangementTypes)salesArrangement.SalesArrangementTypeId == SalesArrangementTypes.Mortgage
+        return salesArrangementType.SalesArrangementCategory == (int)SalesArrangementCategories.ProductRequest
             ? new Comment { Text = salesArrangement.Mortgage.Comment }
             : throw new NobyValidationException(90001);
     }
     
-    public GetCommentHandler(ISalesArrangementServiceClient salesArrangementService)
+    public GetCommentHandler(
+        ISalesArrangementServiceClient salesArrangementService,
+        ICodebookServiceClient codebookService)
     {
         _salesArrangementService = salesArrangementService;
+        _codebookService = codebookService;
     }
 }
