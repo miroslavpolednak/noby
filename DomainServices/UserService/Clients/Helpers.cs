433a326cf85d3e38ebc33f953d71a156d4a30edb
filip-tuma <filip@filiptuma.com>
2023-06-15 12:17:34 +0200

    User svc optimalization

--- DomainServices/UserService/Clients/Helpers.cs
+++ DomainServices/UserService/Clients/Helpers.cs
@@ -3,7 +3,8 @@
 internal static class Helpers
 {
     public static string CreateUserCacheKey(string userId)
-    {
-        return $"CisUser:{userId}";
-    }
+        => $"CisUser:{userId}";
+
+    public static string CreateUserPermissionsCacheKey(int userId)
+        => $"CisUser:{userId}:Permissions";
 }
