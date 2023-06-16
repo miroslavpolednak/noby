5b27047886256b1c9ef29ebdd8f5e0693ab90200
E_ZSIBLI <zdenek_siblik@kb.cz>
2023-06-16 17:59:09 +0200

    NOJIRA: database creation scripts for PREPROD.

---
+++ DomainServices/SalesArrangementService/Database/Scripts/PREPROD/0_create_database-SalesArrangementService.sql
@@ -0,0 +1,116 @@
+USE [master]
+GO
+
+/****** Object:  Database [SalesArrangementService]    Script Date: 05.04.2022 19:29:47 ******/
+CREATE DATABASE [SalesArrangementService]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'SalesArrangementService', FILENAME = N'R:\Data_Bronze_12_PP\SalesArrangementService.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'SalesArrangementService_log', FILENAME = N'R:\Data_Silver_07_PP\SalesArrangementService_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [SalesArrangementService].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [SalesArrangementService] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [SalesArrangementService] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [SalesArrangementService] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [SalesArrangementService] SET QUERY_STORE = OFF
+GO
+
+ALTER DATABASE [SalesArrangementService] SET  READ_WRITE 
+GO
+
+
