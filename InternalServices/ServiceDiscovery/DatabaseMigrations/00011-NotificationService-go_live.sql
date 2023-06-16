bf32df1e6790d7f41615c3380dbe9c599f3ce82d
E_ZSIBLI <zdenek_siblik@kb.cz>
2023-06-16 20:40:42 +0200

    NOJIRA: DB configuration for ServiceDiscovery, CodebookService and NotificationService for PROD environment.

---
+++ InternalServices/ServiceDiscovery/DatabaseMigrations/00011-NotificationService-go_live.sql
@@ -0,0 +1,10 @@
+INSERT [dbo].[ServiceDiscovery] ([EnvironmentName], [ServiceName], [ServiceUrl], [ServiceType]) VALUES (N'PROD', N'CIS:NotificationService', N'https://ds-notification-prod.vsskb.cz:39015', 1)
+GO
+INSERT [dbo].[ServiceDiscovery] ([EnvironmentName], [ServiceName], [ServiceUrl], [ServiceType]) VALUES (N'PROD', N'CIS:NotificationService', N'https://ds-notification-prod.vsskb.cz:39016', 2)
+GO
+INSERT [dbo].[ServiceDiscovery] ([EnvironmentName], [ServiceName], [ServiceUrl], [ServiceType]) VALUES (N'PROD', N'CIS:ServiceDiscovery', N'https://ds-discovery-prod.vsskb.cz:39000', 1)
+GO
+INSERT [dbo].[ServiceDiscovery] ([EnvironmentName], [ServiceName], [ServiceUrl], [ServiceType]) VALUES (N'PROD', N'DS:CodebookService', N'https://ds-codebook-prod.vsskb.cz:39003', 1)
+GO
+INSERT [dbo].[ServiceDiscovery] ([EnvironmentName], [ServiceName], [ServiceUrl], [ServiceType]) VALUES (N'PROD', N'DS:CodebookService', N'https://ds-codebook-prod.vsskb.cz:39002', 2)
+GO
