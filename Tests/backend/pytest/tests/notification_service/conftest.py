41adc8c2ad296241844e2c34e0c65c0c75a01ee2
Mikel Marek EX <992474q@vsskb.cz>
2023-06-20 12:09:58 +0200

    update urls

--- Tests/backend/pytest/tests/notification_service/conftest.py
+++ Tests/backend/pytest/tests/notification_service/conftest.py
@@ -13,8 +13,8 @@ Base = declarative_base()
 
 URLS = {
     "dev_url": "https://ds-notification-dev.vsskb.cz:30016",
+    "fat_url": "https://ds-notification-fat.vsskb.cz:31016",
     "sit_url": "https://ds-notification-sit1.vsskb.cz:32016",
-    "fat_url": "https://ds-notification-sit1.vsskb.cz:31016",
     "uat_url": "https://ds-notification-uat.vsskb.cz:33016",
 }
 
