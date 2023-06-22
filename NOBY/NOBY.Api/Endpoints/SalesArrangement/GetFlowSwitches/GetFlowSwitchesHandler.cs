3a5fc274a08530dec9e8ffbed1b5f925a8b26b6f
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-06-22 12:54:29 +0200

    feature: flow switches signing section false if documents are not signed

--- NOBY/NOBY.Api/Endpoints/SalesArrangement/GetFlowSwitches/GetFlowSwitchesHandler.cs
+++ NOBY/NOBY.Api/Endpoints/SalesArrangement/GetFlowSwitches/GetFlowSwitchesHandler.cs
@@ -30,6 +30,17 @@ internal sealed class GetFlowSwitchesHandler
             response.ModelationSection.IsCompleted = response.ModelationSection.IsCompleted && !existingSwitches.Any(t => t.FlowSwitchId == (int)FlowSwitches.IsWflTaskForIPNotApproved && t.Value);
         }
 
+        if (response.SigningSection.IsCompleted)
+        {
+            var documentsToSignListResponse = await _documentOnSaService.GetDocumentsToSignList(request.SalesArrangementId, cancellationToken);
+            var allDocumentSigned = documentsToSignListResponse.DocumentsOnSAToSign.All(d => d.IsSigned);
+
+            if (!allDocumentSigned)
+            {
+                response.SigningSection.IsCompleted = false;
+            }
+        }
+        
         return response;
     }
 
@@ -53,12 +64,15 @@ internal sealed class GetFlowSwitchesHandler
 
     private readonly DomainServices.SalesArrangementService.Clients.ISalesArrangementServiceClient _salesArrangementService;
     private readonly Infrastructure.Services.FlowSwitches.IFlowSwitchesService _flowSwitches;
+    private readonly DomainServices.DocumentOnSAService.Clients.IDocumentOnSAServiceClient _documentOnSaService;
 
     public GetFlowSwitchesHandler(
         DomainServices.SalesArrangementService.Clients.ISalesArrangementServiceClient arrangementServiceClient,
-        Infrastructure.Services.FlowSwitches.IFlowSwitchesService flowSwitches)
+        Infrastructure.Services.FlowSwitches.IFlowSwitchesService flowSwitches,
+        DomainServices.DocumentOnSAService.Clients.IDocumentOnSAServiceClient documentOnSaService)
     {
         _flowSwitches = flowSwitches;
+        _documentOnSaService = documentOnSaService;
         _salesArrangementService = arrangementServiceClient;
     }
 }
