73e812346acc9ddd1b6643de4c396a4bdb793355
filip-tuma <filip@filiptuma.com>
2023-06-28 18:27:30 +0200

    HFICH-6797

--- DomainServices/ProductService/Api/Database/Entities/Loan.cs
+++ DomainServices/ProductService/Api/Database/Entities/Loan.cs
@@ -69,6 +69,8 @@ internal class Loan
 
 	public string? PredcisliUctu { get; set; }
 
+    public int? HuVypisFrekvence { get; set; }
+
     [Column(TypeName = "decimal(16, 4)")]
     [Precision(16, 4)]
     public decimal? Jistina { get; set; }
@@ -102,119 +104,3 @@ internal class Loan
 	public string? VypisEmail1 { get; set; }
     public string? VypisEmail2 { get; set; }
 }
-
-/*
- * 
-partnerId
-id
-CisloSmlouvy
-VyseUveru
-RadnaSazba
-DelkaFixaceUrokoveSazby
-TypUveru
-MesicniSplatka
--
-ZustatekCelkem
-DatumKonceCerpani
--
--
--
-"tabulka dbo.VztahUver
-
-PartnerId"
-"tabulka dbo.VztahUver
-
-VztahId"
-DatumUzavreniSmlouvy
-DatumFixaceUrokoveSazby
-ZbyvaCerpat
-Jistina
-DruhUveru
-ZustatekCelkem
-DatumPrvniVyplatyZUveru
-DatumPredpSplatnosti DatumZbytkoveSplCelkem
-CPM
-PoradceId
-
-US_DOHODNUTA
-US_DOHODNUTA_DATUM_OD
-PERIODA_FIXACE_DOHODNUTA
-
-
-[Audit_Id][bigint] IDENTITY(1,1) NOT NULL,
-	[Audit_Date_Change] [datetime]
-NOT NULL,
-	[Id] [bigint]
-NOT NULL,
-	[CisloSmlouvy] [varchar] (10) NULL,
-	[SporeniId][bigint] NULL,
-	[ZbyvaCerpat][decimal] (16, 4) NULL,
-	[DatumPosledniVyplatyZUveru][datetime] NULL,
-	[DatumUzavreniSmlouvy][datetime] NULL,
-	[DatumSchvaleniBankou][datetime] NULL,
-	[PartnerId][bigint] NULL,
-	[DatumOtevreniUctu][datetime] NULL,
-	[DatumZruseniVyporadani][datetime] NULL,
-	[JizVycerpanaCastka][decimal] (16, 4) NULL,
-	[DatumFixaceUrokoveSazby][datetime] NULL,
-	[MesicniSplatka][decimal] (16, 4) NULL,
-	[VyseUveru][decimal] (16, 4) NULL,
-	[RadnaSazba][decimal] (16, 4) NULL,
-	[DatumZavedeniDoSystemu][datetime] NULL,
-	[PoradceId][bigint] NULL,
-	[PoradceOrigId][bigint] NULL,
-	[DelkaFixaceUrokoveSazby][smallint] NULL,
-	[TypUveru]
-[smallint]
-NOT NULL,
-	[ZustatekCelkem] [decimal] (16, 4) NULL,
-	[AkceUveruId][int] NULL,
-	[DruhUveru][int] NULL,
-	[PredpDatum1Cerpani][datetime] NULL,
-	[ZahajitCerpaniDo][datetime] NULL,
-	[DatumSkutecnehoSplaceni][datetime] NULL,
-	[UcelUveruId][int] NULL,
-	[CisloUctu][varchar] (10) NULL,
-	[PredcisliUctu][varchar] (6) NULL,
-	[DatumPrvniVyplatyZUveru][datetime] NULL,
-	[DatumPredpSplatnosti][datetime] NULL,
-	[DatumZbytkoveSplatnosti][datetime] NULL,
-	[DatumZbytkoveSplCelkem][datetime] NULL,
-	[DatumKonceCerpani][datetime] NULL,
-	[DatumDosporovaniOd][datetime] NULL,
-	[MesicniDosporovani][decimal] (16, 4) NULL,
-	[CerpaniUveruDo][date] NULL,
-	[CisloSporicihoUctu][varchar] (10) NULL,
-	[DatumPodpisuPrvniZadosti][date] NULL,
-	[DatumSkenovaniZadosti][date] NULL,
-	[DatumZapisu][datetime2] (7) NULL,
-	[PodstavNavrhu][smallint] NULL,
-	[PodstavNavrhuNazov][nvarchar] (50) NULL,
-	[StavNavrhu][smallint] NULL,
-	[StavNavrhuNazov][nvarchar] (50) NULL,
-	[ZmNavrhId][bigint] NULL,
-	[ZdrojDat][tinyint] NULL,
-	[MpTransportId][int] NULL,
-	[DatumOdpisu][date] NULL,
-	[DatumZesplatneni][date] NULL,
-	[DuvodVyporadaniId][int] NULL,
-	[OnlineCerpaniMpHome][bit] NULL,
-	[DatumPosledniMilnik][date] NULL,
-	[EtlChecksum]
-[int]
-NOT NULL,
-	[EtlDataSource] [tinyint]
-NOT NULL,
-	[EtlDataSourceSpec] [tinyint]
-NOT NULL,
-	[EtlModified] [datetime2] (7) NULL,
-	[EtlRunId]
-[int]
-NOT NULL,
-	[PredpocUrokyRadny] [decimal] (16, 4) NULL,
-	[MpHomeIsActive][bit] NULL,
-	[OcekavanaSplatka][decimal] (18, 2) NULL,
-	[OcekavanaSplatkaDosporovani][decimal] (18, 2) NULL,
-	[TrestanoPredplaceno][decimal] (18, 2) NULL,
-	[NedoplatekDosporovani][decimal] (18, 2) NULL,
-*/
