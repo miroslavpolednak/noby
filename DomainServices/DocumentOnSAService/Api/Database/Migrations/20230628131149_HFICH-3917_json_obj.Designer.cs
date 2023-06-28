2975b223df6a96851407569a90055c4e9d4a2088
Polednak Miroslav <miroslav.polednak@mpss.cz>
2023-06-28 16:24:31 +0200

    Job done

--- DomainServices/DocumentOnSAService/Api/Database/Migrations/20230628131149_HFICH-3917_json_obj.Designer.cs
+++ DomainServices/DocumentOnSAService/Api/Database/Migrations/20230628131149_HFICH-3917_json_obj.Designer.cs
@@ -4,6 +4,7 @@ using DomainServices.DocumentOnSAService.Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DomainServices.DocumentOnSAService.Api.Database.Migrations
 {
     [DbContext(typeof(DocumentOnSAServiceDbContext))]
-    partial class DocumentOnSAServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230628131149_HFICH-3917_json_obj")]
+    partial class HFICH3917_json_obj
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
