5a17d0d1296f399f205dd57452b06ddf343f77e2
filip-tuma <filip@filiptuma.com>
2023-06-30 21:12:31 +0200

    no message

---
+++ NOBY/NOBY.Api/Endpoints/RealEstateValuation/PatchDeveloperOnRealEstateValuation/PatchDeveloperOnRealEstateValuationHandler.cs
@@ -0,0 +1,43 @@
+﻿using DomainServices.RealEstateValuationService.Clients;
+
+namespace NOBY.Api.Endpoints.RealEstateValuation.PatchDeveloperOnRealEstateValuation;
+
+internal sealed class PatchDeveloperOnRealEstateValuationHandler
+    : IRequestHandler<PatchDeveloperOnRealEstateValuationRequest>
+{
+    public async Task Handle(PatchDeveloperOnRealEstateValuationRequest request, CancellationToken cancellationToken)
+    {
+        var instance = await _realEstateValuationService.GetRealEstateValuationDetail(request.RealEstateValuationId, cancellationToken);
+
+        // podvrhnute caseId
+        if (instance.RealEstateValuationGeneralDetails.CaseId != request.CaseId
+            || !instance.RealEstateValuationGeneralDetails.DeveloperAllowed)
+        {
+            throw new CisAuthorizationException();
+        }
+
+        int? valuationStateId = null;
+
+        if (instance.RealEstateValuationGeneralDetails.ValuationStateId == 7)
+        {
+            valuationStateId = request.DeveloperApplied ? 4 : 7;
+        }
+        else if (instance.RealEstateValuationGeneralDetails.ValuationStateId == 4 && instance.RealEstateValuationGeneralDetails.DeveloperApplied && !request.DeveloperApplied)
+        {
+            valuationStateId = 7;
+        }
+        else
+        {
+            throw new CisAuthorizationException();
+        }
+
+        await _realEstateValuationService.PatchDeveloperOnRealEstateValuation(request.RealEstateValuationId, valuationStateId.Value, request.DeveloperApplied, cancellationToken);
+    }
+
+    private readonly IRealEstateValuationServiceClient _realEstateValuationService;
+
+    public PatchDeveloperOnRealEstateValuationHandler(IRealEstateValuationServiceClient realEstateValuationService)
+    {
+        _realEstateValuationService = realEstateValuationService;
+    }
+}
---
+++ NOBY/NOBY.Api/Endpoints/RealEstateValuation/PatchDeveloperOnRealEstateValuation/PatchDeveloperOnRealEstateValuationRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NOBY.Api.Endpoints.RealEstateValuation.PatchDeveloperOnRealEstateValuation;
+
+/// <summary>
+/// Editovatelné atributy Ocenění nemovitosti
+/// </summary>
+public sealed class PatchDeveloperOnRealEstateValuationRequest
+    : IRequest
+{
+    [JsonIgnore]
+    internal long CaseId;
+
+    [JsonIgnore]
+    internal int RealEstateValuationId;
+
+    /// <summary>
+    /// True pokud je aplikované hromadné ocenění z developerského projektu
+    /// </summary>
+    public bool DeveloperApplied { get; set; }
+
+    internal PatchDeveloperOnRealEstateValuationRequest InfuseId(long caseId, int realEstateValuationId)
+    {
+        this.RealEstateValuationId = realEstateValuationId;
+        this.CaseId = caseId;
+        return this;
+    }
+}
