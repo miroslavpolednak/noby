16ce18f1adbf7caa56d53cb0ddb1994478b8821f
filip-tuma <filip@filiptuma.com>
2023-07-18 15:18:12 +0200

    audit logger

--- CIS/Infrastructure.Telemetry/AuditLog/Database/DatabaseWriter.cs
+++ CIS/Infrastructure.Telemetry/AuditLog/Database/DatabaseWriter.cs
@@ -6,6 +6,9 @@ namespace CIS.Infrastructure.Telemetry.AuditLog.Database;
 internal sealed class DatabaseWriter
 {
     private const string _insertSql = "INSERT INTO dbo.AuditEvent (EventID, AuditEventTypeId, Detail) VALUES (@EventID, @AuditEventTypeId, @Detail)";
+
+    private const string _nextSequenceId = "SELECT NEXT VALUE FOR dbo.NobyLoggerSequence";
+
     private readonly string _connectionString;
 
     internal DatabaseWriter(string connectionString)
@@ -18,6 +21,14 @@ internal sealed class DatabaseWriter
         using (var connection = new SqlConnection(_connectionString))
         {
             connection.Execute(_insertSql, eventObject);
+        }
+    }
+
+    internal long GetSequenceId()
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            return connection.QuerySingle<long>(_nextSequenceId);
         }
     }
 }
