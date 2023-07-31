6b7a14b341320bd1d5d05742fc9b13e041666c68
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-07-31 16:23:35 +0200

    wip: update document status job

---
+++ DomainServices/DocumentOnSAService/Api/BackgroundServices/UpdateDocumentStatus/UpdateDocumentStatusJob.cs
@@ -0,0 +1,56 @@
+﻿using CIS.Foms.Enums;
+using CIS.Foms.Types.Enums;
+using DomainServices.DocumentOnSAService.Api.Database;
+using DomainServices.DocumentOnSAService.Contracts;
+using ExternalServices.ESignatures.V1;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomainServices.DocumentOnSAService.Api.BackgroundServices.UpdateDocumentStatus;
+
+public sealed class UpdateDocumentStatusJob
+    : CIS.Infrastructure.BackgroundServices.ICisBackgroundServiceJob
+{
+    private readonly IMediator _mediator;
+    private readonly DocumentOnSAServiceDbContext _dbContext;
+    private readonly IESignaturesClient _eSignaturesClient;
+
+    public UpdateDocumentStatusJob(
+        IMediator mediator,
+        DocumentOnSAServiceDbContext dbContext,
+        IESignaturesClient eSignaturesClient)
+    {
+        _mediator = mediator;
+        _dbContext = dbContext;
+        _eSignaturesClient = eSignaturesClient;
+    }
+    
+    public async Task ExecuteJobAsync(CancellationToken cancellationToken)
+    {
+        var documentOnSas = await _dbContext.DocumentOnSa
+            .Where(s => s.SignatureTypeId == (int)SignatureTypes.Electronic && s.IsValid && !s.IsSigned && !s.IsFinal)
+            .ToListAsync(cancellationToken);
+
+        foreach (var documentOnSa in documentOnSas)
+        {
+            var status = await _eSignaturesClient.GetDocumentStatus(documentOnSa.ExternalId, cancellationToken);
+
+            if (status is EDocumentStatuses.SIGNED or EDocumentStatuses.VERIFIED or EDocumentStatuses.SENT)
+            {
+                var signRequest = new SignDocumentRequest
+                {
+                    DocumentOnSAId = documentOnSa.DocumentOnSAId,
+                    SignatureTypeId = (int)SignatureTypes.Electronic
+                };
+                
+                await _mediator.Send(signRequest, cancellationToken);
+            }
+
+            if (status == EDocumentStatuses.DELETED)
+            {
+                documentOnSa.IsValid = false;
+            }
+        }
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
