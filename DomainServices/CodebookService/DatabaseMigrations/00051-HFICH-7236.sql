acc71fa0d7683afabd134f6dc5d84bb2e95eb1a1
David Martínek <david.martinek@mpss.cz>
2023-08-01 12:48:17 +0200

    HFICH-7236 script fixed

--- DomainServices/CodebookService/DatabaseMigrations/00051-HFICH-7236.sql
+++ DomainServices/CodebookService/DatabaseMigrations/00051-HFICH-7236.sql
@@ -3,3 +3,7 @@ ALTER TABLE [dbo].[SalesArrangementType] ADD IsFormSentToCmp BIT NOT NULL DEFAUL
 GO
 
 UPDATE [dbo].[SalesArrangementType] SET IsFormSentToCmp = 1 WHERE Id IN (1, 6, 10, 11, 12)
+
+UPDATE [dbo].[SqlQuery] 
+SET SqlQueryText = 'SELECT Id, Name, SalesArrangementCategory, Description, IsFormSentToCmp FROM [dbo].[SalesArrangementType] ORDER BY Id'
+WHERE SqlQueryId = 'SalesArrangementTypes'
