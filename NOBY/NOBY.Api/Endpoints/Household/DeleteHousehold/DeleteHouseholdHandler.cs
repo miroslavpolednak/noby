4e2d8724e4804661b093080490d432e7bff55f74
filip-tuma <filip@filiptuma.com>
2023-08-03 13:46:27 +0200

    HFICH-7265

--- NOBY/NOBY.Api/Endpoints/Household/DeleteHousehold/DeleteHouseholdHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Household/DeleteHousehold/DeleteHouseholdHandler.cs
@@ -1,5 +1,7 @@
 ﻿using CIS.Foms.Enums;
 using DomainServices.HouseholdService.Clients;
+using DomainServices.HouseholdService.Contracts;
+using DomainServices.ProductService.Clients;
 using DomainServices.SalesArrangementService.Clients;
 
 namespace NOBY.Api.Endpoints.Household.DeleteHousehold;
@@ -10,9 +12,14 @@ internal sealed class DeleteHouseholdHandler
     public async Task<int> Handle(DeleteHouseholdRequest request, CancellationToken cancellationToken)
     {
         var household = await _householdService.GetHousehold(request.HouseholdId, cancellationToken);
-
+        
         // smazat
         await _householdService.DeleteHousehold(request.HouseholdId, cancellationToken: cancellationToken);
+
+        // smazat vazbu klient-produkt
+        var customers = await _customerOnSAService.GetCustomerList(household.SalesArrangementId, cancellationToken);
+        await deleteRelationship(household.CustomerOnSAId1, household.CaseId, customers, cancellationToken);
+        await deleteRelationship(household.CustomerOnSAId2, household.CaseId, customers, cancellationToken);
 
         // HFICH-5233
         if (household.HouseholdTypeId == (int)HouseholdTypes.Codebtor)
@@ -29,13 +36,35 @@ internal sealed class DeleteHouseholdHandler
         return request.HouseholdId;
     }
 
+    private async Task deleteRelationship(int? customerOnSA, long caseId, List<CustomerOnSA> customers, CancellationToken cancellationToken)
+    {
+        if (!customerOnSA.HasValue) return;
+
+        var partnerId = customers
+            .First(t => t.CustomerOnSAId == customerOnSA.Value)
+            ?.CustomerIdentifiers
+            .FirstOrDefault(t => t.IdentityScheme == CIS.Infrastructure.gRPC.CisTypes.Identity.Types.IdentitySchemes.Mp)
+            ?.IdentityId;
+
+        if (partnerId.HasValue)
+        {
+            await _productService.DeleteContractRelationship(Convert.ToInt32(partnerId.Value), caseId, cancellationToken);
+        }
+    }
+
+    private readonly IProductServiceClient _productService;
     private readonly ISalesArrangementServiceClient _salesArrangementService;
     private readonly IHouseholdServiceClient _householdService;
+    private readonly ICustomerOnSAServiceClient _customerOnSAService;
     
     public DeleteHouseholdHandler(
+        IProductServiceClient productService,
+        ICustomerOnSAServiceClient customerOnSAService,
         IHouseholdServiceClient householdService, 
         ISalesArrangementServiceClient salesArrangementService)
     {
+        _productService = productService;
+        _customerOnSAService = customerOnSAService;
         _salesArrangementService = salesArrangementService;
         _householdService = householdService;
     }
