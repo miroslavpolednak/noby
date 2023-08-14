564310aa85e436b3c3a1f7819e9610fe29a4c896
David Martínek <david.martinek@mpss.cz>
2023-08-14 15:51:30 +0200

    HFICH-7376 FE API RuianAddress

---
+++ ExternalServices/RuianAddress/V1/RealRuianAddressClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using CIS.Infrastructure.ExternalServicesHelpers;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace ExternalServices.RuianAddress.V1;
+
+internal class RealRuianAddressClient : IRuianAddressClient
+{
+    private readonly HttpClient _httpClient;
+
+    public RealRuianAddressClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<Contracts.AddressDTO> GetAddressDetail(long addressId, CancellationToken cancellationToken)
+    {
+        var response = await _httpClient.GetAsync($"{_httpClient.BaseAddress}/addresses/{addressId}", cancellationToken);
+
+        return await response.EnsureSuccessStatusAndReadJson<Contracts.AddressDTO>(StartupExtensions.ServiceName, cancellationToken);
+    }
+
+    public async Task<ICollection<Contracts.AddressDTO>> FindAddresses(string searchText, int pageSize, CancellationToken cancellationToken)
+    {
+        var queryBuilder = new QueryBuilder
+        {
+            { "name", searchText },
+            { "limit", pageSize.ToString(CultureInfo.InvariantCulture) }
+        };
+
+        var response = await _httpClient.GetAsync(QueryHelpers.AddQueryString($"{_httpClient.BaseAddress}/addresses/find", queryBuilder!), cancellationToken);
+
+        var test = await response.Content.ReadAsStringAsync(cancellationToken);
+
+        var pagedResult = await response.EnsureSuccessStatusAndReadJson<Contracts.AddressDTOPagedResponse>(StartupExtensions.ServiceName, cancellationToken);
+
+        return pagedResult.Items ?? new Collection<Contracts.AddressDTO>();
+    }
+}
--- NOBY/NOBY.Api/Endpoints/Address/GetAddressDetail/GetAddressDetailHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Address/GetAddressDetail/GetAddressDetailHandler.cs
@@ -1,32 +1,32 @@
-﻿namespace NOBY.Api.Endpoints.Address.GetAddressDetail;
+﻿using System.Globalization;
+using DomainServices.CodebookService.Clients;
+using ExternalServices.AddressWhisperer.V1;
+using ExternalServices.RuianAddress.V1;
+
+namespace NOBY.Api.Endpoints.Address.GetAddressDetail;
 
 internal sealed class GetAddressDetailHandler
     : IRequestHandler<GetAddressDetailRequest, GetAddressDetailResponse>
 {
+    private readonly IAddressWhispererClient _addressWhisperer;
+    private readonly IRuianAddressClient _ruianAddress;
+    private readonly ICodebookServiceClient _codebookService;
+
+    public GetAddressDetailHandler(IAddressWhispererClient addressWhisperer, IRuianAddressClient ruianAddress, ICodebookServiceClient codebookService)
+    {
+        _addressWhisperer = addressWhisperer;
+        _ruianAddress = ruianAddress;
+        _codebookService = codebookService;
+    }
+
     public async Task<GetAddressDetailResponse> Handle(GetAddressDetailRequest request, CancellationToken cancellationToken)
     {
-        // zeme z ciselniku
-        string country = (await _codebookService.Countries(cancellationToken)).FirstOrDefault(t => t.Id == request.CountryId)?.ShortName
-            ?? throw new CisValidationException($"Country #{request.CountryId} not found");
+        try
+        {
+            if (request.CountryId == IRuianAddressClient.AllowedCountryId)
+                return await GetRuianAddressDetail(request.AddressId, cancellationToken);
 
-        try 
-        { 
-            var result = await _addressWhispererClient.GetAddressDetail(request.SessionId, request.AddressId, request.Title, country, cancellationToken);
-
-            return result is null ? new GetAddressDetailResponse() : new GetAddressDetailResponse
-            {
-                StreetNumber = result.StreetNumber,
-                City = result.City,
-                CountryId = (await _codebookService.Countries(cancellationToken)).FirstOrDefault(t => t.ShortName == result.Country)?.Id,
-                CityDistrict = result.CityDistrict,
-                HouseNumber = result.HouseNumber,
-                Street = result.Street,
-                PragueDistrict = result.PragueDistrict,
-                Postcode = result.Postcode,
-                EvidenceNumber = result.EvidenceNumber,
-                DeliveryDetails = result.DeliveryDetails,
-                AddressPointId = result.AddressPointId
-            };
+            return await GetAddressWhispererDetail(request, cancellationToken);
         }
         catch
         {
@@ -34,12 +34,48 @@ internal sealed class GetAddressDetailHandler
         }
     }
 
-    private ExternalServices.AddressWhisperer.V1.IAddressWhispererClient _addressWhispererClient;
-    private DomainServices.CodebookService.Clients.ICodebookServiceClient _codebookService;
-
-    public GetAddressDetailHandler(ExternalServices.AddressWhisperer.V1.IAddressWhispererClient addressWhispererClient, DomainServices.CodebookService.Clients.ICodebookServiceClient codebookService)
+    private async Task<GetAddressDetailResponse> GetRuianAddressDetail(string addressId, CancellationToken cancellationToken)
     {
-        _addressWhispererClient = addressWhispererClient;
-        _codebookService = codebookService;
+        var detail = await _ruianAddress.GetAddressDetail(long.Parse(addressId, CultureInfo.InvariantCulture), cancellationToken);
+
+        return new GetAddressDetailResponse
+        {
+            Street = detail.StreetName,
+            StreetNumber = $"{detail.OrientationNumber}{detail.OrientationNumberEndChar}",
+            HouseNumber = detail.HouseNumber.ToString(),
+            Postcode = detail.ZipCode.ToString(CultureInfo.InvariantCulture),
+            City = detail.TownName,
+            CityDistrict = detail.TownPartName,
+            PragueDistrict = detail.MopName,
+            AddressPointId = detail.Id.ToString(CultureInfo.InvariantCulture),
+            KatuzId = (int?)detail.TerritoryId,
+            KatuzTitle = detail.TerritoryName,
+            CountryId = IRuianAddressClient.AllowedCountryId
+        };
+    }
+
+    private async Task<GetAddressDetailResponse> GetAddressWhispererDetail(GetAddressDetailRequest request, CancellationToken cancellationToken)
+    {
+        // zeme z ciselniku
+        string country = (await _codebookService.Countries(cancellationToken)).FirstOrDefault(t => t.Id == request.CountryId)?.ShortName
+                         ?? throw new CisValidationException($"Country #{request.CountryId} not found");
+
+
+        var result = await _addressWhisperer.GetAddressDetail(request.SessionId, request.AddressId, request.Title, country, cancellationToken);
+
+        return result is null ? new GetAddressDetailResponse() : new GetAddressDetailResponse
+        {
+            StreetNumber = result.StreetNumber,
+            City = result.City,
+            CountryId = (await _codebookService.Countries(cancellationToken)).FirstOrDefault(t => t.ShortName == result.Country)?.Id,
+            CityDistrict = result.CityDistrict,
+            HouseNumber = result.HouseNumber,
+            Street = result.Street,
+            PragueDistrict = result.PragueDistrict,
+            Postcode = result.Postcode,
+            EvidenceNumber = result.EvidenceNumber,
+            DeliveryDetails = result.DeliveryDetails,
+            AddressPointId = result.AddressPointId
+        };
     }
 }
