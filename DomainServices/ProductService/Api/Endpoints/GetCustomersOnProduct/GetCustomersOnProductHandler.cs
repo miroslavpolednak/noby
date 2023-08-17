4421f8a02fcbe2e037da2fb023eb7c6897d59d3d
David Martínek <david.martinek@mpss.cz>
2023-08-17 10:26:24 +0200

    HFICH-7483

--- DomainServices/ProductService/Api/Endpoints/GetCustomersOnProduct/GetCustomersOnProductHandler.cs
+++ DomainServices/ProductService/Api/Endpoints/GetCustomersOnProduct/GetCustomersOnProductHandler.cs
@@ -1,24 +1,24 @@
-﻿using DomainServices.ProductService.Contracts;
+﻿using CIS.Foms.Enums;
+using DomainServices.CodebookService.Clients;
+using DomainServices.ProductService.Contracts;
 using Microsoft.EntityFrameworkCore;
 
 namespace DomainServices.ProductService.Api.Endpoints.GetCustomersOnProduct;
 
 internal sealed class GetCustomersOnProductHandler
-    : IRequestHandler<Contracts.GetCustomersOnProductRequest, GetCustomersOnProductResponse>
+    : IRequestHandler<GetCustomersOnProductRequest, GetCustomersOnProductResponse>
 {
-    public async Task<GetCustomersOnProductResponse> Handle(Contracts.GetCustomersOnProductRequest request, CancellationToken cancellation)
+    public async Task<GetCustomersOnProductResponse> Handle(GetCustomersOnProductRequest request, CancellationToken cancellation)
     {
-        // Kontrola, zda se jedná o KB produkt (typ uveru = 3), chyba pokud ne vyhodit chybu
-        var typUveru = (await _dbContext.Loans.AsNoTracking().FirstOrDefaultAsync(t => t.Id == request.ProductId, cancellation))?.TypUveru;
-        if (typUveru == null)
-        {
-            throw ErrorCodeMapper.CreateNotFoundException(ErrorCodeMapper.NotFound12001, request.ProductId);
-        }
-        if (typUveru.Value != 3)
-        {
+        var productTypes = await _codebookService.ProductTypes(cancellation);
+
+        var loan = await _dbContext.Loans.AsNoTracking().FirstOrDefaultAsync(t => t.Id == request.ProductId, cancellation) ??
+                   throw ErrorCodeMapper.CreateNotFoundException(ErrorCodeMapper.NotFound12001, request.ProductId);
+
+        // Kontrola, zda se jedná o KB produkt, chyba pokud ne vyhodit chybu
+        if (!productTypes.Any(p => p.Id == loan.KodProduktyUv && p.MandantId == (int)Mandants.Kb))
             throw ErrorCodeMapper.CreateValidationException(ErrorCodeMapper.InvalidArgument12019);
-        }
-        
+
         // Zjištění seznamu klientů na produktu, vyhodit tvrdou chybu pokud je množina prázdná (nesmí se stávat, pokud není nekonzistence dat)
         var customers = await _dbContext.Relationships
             .AsNoTracking()
@@ -30,12 +30,6 @@ internal sealed class GetCustomersOnProductHandler
         {
             throw ErrorCodeMapper.CreateValidationException(ErrorCodeMapper.InvalidArgument12020);
         }
-
-        if (customers.Any(t => !t.KbId.HasValue))
-        {
-            throw ErrorCodeMapper.CreateValidationException(ErrorCodeMapper.InvalidArgument12021);
-        }
-
 
         var model = new GetCustomersOnProductResponse();
         foreach (var customer in customers)
@@ -56,9 +50,11 @@ internal sealed class GetCustomersOnProductHandler
     }
 
     private readonly Database.ProductServiceDbContext _dbContext;
+    private readonly ICodebookServiceClient _codebookService;
 
-    public GetCustomersOnProductHandler(Database.ProductServiceDbContext dbContext)
+    public GetCustomersOnProductHandler(Database.ProductServiceDbContext dbContext, ICodebookServiceClient codebookService)
     {
         _dbContext = dbContext;
+        _codebookService = codebookService;
     }
 }
