dfefd9e680148d0279e272cba5695ee9174d7776
David Martínek <david.martinek@mpss.cz>
2023-08-17 10:24:27 +0200

    Revert "FE API - SendToCmp fixed KB ID synchronization"
    This reverts commit 2ba768c64db6787212fa9d1ec775f0dad69a8114.

---
+++ NOBY/NOBY.Api/Endpoints/SalesArrangement/SendToCmp/Dto/CustomersData.cs
@@ -0,0 +1,10 @@
+﻿using DomainServices.ProductService.Contracts;
+
+namespace NOBY.Api.Endpoints.SalesArrangement.SendToCmp.Dto;
+
+internal sealed class CustomersData
+{
+    public required ICollection<CustomerOnSaExtended> CustomersOnSa { get; init; }
+
+    public required ICollection<GetCustomersOnProductResponseItem> RedundantCustomersOnProduct { get; init; }
+}
