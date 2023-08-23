1f0e47731240f7e63eef1fe12fa6c8403b2fb47a
Polednak Miroslav <miroslav.polednak@mpss.cz>
2023-08-23 11:40:54 +0200

    Added logic

--- DomainServices/DocumentOnSAService/Api/Endpoints/SignDocument/SignDocumentHandler.cs
+++ DomainServices/DocumentOnSAService/Api/Endpoints/SignDocument/SignDocumentHandler.cs
@@ -3,6 +3,10 @@ using CIS.Core.Security;
 using CIS.Foms.Enums;
 using CIS.Infrastructure.Audit;
 using CIS.Infrastructure.gRPC.CisTypes;
+using DomainServices.CaseService.Clients;
+using DomainServices.CodebookService.Clients;
+using DomainServices.CustomerService.Clients;
+using DomainServices.CustomerService.Contracts;
 using DomainServices.DocumentOnSAService.Api.Common;
 using DomainServices.DocumentOnSAService.Api.Database;
 using DomainServices.DocumentOnSAService.Api.Database.Entities;
@@ -18,6 +22,8 @@ using ExternalServices.Sulm.V1;
 using FastEnumUtility;
 using Google.Protobuf.WellKnownTypes;
 using Microsoft.EntityFrameworkCore;
+using static DomainServices.HouseholdService.Contracts.GetCustomerChangeMetadataResponse.Types;
+using _CustomerService = DomainServices.CustomerService.Contracts;
 
 namespace DomainServices.DocumentOnSAService.Api.Endpoints.SignDocument;
 
@@ -34,6 +40,10 @@ public sealed class SignDocumentHandler : IRequestHandler<SignDocumentRequest, E
     private readonly IProductServiceClient _productService;
     private readonly IAuditLogger _auditLogger;
     private readonly ISalesArrangementStateManager _salesArrangementStateManager;
+    private readonly ICustomerChangeDataMerger _customerChangeDataMerger;
+    private readonly ICustomerServiceClient _customerService;
+    private readonly ICodebookServiceClient _codebookService;
+    private readonly ICaseServiceClient _caseService;
 
     public SignDocumentHandler(
         DocumentOnSAServiceDbContext dbContext,
@@ -46,7 +56,11 @@ public sealed class SignDocumentHandler : IRequestHandler<SignDocumentRequest, E
         ICustomerOnSAServiceClient customerOnSaService,
         IProductServiceClient productService,
         IAuditLogger auditLogger,
-        ISalesArrangementStateManager salesArrangementStateManager)
+        ISalesArrangementStateManager salesArrangementStateManager,
+        ICustomerChangeDataMerger customerChangeDataMerger,
+        ICustomerServiceClient customerService,
+        ICodebookServiceClient codebookService,
+        ICaseServiceClient caseService)
     {
         _dbContext = dbContext;
         _dateTime = dateTime;
@@ -59,6 +73,10 @@ public sealed class SignDocumentHandler : IRequestHandler<SignDocumentRequest, E
         _productService = productService;
         _auditLogger = auditLogger;
         _salesArrangementStateManager = salesArrangementStateManager;
+        _customerChangeDataMerger = customerChangeDataMerger;
+        _customerService = customerService;
+        _codebookService = codebookService;
+        _caseService = caseService;
     }
 
     public async Task<Empty> Handle(SignDocumentRequest request, CancellationToken cancellationToken)
@@ -92,29 +110,27 @@ public sealed class SignDocumentHandler : IRequestHandler<SignDocumentRequest, E
         var houseHold = documentOnSa.HouseholdId.HasValue
             ? await _householdService.GetHousehold(documentOnSa.HouseholdId!.Value, cancellationToken)
             : null;
-        
+
         // SUML call
         if (documentOnSa.DocumentTypeId == DocumentTypes.ZADOSTHU.ToByte()) // 4 
-            await SumlCall(documentOnSa, houseHold!, cancellationToken);
+            await SumlCall(salesArrangement, houseHold!, cancellationToken);
 
-        // set flow switches
-        if (houseHold is not null)
+        if (houseHold is null)
         {
-            var flowSwitchId = houseHold.HouseholdTypeId switch
+            // CRS
+            if (documentOnSa.DocumentTypeId == DocumentTypes.DANRESID.ToByte()) // 13
             {
-                (int)HouseholdTypes.Main => FlowSwitches.Was3601MainChangedAfterSigning,
-                (int)HouseholdTypes.Codebtor => FlowSwitches.Was3602CodebtorChangedAfterSigning,
-                _ => throw new CisValidationException("Unsupported HouseholdType")
-            };
-        
-            await _salesArrangementService.SetFlowSwitches(houseHold.SalesArrangementId, new()
-            {
-                new() { FlowSwitchId = (int)flowSwitchId, Value = false }
-            }, cancellationToken);
+                await ProcessCrsDocumentOnSa(documentOnSa, cancellationToken);
+            }
+        }
+        else
+        {
+            await SetFlowSwitch(houseHold!, cancellationToken);
+            await ProcessDocumentOnSaWithHousehold(salesArrangement, houseHold, documentOnSa, cancellationToken);
         }
 
         await _dbContext.SaveChangesAsync(cancellationToken);
-        
+
         _auditLogger.LogWithCurrentUser(
             AuditEventTypes.Noby007,
             "Dokument byl označen za podepsaný",
@@ -125,7 +141,7 @@ public sealed class SignDocumentHandler : IRequestHandler<SignDocumentRequest, E
                 new("form", documentOnSa.FormId)
             }
         );
-        
+
         // SA state
         if (salesArrangement.State == SalesArrangementStates.InSigning.ToByte())
         {
@@ -135,8 +151,119 @@ public sealed class SignDocumentHandler : IRequestHandler<SignDocumentRequest, E
         {
             throw CIS.Core.ErrorCodes.ErrorCodeMapperBase.CreateValidationException(ErrorCodeMapper.SigningInvalidSalesArrangementState);
         }
-        
+
         return new Empty();
+    }
+
+    private async Task ProcessCrsDocumentOnSa(DocumentOnSa documentOnSa, CancellationToken cancellationToken)
+    {
+        var customerOnSa = await _customerOnSAService.GetCustomer(documentOnSa.CustomerOnSAId1!.Value, cancellationToken);
+
+        var customerChangeMetadata = await GetCustomerOnSaMetadata(documentOnSa, customerOnSa, cancellationToken);
+
+        if (customerChangeMetadata.CustomerChangeMetadata.WasCRSChanged)
+        {
+            var customerDetail = await _customerService.GetCustomerDetail(customerOnSa.CustomerIdentifiers.First(r => r.IdentityScheme == Identity.Types.IdentitySchemes.Kb), cancellationToken);
+            _customerChangeDataMerger.MergeTaxResidence(customerDetail?.NaturalPerson!, customerOnSa);
+            var updateCustomerRequest = MapUpdateCustomerRequest((int)CIS.Foms.Enums.Mandants.Kb, customerDetail!);
+            await _customerService.UpdateCustomer(updateCustomerRequest, cancellationToken);
+            // Throw away locally stored CRS data (keep client changes) 
+            var jsonCustomerChangeDataWithoutCrs = _customerChangeDataMerger.TrowAwayLocallyStoredCrsData(customerOnSa);
+            await _customerOnSAService.UpdateCustomerDetail(MapUpdateCustomerOnSaRequest(customerOnSa, jsonCustomerChangeDataWithoutCrs), cancellationToken);
+        }
+    }
+
+    private async Task ProcessDocumentOnSaWithHousehold(SalesArrangement salesArrangement, Household household, DocumentOnSa documentOnSa, CancellationToken cancellationToken)
+    {
+        var mandantId = await GetMandantId(salesArrangement, cancellationToken);
+
+        if (mandantId != (int)CIS.Foms.Enums.Mandants.Kb)
+        {
+            throw new CisValidationException(90002, $"Mp products not supported (mandant {mandantId})");
+        }
+
+        var customersOnSa = await GetCustomersOnSa(household, cancellationToken);
+        foreach (var customerOnSa in customersOnSa)
+        {
+            var customerChangeMetadata = await GetCustomerOnSaMetadata(documentOnSa, customerOnSa, cancellationToken);
+            if (customerChangeMetadata.CustomerChangeMetadata.WereClientDataChanged)
+            {
+                var customerDetail = await _customerService.GetCustomerDetail(customerOnSa.CustomerIdentifiers.First(r => r.IdentityScheme == Identity.Types.IdentitySchemes.Kb), cancellationToken);
+                _customerChangeDataMerger.MergeClientData(customerDetail, customerOnSa);
+                var updateCustomerRequest = MapUpdateCustomerRequest(mandantId.Value, customerDetail);
+                await _customerService.UpdateCustomer(updateCustomerRequest, cancellationToken);
+                //Throw away locally stored Client data (keep CRS changes) 
+                var jsonCustomerChangeDataWithCrs = _customerChangeDataMerger.TrowAwayLocallyStoredClientData(customerOnSa);
+                await _customerOnSAService.UpdateCustomerDetail(MapUpdateCustomerOnSaRequest(customerOnSa, jsonCustomerChangeDataWithCrs), cancellationToken);
+            }
+        }
+    }
+
+    private async Task<int?> GetMandantId(SalesArrangement salesArrangement, CancellationToken cancellationToken)
+    {
+        var caseDetail = await _caseService.GetCaseDetail(salesArrangement.CaseId, cancellationToken);
+        var productTypes = await _codebookService.ProductTypes(cancellationToken);
+        return productTypes.Single(r => r.Id == caseDetail.Data.ProductTypeId).MandantId;
+    }
+
+    private async Task<List<CustomerOnSA>> GetCustomersOnSa(Household household, CancellationToken cancellationToken)
+    {
+        var customers = new List<CustomerOnSA>();
+        if (household.CustomerOnSAId1 is not null)
+        {
+            customers.Add(await _customerOnSAService.GetCustomer(household.CustomerOnSAId1.Value, cancellationToken));
+        }
+        if (household.CustomerOnSAId2 is not null)
+        {
+            customers.Add(await _customerOnSAService.GetCustomer(household.CustomerOnSAId2.Value, cancellationToken));
+        }
+
+        return customers;
+    }
+
+    private static UpdateCustomerDetailRequest MapUpdateCustomerOnSaRequest(CustomerOnSA customerOnSa, string? customerChangeDataJson)
+    {
+        return new UpdateCustomerDetailRequest
+        {
+            CustomerOnSAId = customerOnSa.CustomerOnSAId,
+            CustomerAdditionalData = customerOnSa.CustomerAdditionalData,
+            CustomerChangeData = customerChangeDataJson
+        };
+    }
+
+    private static _CustomerService.UpdateCustomerRequest MapUpdateCustomerRequest(int mandantId, CustomerDetailResponse customerDetail)
+    {
+        return new _CustomerService.UpdateCustomerRequest
+        {
+            Addresses = { customerDetail.Addresses },
+            Contacts = { customerDetail.Contacts },
+            CustomerIdentification = customerDetail.CustomerIdentification,
+            IdentificationDocument = customerDetail.IdentificationDocument,
+            Identities = { customerDetail.Identities },
+            Mandant = (CIS.Infrastructure.gRPC.CisTypes.Mandants)mandantId,
+            NaturalPerson = customerDetail.NaturalPerson
+        };
+    }
+
+    private async Task<GetCustomerChangeMetadataResponseItem> GetCustomerOnSaMetadata(DocumentOnSa documentOnSa, CustomerOnSA customerOnSa, CancellationToken cancellationToken)
+    {
+        var customersChangeMetadata = await _customerOnSAService.GetCustomerChangeMetadata(documentOnSa.SalesArrangementId, cancellationToken);
+        return customersChangeMetadata!.Single(r => r.CustomerOnSAId == customerOnSa.CustomerOnSAId);
+    }
+
+    private async Task SetFlowSwitch(Household houseHold, CancellationToken cancellationToken)
+    {
+        var flowSwitchId = houseHold.HouseholdTypeId switch
+        {
+            (int)HouseholdTypes.Main => FlowSwitches.Was3601MainChangedAfterSigning,
+            (int)HouseholdTypes.Codebtor => FlowSwitches.Was3602CodebtorChangedAfterSigning,
+            _ => throw new CisValidationException("Unsupported HouseholdType")
+        };
+
+        await _salesArrangementService.SetFlowSwitches(houseHold.SalesArrangementId, new()
+            {
+                new() { FlowSwitchId = (int)flowSwitchId, Value = false }
+            }, cancellationToken);
     }
 
     private async Task UpdateFirstSignatureDate(DateTime signatureDate, SalesArrangement salesArrangement, CancellationToken cancellationToken)
@@ -157,9 +284,8 @@ public sealed class SignDocumentHandler : IRequestHandler<SignDocumentRequest, E
         await _productService.UpdateMortgage(new UpdateMortgageRequest { ProductId = salesArrangement.CaseId, Mortgage = mortgageResponse.Mortgage }, cancellationToken);
     }
 
-    private async Task SumlCall(DocumentOnSa documentOnSa, Household houseHold, CancellationToken cancellationToken)
+    private async Task SumlCall(SalesArrangement salesArrangement, Household houseHold, CancellationToken cancellationToken)
     {
-        var salesArrangement = await _salesArrangementService.GetSalesArrangement(documentOnSa.SalesArrangementId, cancellationToken);
         if (salesArrangement.IsProductSalesArrangement())
         {
             await SumlCallForSpecifiedCustomer(houseHold.CustomerOnSAId1!.Value, cancellationToken);
