d594d88ab6d009af6283e936a151b6971ffab608
filip-tuma <filip@filiptuma.com>
2023-08-27 22:10:09 +0200

    no message

--- DomainServices/HouseholdService/Api/Endpoints/Household/GetHouseholdIdByCustomerOnSAId/GetHouseholdIdByCustomerOnSAIdHandler.cs
+++ DomainServices/HouseholdService/Api/Endpoints/Household/GetHouseholdIdByCustomerOnSAId/GetHouseholdIdByCustomerOnSAIdHandler.cs
@@ -7,8 +7,20 @@ internal sealed class GetHouseholdIdByCustomerOnSAIdHandler
 {
     public async Task<GetHouseholdIdByCustomerOnSAIdResponse> Handle(GetHouseholdIdByCustomerOnSAIdRequest request, CancellationToken cancellationToken)
     {
+        // aby ten dotaz jel nejdriv podle indexu SAid
+        var customer = await _dbContext.Customers
+            .AsNoTracking()
+            .Where(t => t.CustomerOnSAId == request.CustomerOnSAId)
+            .Select(t => new { t.SalesArrangementId })
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (customer is null)
+        {
+            return new GetHouseholdIdByCustomerOnSAIdResponse();
+        }
+
         var model = await _dbContext.Households
-            .Where(t => t.CustomerOnSAId1 == request.CustomerOnSAId || t.CustomerOnSAId2 == request.CustomerOnSAId)
+            .Where(t => t.SalesArrangementId == customer.SalesArrangementId && (t.CustomerOnSAId1 == request.CustomerOnSAId || t.CustomerOnSAId2 == request.CustomerOnSAId))
             .AsNoTracking()
             .Select(t => new { t.HouseholdId })
             .FirstOrDefaultAsync(cancellationToken);
