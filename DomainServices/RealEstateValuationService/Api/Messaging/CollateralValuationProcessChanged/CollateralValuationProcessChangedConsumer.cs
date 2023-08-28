e12cebb0c7fe65e21d0f7e4740a5bc1ab9ef7a0a
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-08-28 14:25:20 +0200

    wip: CollateralValuationProcessChangedConsumer in RealEStateService

--- DomainServices/RealEstateValuationService/Api/Messaging/CollateralValuationProcessChanged/CollateralValuationProcessChangedConsumer.cs
+++ DomainServices/RealEstateValuationService/Api/Messaging/CollateralValuationProcessChanged/CollateralValuationProcessChangedConsumer.cs
@@ -1,4 +1,9 @@
-﻿using MassTransit;
+﻿using cz.mpss.api.starbuild.mortgageworkflow.mortgageprocessevents.v1;
+using DomainServices.CaseService.Clients;
+using DomainServices.CaseService.Contracts;
+using DomainServices.RealEstateValuationService.Contracts;
+using DomainServices.RealEstateValuationService.ExternalServices.PreorderService.V1;
+using MassTransit;
 
 namespace DomainServices.RealEstateValuationService.Api.Messaging.CollateralValuationProcessChanged;
 
@@ -14,15 +19,68 @@ internal sealed class CollateralValuationProcessChangedConsumer
         {
             _logger.KafkaMessageCurrentTaskIdIncorrectFormat(message.currentTask.id);
         }
+
+        var taskDetail = await _caseService.GetTaskDetail(currentTaskId, token);
+        var realEstateValuation = taskDetail.TaskDetail.RealEstateValuation;
         
-        
+        await (message.state switch
+        {
+            ProcessStateEnum.TERMINATED => HandleTerminated(realEstateValuation, token),
+            ProcessStateEnum.COMPLETED => HandleCompleted(realEstateValuation, token),
+            _ => Task.CompletedTask
+        });
     }
-    
+
+    private async Task HandleTerminated(AmendmentRealEstateValuation realEstateValuation, CancellationToken token)
+    {
+        var getRequest = new GetRealEstateValuationDetailByOrderIdRequest { OrderId = realEstateValuation.OrderId };
+        var realEstateValuationDetail = await _mediator.Send(getRequest, token);
+
+        var updateRequest = new UpdateStateByRealEstateValuationRequest
+        {
+            RealEstateValuationId = realEstateValuationDetail.RealEstateValuationId,
+            ValuationStateId = 5
+        };
+
+        await _mediator.Send(updateRequest, token);
+    }
+
+    private async Task HandleCompleted(AmendmentRealEstateValuation realEstateValuation, CancellationToken token)
+    {
+        var getRequest = new GetRealEstateValuationDetailByOrderIdRequest { OrderId = realEstateValuation.OrderId };
+        var realEstateValuationDetail = await _mediator.Send(getRequest, token);
+        
+        if (!realEstateValuation.OnlineValuation)
+        {
+            var orderResultResponse = await _preorderServiceClient.GetOrderResult(realEstateValuation.OrderId, token);
+            var valuationResultCurrentPrice = orderResultResponse.ValuationResultCurrentPrice;
+            var valuationResultFuturePrice = orderResultResponse.ValuationResultFuturePrice;
+            // todo:
+        }
+
+        var updateRequest = new UpdateStateByRealEstateValuationRequest
+        {
+            RealEstateValuationId = realEstateValuationDetail.RealEstateValuationId,
+            ValuationStateId = 4
+        };
+        
+        await _mediator.Send(updateRequest, token);
+    }
+
+    private readonly IMediator _mediator;
+    private readonly ICaseServiceClient _caseService;
+    private readonly IPreorderServiceClient _preorderServiceClient;
     private readonly ILogger<CollateralValuationProcessChangedConsumer> _logger;
 
     public CollateralValuationProcessChangedConsumer(
+        IMediator mediator,
+        ICaseServiceClient caseService,
+        IPreorderServiceClient preorderServiceClient,
         ILogger<CollateralValuationProcessChangedConsumer> logger)
     {
+        _mediator = mediator;
+        _caseService = caseService;
+        _preorderServiceClient = preorderServiceClient;
         _logger = logger;
     }
 }
