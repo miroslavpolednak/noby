cc84d919b2dbc04acd7626e55b1e54b6c37455ab
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-08-28 13:39:54 +0200

    wip: real estate consuming kafka CollateralValuation and InformationRequest messages

---
+++ DomainServices/RealEstateValuationService/Api/Messaging/CollateralValuationProcessChanged/CollateralValuationProcessChangedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+
+namespace DomainServices.RealEstateValuationService.Api.Messaging.CollateralValuationProcessChanged;
+
+internal sealed class CollateralValuationProcessChangedConsumer
+    : IConsumer<cz.mpss.api.starbuild.mortgageworkflow.mortgageprocessevents.v1.CollateralValuationProcessChanged> 
+{
+    public async Task Consume(ConsumeContext<cz.mpss.api.starbuild.mortgageworkflow.mortgageprocessevents.v1.CollateralValuationProcessChanged> context)
+    {
+        var token = context.CancellationToken;
+        var message = context.Message;
+        
+        if (!int.TryParse(message.currentTask.id, out var currentTaskId))
+        {
+            _logger.KafkaMessageCurrentTaskIdIncorrectFormat(message.currentTask.id);
+        }
+        
+        
+    }
+    
+    private readonly ILogger<CollateralValuationProcessChangedConsumer> _logger;
+
+    public CollateralValuationProcessChangedConsumer(
+        ILogger<CollateralValuationProcessChangedConsumer> logger)
+    {
+        _logger = logger;
+    }
+}
---
+++ DomainServices/RealEstateValuationService/Api/Messaging/InformationRequestProcessChanged/InformationRequestProcessChangedConsumer.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+
+namespace DomainServices.RealEstateValuationService.Api.Messaging.InformationRequestProcessChanged;
+
+internal sealed class InformationRequestProcessChangedConsumer
+    : IConsumer<cz.mpss.api.starbuild.mortgageworkflow.mortgageprocessevents.v1.InformationRequestProcessChanged>
+{
+    public async Task Consume(ConsumeContext<cz.mpss.api.starbuild.mortgageworkflow.mortgageprocessevents.v1.InformationRequestProcessChanged> context)
+    {
+        var token = context.CancellationToken;
+        var message = context.Message;
+        
+        if (!int.TryParse(message.currentTask.id, out var currentTaskId))
+        {
+            _logger.KafkaMessageCurrentTaskIdIncorrectFormat(message.currentTask.id);
+        }
+        
+        if (!long.TryParse(message.@case.caseId.id, out var caseId))
+        {
+            _logger.KafkaMessageCaseIdIncorrectFormat(message.@case.caseId.id);
+        }
+    }
+
+    private readonly ILogger<InformationRequestProcessChangedConsumer> _logger;
+
+    public InformationRequestProcessChangedConsumer(
+        ILogger<InformationRequestProcessChangedConsumer> logger)
+    {
+        _logger = logger;
+    }
+}
