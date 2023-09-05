3269dd136d77544352b71b9e2a9df9a60f0a2453
filip-tuma <filip@filiptuma.com>
2023-09-05 14:53:35 +0200

    no message

--- CIS/Infrastructure.CisMediatR/PayloadLogger/PayloadLoggerBehavior.cs
+++ CIS/Infrastructure.CisMediatR/PayloadLogger/PayloadLoggerBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using CIS.Infrastructure.Logging;
+using CIS.Infrastructure.CisMediatR.PayloadLogger;
 
 namespace CIS.Infrastructure.CisMediatR;
 
@@ -9,7 +10,8 @@ public sealed class PayloadLoggerBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
     private readonly ILogger<PayloadLoggerBehavior<TRequest, TResponse>> _logger;
-    
+    private JsonConvertByteString _byteStringConverter = new JsonConvertByteString();
+
     public PayloadLoggerBehavior(ILogger<PayloadLoggerBehavior<TRequest, TResponse>> logger)
     {
         _logger = logger;
@@ -21,7 +23,7 @@ public sealed class PayloadLoggerBehavior<TRequest, TResponse>
 
         using (_logger.BeginScope(new Dictionary<string, object>
             {
-                { "Payload", JsonConvert.SerializeObject(request) }
+                { "Payload", JsonConvert.SerializeObject(request, _byteStringConverter) }
             }))
         {
             _logger.RequestHandlerStarted(requestName);
@@ -30,12 +32,14 @@ public sealed class PayloadLoggerBehavior<TRequest, TResponse>
         var response = await next();
 
         if (response is null || response is Unit)
+        {
             _logger.RequestHandlerFinishedWithEmptyResult(requestName);
+        }
         else
         {
             using (_logger.BeginScope(new Dictionary<string, object>
             {
-                { "Payload", JsonConvert.SerializeObject(response) }
+                { "Payload", JsonConvert.SerializeObject(response, _byteStringConverter) }
             }))
             {
                 _logger.RequestHandlerFinished(requestName);
