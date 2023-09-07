64b6604f93646cd94132c1e310e23f74168be091
Polednak Miroslav <miroslav.polednak@mpss.cz>
2023-09-07 22:27:33 +0200

    Added logic + code refactoring

---
+++ NOBY/NOBY.Api/Endpoints/DocumentOnSA/SearchDocumentsOnSaOnCase/SearchDocumentsOnSaOnCaseValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using NOBY.Api.Endpoints.DocumentOnSA.Search;
+
+namespace NOBY.Api.Endpoints.DocumentOnSA.SearchDocumentsOnSaOnCase;
+
+public class SearchDocumentsOnSaOnCaseValidator : AbstractValidator<SearchDocumentsOnSaOnCaseRequest>
+{
+    public SearchDocumentsOnSaOnCaseValidator()
+    {
+        RuleFor(e => e.EACodeMainId).NotNull().WithMessage($"{nameof(SearchDocumentsOnSaRequest.EACodeMainId)} is required");
+    }
+}
---
+++ NOBY/NOBY.Services/EaCodeMain/EaCodeMainHelper.cs
@@ -0,0 +1,46 @@
+﻿using DomainServices.CodebookService.Clients;
+using DomainServices.CodebookService.Contracts.v1;
+using NOBY.Infrastructure.ErrorHandling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOBY.Services.EaCodeMain;
+
+public interface IEaCodeMainHelper
+{
+    Task ValidateEaCodeMain(int eCodeMainId, CancellationToken cancellationToken);
+
+    Task<List<int>> GetDocumentTypeIdsAccordingEaCodeMain(int eCodeMainId, CancellationToken cancellationToken);
+}
+
+[ScopedService, AsImplementedInterfacesService]
+public class EaCodeMainHelper : IEaCodeMainHelper
+{
+    private readonly ICodebookServiceClient _codebookService;
+
+    public EaCodeMainHelper(ICodebookServiceClient codebookService)
+    {
+        _codebookService = codebookService;
+    }
+
+    public async Task<List<int>> GetDocumentTypeIdsAccordingEaCodeMain(int eCodeMainId, CancellationToken cancellationToken)
+    {
+        var documentTypes = await _codebookService.DocumentTypes(cancellationToken);
+        return documentTypes.Where(r => r.EACodeMainId == eCodeMainId)
+                                    .Select(s => s.Id)
+                                    .ToList();
+    }
+
+    public async Task ValidateEaCodeMain(int eCodeMainId, CancellationToken cancellationToken)
+    {
+        var eaCodeMains = await _codebookService.EaCodesMain(cancellationToken);
+        var eaCodeMain = eaCodeMains.Find(r => r.Id == eCodeMainId) 
+            ?? throw new NobyValidationException($"Specified EACodeMainId:{eCodeMainId} isn't valid");
+        
+        if (!eaCodeMain.IsFormIdRequested)
+            throw new NobyValidationException($"Specified EACodeMainId has IsFormIdRequested == false");
+    }
+}
