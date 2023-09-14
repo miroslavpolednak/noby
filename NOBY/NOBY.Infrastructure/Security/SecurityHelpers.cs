3038e083acdefed946bd664cc50f9928149a3663
filip-tuma <filip@filiptuma.com>
2023-09-14 09:41:55 +0200

    no message

---
+++ NOBY/NOBY.Infrastructure/Security/SecurityHelpers.cs
@@ -0,0 +1,30 @@
+﻿using CIS.Core.Security;
+using CIS.Foms.Enums;
+using DomainServices.UserService.Clients.Authorization;
+
+namespace NOBY.Infrastructure.Security;
+
+public static class SecurityHelpers
+{
+    /// <summary>
+    /// Autorizace uzivatele na Case podle OwnerUserId a na stav Case
+    /// </summary>
+    public static void CheckCaseOwnerAndState(ICurrentUserAccessor currentUser, in int ownerUserId, in int caseState)
+    {
+        // vidi vlastni Case nebo ma pravo videt vse
+        if (currentUser.User!.Id != ownerUserId && !currentUser.HasPermission(UserPermissions.DASHBOARD_AccessAllCases))
+        {
+            throw new CisAuthorizationException("CaseOwnerValidation: user is not owner of the Case or does not have DASHBOARD_AccessAllCases permission");
+        }
+
+        // zakazane stavy Case
+        if (caseState is (int)CaseStates.Finished or (int)CaseStates.Cancelled)
+        {
+            throw new CisAuthorizationException($"CaseOwnerValidation: Case state is {caseState}");
+        }
+        else if (caseState is 5 && !currentUser.HasPermission(UserPermissions.CASE_ViewAfterDrawing))
+        {
+            throw new CisAuthorizationException($"CaseOwnerValidation: CASE_ViewAfterDrawing missing");
+        }
+    }
+}
