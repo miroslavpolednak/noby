825283881944736247a05dfdc5852ccc0426bf78
filip-tuma <filip@filiptuma.com>
2023-09-16 00:32:40 +0200

    HFICH-8080

--- DomainServices/DocumentArchiveService/Api/Endpoints/DocumentArchiveServiceGrpc.cs
+++ DomainServices/DocumentArchiveService/Api/Endpoints/DocumentArchiveServiceGrpc.cs
@@ -14,21 +14,27 @@ public class DocumentArchiveServiceGrpc : Contracts.v1.DocumentArchiveService.Do
         _mediator = mediator;
     }
 
+    [Authorize(Roles = "WriteDocumentArchiveService")]
     public override async Task<GenerateDocumentIdResponse> GenerateDocumentId(GenerateDocumentIdRequest request, ServerCallContext context)
      => await _mediator.Send(request, context.CancellationToken);
 
+    [Authorize(Roles = "WriteDocumentArchiveService")]
     public override async Task<Empty> UploadDocument(UploadDocumentRequest request, ServerCallContext context)
      => await _mediator.Send(request, context.CancellationToken);
 
+    [Authorize(Roles = "ReadDocumentArchiveService")]
     public override async Task<GetDocumentResponse> GetDocument(GetDocumentRequest request, ServerCallContext context)
      => await _mediator.Send(request, context.CancellationToken);
 
+    [Authorize(Roles = "ReadDocumentArchiveService")]
     public override async Task<GetDocumentListResponse> GetDocumentList(GetDocumentListRequest request, ServerCallContext context)
      => await _mediator.Send(request, context.CancellationToken);
 
+    [Authorize(Roles = "ReadDocumentArchiveService")]
     public override async Task<GetDocumentsInQueueResponse> GetDocumentsInQueue(GetDocumentsInQueueRequest request, ServerCallContext context)
      => await _mediator.Send(request, context.CancellationToken);
 
+    [Authorize(Roles = "WriteDocumentArchiveService")]
     public override async Task<Empty> SetDocumentStatusInQueue(SetDocumentStatusInQueueRequest request, ServerCallContext context)
      => await _mediator.Send(request, context.CancellationToken);
 
