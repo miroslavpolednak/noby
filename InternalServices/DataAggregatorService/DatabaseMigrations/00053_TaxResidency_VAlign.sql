e2100851672069ec0f44c54fe774980fb198e625
David Martínek <david.martinek@mpss.cz>
2023-09-16 11:15:21 +0200

    CRS - added current date to template

--- InternalServices/DataAggregatorService/DatabaseMigrations/00053_TaxResidency_VAlign.sql
+++ InternalServices/DataAggregatorService/DatabaseMigrations/00053_TaxResidency_VAlign.sql
@@ -1,1 +1,3 @@
 UPDATE DocumentSpecialDataField SET VAlign = 2 WHERE DocumentId = 13 AND AcroFieldName = 'DuvodNeposkytnuti'
+
+INSERT INTO DocumentDataField VALUES (13, '001', 'ProhlaseniDatum', 22, NULL, NULL, NULL, NULL)
