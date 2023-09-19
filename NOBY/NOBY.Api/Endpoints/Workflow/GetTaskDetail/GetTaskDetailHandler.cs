a79e84fea1250fa8e6fa193539abb064ef0c5259
filip-tuma <filip@filiptuma.com>
2023-09-19 20:59:45 +0200

    HFICH-7972

--- NOBY/NOBY.Api/Endpoints/Workflow/GetTaskDetail/GetTaskDetailHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Workflow/GetTaskDetail/GetTaskDetailHandler.cs
@@ -1,4 +1,5 @@
-﻿using DomainServices.CaseService.Clients;
+﻿using CIS.Core.Security;
+using DomainServices.CaseService.Clients;
 
 namespace NOBY.Api.Endpoints.Workflow.GetTaskDetail;
 
@@ -15,6 +16,15 @@ internal sealed class GetTaskDetailHandler
         {
             throw new NobyValidationException(90032, "TaskTypeId not allowed");
         }
+
+        if ((task.TaskTypeId is 6 or 8) && !_currentUserAccessor.HasPermission(UserPermissions.WFL_TASK_DETAIL_SigningView))
+        {
+            throw new CisAuthorizationException("Task detail manage permission missing");
+        }
+        else if (!(task.TaskTypeId is 6 or 8) && !_currentUserAccessor.HasPermission(UserPermissions.WFL_TASK_DETAIL_OtherView))
+        {
+            throw new CisAuthorizationException("Task detail manage permission missing");
+        }
         
         var (taskDto, taskDetailDto, documents) = await _workflowTaskService.GetTaskDetail(request.CaseId, task.TaskIdSb, cancellationToken);
 
@@ -26,14 +36,17 @@ internal sealed class GetTaskDetailHandler
         };
     }
 
+    private readonly ICurrentUserAccessor _currentUserAccessor;
     private readonly Services.WorkflowTask.IWorkflowTaskService _workflowTaskService;
     private readonly ICaseServiceClient _caseService;
     private static int[] _allowedTaskTypeIds = { 1, 2, 3, 6, 7 };
 
     public GetTaskDetailHandler(
-            Services.WorkflowTask.IWorkflowTaskService workflowTaskService,
-            ICaseServiceClient caseService)
+        ICurrentUserAccessor currentUserAccessor,
+        Services.WorkflowTask.IWorkflowTaskService workflowTaskService,
+        ICaseServiceClient caseService)
     {
+        _currentUserAccessor = currentUserAccessor;
         _workflowTaskService = workflowTaskService;
         _caseService = caseService;
     }
