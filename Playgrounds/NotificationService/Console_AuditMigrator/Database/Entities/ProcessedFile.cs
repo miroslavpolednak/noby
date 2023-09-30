133d8a8a30a285cd907ccfb1ce40c9601b7d556a
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-09-30 21:10:33 +0200

    feature: notification service first version of audit log migrator

---
+++ Playgrounds/NotificationService/Console_AuditMigrator/Database/Entities/ProcessedFile.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Console_AuditMigrator.Database.Entities;
+
+public class ProcessedFile
+{
+    [Key]
+    public int Id { get; set; }
+    public string FileName { get; set; } = null!;
+    public DateTime Timestamp { get; set; }
+    public virtual ICollection<ApplicationLog> ApplicationLogs { get; set; } = null!;
+}
