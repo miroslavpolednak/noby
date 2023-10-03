4fd1f12a9e1f271541a0a12206ef416d20224778
David Martínek <david.martinek@mpss.cz>
2023-10-03 18:31:21 +0200

    TaxResidency document - added CaseId

--- NOBY/NOBY.Api/Endpoints/Document/GeneralDocument/GetGeneralDocumentHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Document/GeneralDocument/GetGeneralDocumentHandler.cs
@@ -1,18 +1,28 @@
-﻿using NOBY.Api.Endpoints.Document.Shared;
+﻿using DomainServices.SalesArrangementService.Clients;
+using NOBY.Api.Endpoints.Document.Shared;
 
 namespace NOBY.Api.Endpoints.Document.GeneralDocument;
 
 internal sealed class GetGeneralDocumentHandler : IRequestHandler<GetGeneralDocumentRequest, ReadOnlyMemory<byte>>
 {
+    private readonly ISalesArrangementServiceClient _salesArrangementService;
     private readonly DocumentGenerator _documentGenerator;
 
-    public GetGeneralDocumentHandler(DocumentGenerator documentGenerator)
+    public GetGeneralDocumentHandler(ISalesArrangementServiceClient salesArrangementService, DocumentGenerator documentGenerator)
     {
+        _salesArrangementService = salesArrangementService;
         _documentGenerator = documentGenerator;
     }
 
     public async Task<ReadOnlyMemory<byte>> Handle(GetGeneralDocumentRequest request, CancellationToken cancellationToken)
     {
+        if (request.InputParameters.SalesArrangementId.HasValue)
+        {
+            var saValidationResult = await _salesArrangementService.ValidateSalesArrangementId(request.InputParameters.SalesArrangementId.Value, false, cancellationToken);
+
+            request.InputParameters.CaseId = saValidationResult.CaseId;
+        }
+
         var generateDocumentRequest = await _documentGenerator.CreateRequest(request, cancellationToken);
 
         return await _documentGenerator.GenerateDocument(generateDocumentRequest, cancellationToken);
