8c64ce5e659100db8d59b16ddc67e6bb5579f09f
David Martínek <david.martinek@mpss.cz>
2023-10-05 20:07:08 +0200

    TaxResidency migration script updated

--- InternalServices/DataAggregatorService/DatabaseMigrations/00057_TaxResidencyTemplate.sql
+++ InternalServices/DataAggregatorService/DatabaseMigrations/00057_TaxResidencyTemplate.sql
@@ -4,4 +4,7 @@ UPDATE DocumentDataField SET DataFieldId = 215 WHERE DocumentId = 13 AND AcroFie
 
 UPDATE DocumentDataField SET DefaultTextIfNull = '--' WHERE DocumentId = 9 AND AcroFieldName = 'DetailniPopisText'
 
-UPDATE DocumentDataField SET StringFormat = '--', DefaultTextIfNull = '--' WHERE DocumentId = 9 AND AcroFieldName = 'ZmocnenecProDorucovani'
+UPDATE DocumentDataField SET StringFormat = NULL, DefaultTextIfNull = '--' WHERE DocumentId = 9 AND AcroFieldName = 'ZmocnenecProDorucovani'
+
+UPDATE DocumentDynamicStringFormat SET StringFormat = '--' WHERE DynamicStringFormatId = 123
+UPDATE DocumentDynamicStringFormatCondition SET EqualToValue = 'False' WHERE DynamicStringFormatId = 123
