e8bc5ee077e645ad92bcb16c26cae7324cd2f48e
David Martínek <david.martinek@mpss.cz>
2023-10-06 19:25:53 +0200

    HFICH-8456

--- NOBY/NOBY.Api/Endpoints/Cases/GetDashboardFilters/GetDashboardFiltersHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Cases/GetDashboardFilters/GetDashboardFiltersHandler.cs
@@ -8,14 +8,16 @@ internal sealed class GetDashboardFiltersHandler
         // zavolat BE sluzbu
         var result = await _caseService.GetCaseCounts(_userAccessor.User!.Id, cancellationToken);
 
+        result.RemoveAll(x => x.State is 6 or 7 or 10 || x.State is 5 && !_userAccessor.HasPermission(UserPermissions.CASE_ViewAfterDrawing));
+
         // rucne vytvorena kolekce podle Motalika
         return new List<GetDashboardFiltersResponse>
         {
-            new GetDashboardFiltersResponse(1, "Vše", result.Where(t => t.State is not (6 or 7 or 10)).Select(t => t.Count).Sum()),
-            new GetDashboardFiltersResponse(2, "Žádosti o úvěr", result.Where(t => t.State is 1 or 2 or 8).Select(t => t.Count).Sum()),
-            new GetDashboardFiltersResponse(3, "Podepisování smluv", result.Where(t => t.State == 3).Select(t => t.Count).Sum()),
-            new GetDashboardFiltersResponse(4, "Čerpání", result.Where(t => t.State == 4).Select(t => t.Count).Sum()),
-            new GetDashboardFiltersResponse(5, "Správa", result.Where(t => t.State == 5).Select(t => t.Count).Sum())
+            new(1, "Vše", result.Select(t => t.Count).Sum()),
+            new(2, "Žádosti o úvěr", result.Where(t => t.State is 1 or 2 or 8).Select(t => t.Count).Sum()),
+            new(3, "Podepisování smluv", result.Where(t => t.State == 3).Select(t => t.Count).Sum()),
+            new(4, "Čerpání", result.Where(t => t.State == 4).Select(t => t.Count).Sum()),
+            new(5, "Správa", result.Where(t => t.State == 5).Select(t => t.Count).Sum())
         };
     }
 
