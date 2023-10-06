9d43524284c19c61bb386926acc5dc83e976e652
David Martínek <david.martinek@mpss.cz>
2023-10-06 14:25:07 +0200

    HFICH-8609 + fixed CustomerChange template acrofieldname in script

--- InternalServices/DataAggregatorService/DatabaseMigrations/00058_CustomerChange_PaymentAccount_User.sql
+++ InternalServices/DataAggregatorService/DatabaseMigrations/00058_CustomerChange_PaymentAccount_User.sql
@@ -1,10 +1,10 @@
 INSERT INTO DocumentDynamicInputParameter VALUES (9, '001', 2)
 
-INSERT INTO DocumentSpecialDataField VALUES (9, '001', 'CisloUverovehoUctu', 6, 'PaymentAccount', NULL, NULL, NULL, NULL)
+INSERT INTO DocumentSpecialDataField VALUES (9, '001', 'CisloUverUctu', 6, 'PaymentAccount', NULL, NULL, NULL, NULL)
 
-INSERT INTO DocumentVariant VALUES (9, '001', 'CisloUverovehoUctu', 'A')
-INSERT INTO DocumentVariant VALUES (9, '001', 'CisloUverovehoUctu', 'B')
-INSERT INTO DocumentVariant VALUES (9, '001', 'CisloUverovehoUctu', 'C')
-INSERT INTO DocumentVariant VALUES (9, '001', 'CisloUverovehoUctu', 'D')
+INSERT INTO DocumentVariant VALUES (9, '001', 'CisloUverUctu', 'A')
+INSERT INTO DocumentVariant VALUES (9, '001', 'CisloUverUctu', 'B')
+INSERT INTO DocumentVariant VALUES (9, '001', 'CisloUverUctu', 'C')
+INSERT INTO DocumentVariant VALUES (9, '001', 'CisloUverUctu', 'D')
 
 DELETE FROM DocumentDynamicInputParameter WHERE DocumentId = 9 AND DynamicInputParameterId = 7
