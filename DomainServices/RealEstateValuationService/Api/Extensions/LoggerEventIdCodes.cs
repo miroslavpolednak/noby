78a755654d5a4d00fc8bfb29875d83b6e7aac89e
Karel Nguyen <karel.nguyen-trong@mpss.cz>
2023-10-06 14:04:56 +0200

    fix: realestatevaluation service collateral valuation process changed consumer

--- DomainServices/RealEstateValuationService/Api/Extensions/LoggerEventIdCodes.cs
+++ DomainServices/RealEstateValuationService/Api/Extensions/LoggerEventIdCodes.cs
@@ -5,4 +5,5 @@ internal sealed class LoggerEventIdCodes
     public const int AttachmentDeleteFailed = 22501;
     public const int KafkaMessageCaseIdIncorrectFormat = 22502;
     public const int KafkaMessageCurrentTaskIdIncorrectFormat = 22503;
+    public const int RealEstateValuationNotFound = 22504;
 }
