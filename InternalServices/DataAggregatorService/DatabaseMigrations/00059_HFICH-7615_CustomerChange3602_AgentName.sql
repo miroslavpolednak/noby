ff8a0209ed394cb2f1532e56b18b6b9d0320338d
David Martínek <david.martinek@mpss.cz>
2023-10-10 16:37:55 +0200

    HFICH-7615

---
+++ InternalServices/DataAggregatorService/DatabaseMigrations/00059_HFICH-7615_CustomerChange3602_AgentName.sql
@@ -0,0 +1,3 @@
+DELETE FROM DocumentDataField WHERE DocumentId = 16 AND AcroFieldName = 'Zmocnenec'
+
+INSERT INTO DocumentSpecialDataField VALUES (16, '001', 'Zmocnenec', 1, 'AgentName', NULL, NULL, '--', NULL)
