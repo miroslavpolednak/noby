ffd0c49fd0adaa67ca6119e7b0e883be52f49570
David Martínek <david.martinek@mpss.cz>
2023-10-11 20:01:17 +0200

    DatabaseMigrations added check only if migration exists

---
+++ DatabaseMigrations/ExitCode.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseMigrations;
+
+public enum ExitCode
+{
+    DirectoryNotExist = -3,
+    MigrationFailed = -2,
+    UnknownError = -1,
+    Success = 0,
+    NoMigrationAvailable = 1
+}
