888a28500d87541e1b57444316933df5ee8fd203
David Martínek <david.martinek@mpss.cz>
2023-10-28 10:54:32 +0200

    Offer and Calculation template - fixed domicile

---
+++ InternalServices/DataAggregatorService/DatabaseMigrations/00061_Fixed_Domicile.sql
@@ -0,0 +1,5 @@
+UPDATE DocumentDataField SET StringFormat = 'ne' WHERE DocumentId = 2 AND AcroFieldName = 'Domicilace'
+
+UPDATE DocumentDataField 
+SET StringFormat = 'směřování Vašich příjmů na bankovní účet vedený u nás, a to alespoň ve výši 1,5 násobku výše splátky úvěru'
+WHERE DocumentId = 1 AND AcroFieldName = 'Domicilace'
