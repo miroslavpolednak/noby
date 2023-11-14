1da3fce1640778566ae13b98c32b07c3c12825ee
filip-tuma <filip@filiptuma.com>
2023-11-14 15:31:38 +0100

    Logging flush timeout

--- CIS/Infrastructure.Telemetry/Constants.cs
+++ CIS/Infrastructure.Telemetry/Constants.cs
@@ -5,4 +5,6 @@ internal sealed class Constants
     public const string LoggerContextUserIdPropertyName = "CisUserId";
 
     public const string LoggerContextUserIdentPropertyName = "CisUserIdent";
+
+    public const string FileLoggerTemplate = @"{Timestamp:yyyy-MM-dd HH:mm:ss,fff zzz} [{ThreadId}] [{Level:u}] [{TraceId}] [{SpanId}] [{ParentId}] [{CisAppKey}] [{Version}] [{Assembly}] [{SourceContext}] [{MachineName}] [{ClientIp}] [{CisUserId}] [{CisUserIdent}] [{RequestId}] [{RequestPath}] [{ConnectionId}] [{Message}] [{Exception}{Payload}]{NewLine}";
 }
