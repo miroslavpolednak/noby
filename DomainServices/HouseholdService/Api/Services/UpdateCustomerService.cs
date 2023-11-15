4c668b0afff519ef2f45df5ca48a0956b2ebba60
David Martínek <david.martinek@mpss.cz>
2023-11-15 12:48:06 +0100

    SB NewClient - changed ClientType

--- DomainServices/HouseholdService/Api/Services/UpdateCustomerService.cs
+++ DomainServices/HouseholdService/Api/Services/UpdateCustomerService.cs
@@ -24,9 +24,9 @@ internal sealed class UpdateCustomerService
             LastName = cmCustomer.NaturalPerson.LastName,
             DateOfBirth = cmCustomer.NaturalPerson.DateOfBirth,
             // firmu neresime?
-            ClientType = (cmCustomer.NaturalPerson.CitizenshipCountriesId?.Any(t => t == defaultCountry) ?? false) && !string.IsNullOrEmpty(cmCustomer.NaturalPerson.BirthNumber)
-                ? ExternalServices.Eas.Dto.ClientDataModel.ClientTypes.FO 
-                : ExternalServices.Eas.Dto.ClientDataModel.ClientTypes.Foreigner
+            ClientType = string.IsNullOrWhiteSpace(cmCustomer.NaturalPerson.BirthNumber)
+                ? ExternalServices.Eas.Dto.ClientDataModel.ClientTypes.Foreigner
+                : ExternalServices.Eas.Dto.ClientDataModel.ClientTypes.FO
         };
 
         int? id = (await _easClient.CreateNewOrGetExisingClient(model, cancellationToken)).Id;
