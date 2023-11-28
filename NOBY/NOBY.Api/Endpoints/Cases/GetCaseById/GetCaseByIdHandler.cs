c52da5af68cd1f3c79bb72f7e003d35511ff2be1
filip-tuma <filip@filiptuma.com>
2023-11-28 23:50:28 +0100

    no message

--- NOBY/NOBY.Api/Endpoints/Cases/GetCaseById/GetCaseByIdHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Cases/GetCaseById/GetCaseByIdHandler.cs
@@ -1,18 +1,27 @@
-﻿using CIS.Core.Security;
-
-namespace NOBY.Api.Endpoints.Cases.GetCaseById;
+﻿namespace NOBY.Api.Endpoints.Cases.GetCaseById;
 
 internal sealed class GetCaseByIdHandler
     : IRequestHandler<GetCaseByIdRequest, Dto.CaseModel>
 {
     public async Task<Dto.CaseModel> Handle(GetCaseByIdRequest request, CancellationToken cancellationToken)
     {
-        var result = await _caseService.GetCaseDetail(request.CaseId, cancellationToken);
+        DomainServices.CaseService.Contracts.Case? caseInstance = null;
 
-        var model = await _converter.FromContract(result);
+        try
+        {
+            caseInstance = await _caseService.GetCaseDetail(request.CaseId, cancellationToken);
+        }
+        catch (CisNotFoundException)
+        {
+            // osetrena vyjimka - spoustime logiku na vytvoreni case z konsDB
+            await _createCaseFromExternalSources.CreateCase(request.CaseId, cancellationToken);
+            caseInstance = await _caseService.GetCaseDetail(request.CaseId, cancellationToken);
+        }
+
+        var model = await _converter.FromContract(caseInstance!);
         
         // case owner
-        var userInstance = await _userService.GetUser(result.CaseOwner.UserId, cancellationToken);
+        var userInstance = await _userService.GetUser(caseInstance!.CaseOwner.UserId, cancellationToken);
         model.CaseOwner = new Dto.CaseOwnerModel
         {
             Cpm = userInstance.UserInfo.Cpm,
@@ -22,18 +31,18 @@ internal sealed class GetCaseByIdHandler
         return model;
     }
 
-    private readonly ICurrentUserAccessor _currentUser;
+    private readonly Services.CreateCaseFromExternalSources.CreateCaseFromExternalSourcesService _createCaseFromExternalSources;
     private readonly CasesModelConverter _converter;
     private readonly DomainServices.UserService.Clients.IUserServiceClient _userService;
     private readonly DomainServices.CaseService.Clients.ICaseServiceClient _caseService;
 
     public GetCaseByIdHandler(
-        ICurrentUserAccessor currentUser,
+        Services.CreateCaseFromExternalSources.CreateCaseFromExternalSourcesService createCaseFromExternalSources,
         CasesModelConverter converter,
         DomainServices.UserService.Clients.IUserServiceClient userService,
         DomainServices.CaseService.Clients.ICaseServiceClient caseService)
     {
-        _currentUser = currentUser;
+        _createCaseFromExternalSources = createCaseFromExternalSources;
         _userService = userService;
         _converter = converter;
         _caseService = caseService;
