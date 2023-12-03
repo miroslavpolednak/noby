d6560b56351461d86e1ac0a12e21c90ec37b7965
filip-tuma <filip@filiptuma.com>
2023-12-03 23:30:03 +0100

    no message

---
+++ DomainServices/RealEstateValuationService/Api/Database/DocumentDataEntities/RealEstateValudationData.cs
@@ -0,0 +1,49 @@
+﻿namespace DomainServices.RealEstateValuationService.Api.Database.DocumentDataEntities;
+
+internal sealed class RealEstateValudationData
+    : SharedComponents.DocumentDataStorage.IDocumentData
+{
+    public int Version => 1;
+
+    public SpecificDetailHouseAndFlatObject? HouseAndFlatDetails { get; set; }
+    public SpecificDetailParcelObject? ParcelDetails { get; set; }
+    public List<RealEstateValuationDocument>? Documents { get; set; }
+    public List<int>? LoanPurposeDetails { get; set; }
+
+    public sealed class SpecificDetailHouseAndFlatObject
+    {
+        public bool PoorCondition { get; set; }
+        public bool OwnershipRestricted { get; set; }
+        public SpecificDetailFlatOnlyDetails? FlatOnlyDetails { get; set; }
+        public SpecificDetailFinishedHouseAndFlatDetails? FinishedHouseAndFlatDetails { get; set; }
+    }
+
+    public sealed class SpecificDetailFlatOnlyDetails
+    {
+        public bool SpecialPlacement { get; set; }
+        public bool Basement { get; set; }
+    }
+
+    public sealed class SpecificDetailFinishedHouseAndFlatDetails
+    {
+        public bool Leased { get; set; }
+        public bool LeaseApplicable { get; set; }
+    }
+
+    public sealed class SpecificDetailParcelObject
+    {
+        public List<SpecificDetailParcelNumber>? ParcelNumbers { get; set; }
+    }
+
+    public sealed class SpecificDetailParcelNumber
+    {
+        public int? Prefix { get; set; }
+        public int? Number { get; set; }
+    }
+
+    public sealed class RealEstateValuationDocument
+    {
+        public string? DocumentInfoPrice { get; set; }
+        public string? DocumentRecommendationForClient { get; set; }
+    }
+}
