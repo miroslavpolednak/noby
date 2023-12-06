d54e0cc6cf08ef459880ee129a4cd3a7e5845935
Polednak Miroslav <miroslav.polednak@mpss.cz>
2023-12-06 13:18:50 +0100

    Job prepare

---
+++ DomainServices/SalesArrangementService/Api/BackgroundServices/CancelServiceSalesArrangement/CancelServiceSalesArrangementJob.cs
@@ -0,0 +1,20 @@
+﻿
+using DomainServices.SalesArrangementService.Api.Database;
+
+namespace DomainServices.SalesArrangementService.Api.BackgroundServices.CancelServiceSalesArrangement;
+
+internal sealed class CancelServiceSalesArrangementJob
+    : CIS.Infrastructure.BackgroundServices.ICisBackgroundServiceJob
+{
+    private readonly SalesArrangementServiceDbContext _dbContext;
+
+    public CancelServiceSalesArrangementJob(SalesArrangementServiceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task ExecuteJobAsync(CancellationToken cancellationToken)
+    {
+        
+    }
+}
