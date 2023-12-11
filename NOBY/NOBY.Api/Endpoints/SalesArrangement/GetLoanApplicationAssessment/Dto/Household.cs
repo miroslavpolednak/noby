d98f42a3102bacd7cdb865158bd57833f14ede3c
filip-tuma <filip@filiptuma.com>
2023-12-11 20:00:34 +0100

    no message

--- NOBY/NOBY.Api/Endpoints/SalesArrangement/GetLoanApplicationAssessment/Dto/Household.cs
+++ NOBY/NOBY.Api/Endpoints/SalesArrangement/GetLoanApplicationAssessment/Dto/Household.cs
@@ -68,26 +68,125 @@ public sealed class HouseholdRisk
 
 public sealed class HouseholdCustomerObligations
 {
+    /// <summary>
+    /// Jméno klienta
+    /// </summary>
     public string FirstName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Příjmení klienta
+    /// </summary>
     public string LastName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Datum narození klienta
+    /// </summary>
     public DateTime? DateOfBirth { get; set; }
+
+    /// <summary>
+    /// Role klienta
+    /// </summary>
     public CustomerRoles Role { get; set; }
 
+    /// <summary>
+    /// Existující závazky
+    /// </summary>
     public List<HouseholdObligationItem>? ExistingObligations { get; set; }
 
+    /// <summary>
+    /// Rozpracované závazky
+    /// </summary>
     public List<HouseholdObligationItem>? RequestedObligations { get; set; }
 }
 
 public sealed class HouseholdObligationItem
 {
     public int Id { get; set; }
+
+    /// <summary>
+    /// Typ závazku
+    /// </summary>
+    public int ObligationTypeId { get; set; }
+
+    /// <summary>
+    /// Typ závazku - název
+    /// </summary>
     public string ObligationTypeName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Stav závazku
+    /// </summary>
     public ObligationSource Source { get; set; }
+
+    /// <summary>
+    /// Věřitel
+    /// </summary>
     public string CreditorName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Nesplacená jistina
+    /// </summary>
     public Amount? LoanPrincipalAmount { get; set; }
+
+    /// <summary>
+    /// Splátka
+    /// </summary>
     public Amount? InstallmentAmount { get; set; }
+
+    /// <summary>
+    /// Limit
+    /// </summary>
     public Amount? CreditCardLimit { get; set; }
+
+    /// <summary>
+    /// Korekce
+    /// </summary>
     public int? CorrectionTypeId { get; set; }
+
+    /// <summary>
+    /// Kategorie závazku
+    /// </summary>
+    public int ObligationLaExposureId { get; set; }
+
+    /// <summary>
+    /// Kategorie závazku - název
+    /// </summary>
+    public string ObligationLaExposureName { get; set; }
+
+    /// <summary>
+    /// Závazek FOP
+    /// </summary>
+    public bool ZavazekFOP { get; set; }
+
+    /// <summary>
+    /// Kód skupiny
+    /// </summary>
+    public string? KbGroupInstanceCode { get; set; }
+
+    /// <summary>
+    /// Vyčerpaná částka
+    /// </summary>
+    public int DrawingAmount { get; set; }
+
+    /// <summary>
+    /// Číslo  úvěrového účtu
+    /// </summary>
+    public string? BankAccount { get; set; }
+
+    /// <summary>
+    /// Datum poskytnutí
+    /// </summary>
+    public DateTime? ContractDate { get; set; }
+
+    /// <summary>
+    /// Datum splatnosti
+    /// </summary>
+    public DateTime? MaturityDate { get; set; }
+
+    /// <summary>
+    /// Datum aktualizace dat v CBCB
+    /// </summary>
+    public DateTime? CbcbDataLastUpdate { get; set; }
 
     public enum ObligationSource
     {
@@ -97,8 +196,19 @@ public sealed class HouseholdObligationItem
 
     public sealed class Amount
     {
+        /// <summary>
+        /// Částka před korekcí
+        /// </summary>
         public int Value { get; set; }
+
+        /// <summary>
+        /// Částka po korekci
+        /// </summary>
         public int? Correction { get; set; }
-        public string Currency { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Kód měny
+        /// </summary>
+        public string CurrencyCode { get; set; } = string.Empty;
     }
 }
