737c212a28ec7484c8391a8e972cc34d551a150b
David Martínek <david.martinek@mpss.cz>
2023-12-14 18:47:46 +0100

    HFICH-9903 fixed DB view

--- InternalServices/DataAggregatorService/DatabaseMigrations/00069_HFICH-9903_Offer_EmptyText.sql
+++ InternalServices/DataAggregatorService/DatabaseMigrations/00069_HFICH-9903_Offer_EmptyText.sql
@@ -6,3 +6,14 @@ Seznam podkladů k Žádosti o úvěr je možné nalézt zde: www.kb.cz.'
 WHERE DocumentId = 1 AND AcroFieldName = 'Text'
 
 DELETE FROM DocumentDynamicStringFormat WHERE DynamicStringFormatId = 13
+
+GO
+
+CREATE OR ALTER VIEW [dbo].[vw_DocumentDynamicStringFormats] AS
+
+SELECT sf.DocumentId, sf.DocumentVersion, sf.AcroFieldName, sf.StringFormat, sf.[Priority], sf.DynamicStringFormatId, c.EqualToValue, df.FieldPath 
+FROM DocumentDynamicStringFormat sf
+LEFT JOIN DocumentDynamicStringFormatCondition c ON c.DynamicStringFormatId = sf.DynamicStringFormatId
+LEFT JOIN DataField df ON df.DataFieldId = c.DataFieldId
+
+GO
