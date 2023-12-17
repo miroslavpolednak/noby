ce4a61ae3b3a86ad55b92d3820b3ee5396180d6f
David Martínek <david.martinek@mpss.cz>
2023-12-17 23:32:59 +0100

    DatabaseMigrations updated exit codes

--- DatabaseMigrations/ExitCode.cs
+++ DatabaseMigrations/ExitCode.cs
@@ -2,10 +2,10 @@
 
 public enum ExitCode
 {
-    CodeAssemblyNotExist = -4,
-    DirectoryNotExist = -3,
-    MigrationFailed = -2,
-    UnknownError = -1,
     NoMigrationAvailable = -1,
-    Success = 0
+    Success = 0,
+    UnknownError,
+    CodeAssemblyNotExist,
+    DirectoryNotExist,
+    MigrationFailed
 }
