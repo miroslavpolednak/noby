b6bd43cb386ee5328055bb74486644a269d2c71f
Polednak Miroslav <miroslav.polednak@mpss.cz>
2024-01-02 12:46:41 +0100

    Fixed test examples

--- NOBY/NOBY.Api/Endpoints/Test/TestController.cs
+++ NOBY/NOBY.Api/Endpoints/Test/TestController.cs
@@ -64,6 +64,31 @@ public class TestController : ControllerBase
         return result.ToString();
     }
 
+    [HttpGet("t2")]
+    [ApiVersion(2)]
+    public async Task T2V2()
+    {
+        var logger = _context.HttpContext.RequestServices.GetRequiredService<IAuditLogger>();
+        logger.Log(
+            SharedAudit.AuditEventTypes.Noby001,
+            "Nejaka fajn zprava",
+            identities: new List<AuditLoggerHeaderItem> { new("aaa", "bbb") },
+            products: new List<AuditLoggerHeaderItem> { new("111", "Uver") },
+            operation: new("111", "CreateCase"),
+            bodyBefore: new Dictionary<string, string> { { "aaa", "bbb" }, { "ccc", "dddd" } }
+        );
+    }
+
+    [HttpGet("t3")]
+    [ApiVersion(2)]
+    public async Task<string> T3V2()
+    {
+        var client = _context.HttpContext.RequestServices.GetRequiredService<IFileAntivirusService>();
+        var file = System.Text.Encoding.ASCII.GetBytes("X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*");
+        var result = await client.CheckFile(file);
+        return result.ToString();
+    }
+
     private readonly IHttpContextAccessor _context;
     private readonly IMediator _mediator;
 
