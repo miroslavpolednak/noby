2193cae734a0f801392979a96b481675569f99a9
filip-tuma <filip@filiptuma.com>
2024-01-30 09:57:25 +0100

    no message

--- InternalServices/NotificationService/Api/Legacy/HashAlgorithms.cs
+++ InternalServices/NotificationService/Api/Legacy/HashAlgorithms.cs
@@ -2,5 +2,5 @@
 
 internal sealed class HashAlgorithms
 {
-    public static string[] Algorithms = new[] { "" }; 
+    public static string[] Algorithms = new[] { "SHA-256", "SHA-384", "SHA-512", "SHA-3" }; 
 }
