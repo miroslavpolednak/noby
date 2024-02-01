b1e59495ccb10a39900580020b7de20a7f13f0c1
Polednak Miroslav <miroslav.polednak@mpss.cz>
2024-02-01 14:08:33 +0100

    canceled in esignature fixed

--- NOBY/NOBY.Api/Endpoints/DocumentOnSA/StopSigning/StopSigningHandler.cs
+++ NOBY/NOBY.Api/Endpoints/DocumentOnSA/StopSigning/StopSigningHandler.cs
@@ -1,7 +1,7 @@
-﻿using SharedTypes.Enums;
-using DomainServices.DocumentOnSAService.Clients;
+﻿using DomainServices.DocumentOnSAService.Clients;
 using FastEnumUtility;
 using NOBY.Services.PermissionAccess;
+using NOBY.Services.SigningHelper;
 
 namespace NOBY.Api.Endpoints.DocumentOnSA.StopSigning;
 
@@ -9,18 +9,25 @@ public class StopSigningHandler : IRequestHandler<StopSigningRequest>
 {
     private readonly IDocumentOnSAServiceClient _documentOnSAService;
     private readonly INonWFLProductSalesArrangementAccess _nonWFLProductSalesArrangementAccess;
+    private readonly ISigningHelperService _signingHelperService;
 
     public StopSigningHandler(
         IDocumentOnSAServiceClient documentOnSAService,
-        INonWFLProductSalesArrangementAccess nonWFLProductSalesArrangementAccess)
+        INonWFLProductSalesArrangementAccess nonWFLProductSalesArrangementAccess,
+        ISigningHelperService signingHelperService)
     {
         _documentOnSAService = documentOnSAService;
         _nonWFLProductSalesArrangementAccess = nonWFLProductSalesArrangementAccess;
+        _signingHelperService = signingHelperService;
     }
 
     public async Task Handle(StopSigningRequest request, CancellationToken cancellationToken)
     {
-        var documentOnSa = await GetDocumentOnSa(request, cancellationToken);
+        var documentOnSa = await _signingHelperService.GetDocumentOnSa(new()
+        {
+            DocumentOnSAId = request.DocumentOnSAId,
+            SalesArrangementId = request.SalesArrangementId,
+        }, cancellationToken);
 
         if (documentOnSa.Source != DomainServices.DocumentOnSAService.Contracts.Source.Workflow)
             await _nonWFLProductSalesArrangementAccess.CheckNonWFLProductSalesArrangementAccess(documentOnSa.SalesArrangementId, cancellationToken);
@@ -28,31 +35,13 @@ public class StopSigningHandler : IRequestHandler<StopSigningRequest>
         if (documentOnSa.DocumentTypeId == DocumentTypes.DANRESID.ToByte() && documentOnSa.IsSigned) // 13
             throw new NobyValidationException(90036);
 
-        if (documentOnSa.SignatureTypeId == SignatureTypes.Electronic.ToByte())
+        await _signingHelperService.StopSinningAccordingState(new()
         {
-            await _documentOnSAService.RefreshElectronicDocument(documentOnSa.DocumentOnSAId!.Value, cancellationToken);
-            var docOnSaAfterRefresh = await GetDocumentOnSa(request, cancellationToken);
-
-            if (docOnSaAfterRefresh.IsValid == false)
-                return;
-            else if (docOnSaAfterRefresh.IsSigned)
-                await _documentOnSAService.StopSigning(new() { DocumentOnSAId = request.DocumentOnSAId }, cancellationToken);
-            else
-                await _documentOnSAService.StopSigning(new() { DocumentOnSAId = request.DocumentOnSAId, NotifyESignatures = true }, cancellationToken);
-        }
-        else
-        {
-            await _documentOnSAService.StopSigning(new() { DocumentOnSAId = request.DocumentOnSAId }, cancellationToken);
-        }
+            DocumentOnSAId = request.DocumentOnSAId,
+            SalesArrangementId = request.SalesArrangementId,
+            SignatureTypeId = documentOnSa.SignatureTypeId,
+        }, cancellationToken);
     }
 
-    private async Task<DomainServices.DocumentOnSAService.Contracts.DocumentOnSAToSign> GetDocumentOnSa(StopSigningRequest request, CancellationToken cancellationToken)
-    {
-        var documentOnSas = await _documentOnSAService.GetDocumentsOnSAList(request.SalesArrangementId, cancellationToken);
-
-        var documentOnSa = documentOnSas.DocumentsOnSA.SingleOrDefault(d => d.DocumentOnSAId == request.DocumentOnSAId)
-            ?? throw new NobyValidationException($"DocumetnOnSa {request.DocumentOnSAId} not exist for SalesArrangement {request.SalesArrangementId}");
-
-        return documentOnSa;
-    }
+   
 }
--- NOBY/NOBY.Api/Endpoints/SalesArrangement/UpdateParameters/UpdateParametersHandler.cs
+++ NOBY/NOBY.Api/Endpoints/SalesArrangement/UpdateParameters/UpdateParametersHandler.cs
@@ -5,6 +5,7 @@ using DomainServices.SalesArrangementService.Clients;
 using _SA = DomainServices.SalesArrangementService.Contracts;
 using _dto = NOBY.Api.Endpoints.SalesArrangement.SharedDto;
 using FastEnumUtility;
+using NOBY.Services.SigningHelper;
 
 namespace NOBY.Api.Endpoints.SalesArrangement.UpdateParameters;
 
@@ -84,15 +85,21 @@ internal sealed class UpdateParametersHandler
 
             foreach (var documentOnSaToSign in documentResponse.DocumentsOnSAToSign)
             {
-                if (documentOnSaToSign.DocumentTypeId != DocumentTypes.DANRESID.ToByte() && documentOnSaToSign.DocumentOnSAId is not null ) // 13
+                if (documentOnSaToSign.DocumentTypeId != DocumentTypes.DANRESID.ToByte() && documentOnSaToSign.DocumentOnSAId is not null) // 13
                 {
-                    await _documentOnSaService.StopSigning(new() { DocumentOnSAId = documentOnSaToSign.DocumentOnSAId.Value, NotifyESignatures = true }, cancellationToken);
+                    await _signingHelperService.StopSinningAccordingState(new()
+                    {
+                        DocumentOnSAId = documentOnSaToSign.DocumentOnSAId!.Value,
+                        SignatureTypeId = documentOnSaToSign.SignatureTypeId,
+                        SalesArrangementId = documentOnSaToSign.SalesArrangementId
+                    }, cancellationToken);
+
                     // We have to actualise SA after stop Signing (because stop Signing may change SA state)
                     _salesArrangementService.ClearSalesArrangementCache();
                     saInstance = await _salesArrangementService.GetSalesArrangement(request.SalesArrangementId, cancellationToken);
                 }
             }
-            
+
             if (saInstance.State != (int)SalesArrangementStates.InProgress)
             {
                 await _salesArrangementService.UpdateSalesArrangementState(request.SalesArrangementId, (int)SalesArrangementStates.InProgress, cancellationToken);
@@ -124,16 +131,19 @@ internal sealed class UpdateParametersHandler
     private readonly ICodebookServiceClient _codebookService;
     private readonly ISalesArrangementServiceClient _salesArrangementService;
     private readonly IDocumentOnSAServiceClient _documentOnSaService;
+    private readonly ISigningHelperService _signingHelperService;
 
     public UpdateParametersHandler(
         UpdateParametersHelper helper,
         ICodebookServiceClient codebookService,
         ISalesArrangementServiceClient salesArrangementService,
-        IDocumentOnSAServiceClient documentOnSaService)
+        IDocumentOnSAServiceClient documentOnSaService,
+        ISigningHelperService signingHelperService)
     {
         _helper = helper;
         _codebookService = codebookService;
         _documentOnSaService = documentOnSaService;
+        _signingHelperService = signingHelperService;
         _salesArrangementService = salesArrangementService;
     }
 }
