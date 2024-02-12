d9c59bf35f21704a54a55e16ef29e75796cffc50
Polednak Miroslav <miroslav.polednak@mpss.cz>
2024-02-12 16:01:13 +0100

    UpdateTask part1

---
+++ DomainServices/CaseService/Api/Endpoints/UpdateTask/UpdateTaskHandler.cs
@@ -0,0 +1,12 @@
+﻿using DomainServices.CaseService.Contracts;
+using Google.Protobuf.WellKnownTypes;
+
+namespace DomainServices.CaseService.Api.Endpoints.UpdateTask;
+
+public class UpdateTaskHandler : IRequestHandler<UpdateTaskRequest, Empty>
+{
+    public Task<Empty> Handle(UpdateTaskRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DomainServices/CaseService/Api/Endpoints/UpdateTask/UpdateTaskValidator.cs
@@ -0,0 +1,18 @@
+﻿using DomainServices.CaseService.Contracts;
+using FluentValidation;
+
+namespace DomainServices.CaseService.Api.Endpoints.UpdateTask;
+
+public class UpdateTaskValidator : AbstractValidator<UpdateTaskRequest>
+{
+    public UpdateTaskValidator()
+    {
+        RuleFor(t => t.CaseId)
+            .GreaterThan(0)
+            .WithErrorCode(ErrorCodeMapper.CaseIdIsEmpty);
+
+        RuleFor(t => t.TaskIdSB)
+           .GreaterThan(0)
+           .WithErrorCode(ErrorCodeMapper.TaskIdSBIsEmpty);
+    }
+}
