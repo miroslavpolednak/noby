7ff3fb32527d39961178094500a49fb70af27497
filip-tuma <filip@filiptuma.com>
2024-02-17 17:46:23 +0100

    HFICH-10070

--- NOBY/NOBY.Api/Endpoints/Workflow/UpdateTaskDetail/UpdateTaskDetailHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Workflow/UpdateTaskDetail/UpdateTaskDetailHandler.cs
@@ -1,8 +1,8 @@
 ﻿using CIS.Core.Security;
-using SharedTypes.Enums;
 using DomainServices.CaseService.Clients;
 using DomainServices.CaseService.Contracts;
 using DomainServices.CodebookService.Clients;
+using DomainServices.SalesArrangementService.Clients;
 
 namespace NOBY.Api.Endpoints.Workflow.UpdateTaskDetail;
 
@@ -69,6 +69,10 @@ internal sealed class UpdateTaskDetailHandler : IRequestHandler<UpdateTaskDetail
             TaskUserResponse = request.TaskUserResponse,
             TaskId = request.TaskId
         };
+
+        completeTaskRequest.TaskDocumentIds.AddRange(taskDetail.TaskDetail.TaskDocumentIds.Concat(documentIds));
+
+        // podepisovani
         if (request.TaskTypeId == 6)
         {
             completeTaskRequest.CompletionTypeId = _currentUserAccessor.HasPermission(UserPermissions.WFL_TASK_DETAIL_SigningAttachments) ? 2 : 1;
@@ -79,9 +83,28 @@ internal sealed class UpdateTaskDetailHandler : IRequestHandler<UpdateTaskDetail
             }
         }
 
-        completeTaskRequest.TaskDocumentIds.AddRange(taskDetail.TaskDetail.TaskDocumentIds.Concat(documentIds));
+        // retence
+        if (request.TaskTypeId == 9)
+        {
+            if (!_currentUserAccessor.HasPermission(UserPermissions.WFL_TASK_DETAIL_RefinancingOtherManage))
+            {
+                throw new CisAuthorizationException("Missing WFL_TASK_DETAIL_RefinancingOtherManage permission");
+            }
+
+            completeTaskRequest.CompletionTypeId = 2;
+        }
 
         await _caseService.CompleteTask(completeTaskRequest, cancellationToken);
+
+        // retence
+        if (request.TaskTypeId == 9)
+        {
+            var saRequest = new DomainServices.SalesArrangementService.Contracts.UpdateSalesArrangementParametersRequest
+            {
+                SalesArrangementId = 1
+            };
+            await _salesArrangementService.UpdateSalesArrangementParameters(saRequest, cancellationToken);
+        }
 
         if (attachments?.Any() ?? false)
         {
@@ -89,24 +112,28 @@ internal sealed class UpdateTaskDetailHandler : IRequestHandler<UpdateTaskDetail
         }
     }
 
+    private readonly ISalesArrangementServiceClient _salesArrangementService;
     private readonly ICodebookServiceClient _codebookService;
     private readonly ICurrentUserAccessor _currentUserAccessor;
     private readonly ICaseServiceClient _caseService;
     private readonly SharedComponents.Storage.ITempStorage _tempFileManager;
     private readonly Services.UploadDocumentToArchive.IUploadDocumentToArchiveService _uploadDocumentToArchive;
-    private static int[] _allowedTaskTypeIds = { 1, 6 };
+    
+    private static int[] _allowedTaskTypeIds = [ 1, 6, 9 ];
 
     public UpdateTaskDetailHandler(
         ICodebookServiceClient codebookService,
         ICurrentUserAccessor currentUserAccessor,
         Services.UploadDocumentToArchive.IUploadDocumentToArchiveService uploadDocumentToArchive,
         ICaseServiceClient caseService,
-        SharedComponents.Storage.ITempStorage tempFileManager)
+        SharedComponents.Storage.ITempStorage tempFileManager,
+        ISalesArrangementServiceClient salesArrangementService)
     {
         _codebookService = codebookService;
         _currentUserAccessor = currentUserAccessor;
         _uploadDocumentToArchive = uploadDocumentToArchive;
         _caseService = caseService;
         _tempFileManager = tempFileManager;
+        _salesArrangementService = salesArrangementService;
     }
 }
