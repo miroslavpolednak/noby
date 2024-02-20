d46ffd90f2e002711b9f85bfa004b66ed2eceb06
filip-tuma <filip@filiptuma.com>
2024-02-20 22:29:52 +0100

    TaskScheduler update

--- InternalServices/TaskSchedulingService/Api/Jobs/Job1/Job1Handler.cs
+++ InternalServices/TaskSchedulingService/Api/Jobs/Job1/Job1Handler.cs
@@ -1,5 +1,4 @@
-﻿using CIS.Core.Attributes;
-using CIS.InternalServices.TaskSchedulingService.Api.Scheduling.Jobs;
+﻿using CIS.InternalServices.TaskSchedulingService.Api.Scheduling.Jobs;
 
 namespace CIS.InternalServices.TaskSchedulingService.Api.Jobs.Job1;
 
@@ -15,8 +14,8 @@ internal sealed class Job1Handler
 
     public async Task Execute(string? jobData, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("LOGGER START - {Data}", jobData);
+        _logger.LogInformation("LOGGER job1 START - {Data}", jobData);
         await Task.Delay(4000);
-        _logger.LogInformation("LOGGER END");
+        _logger.LogInformation("LOGGER job1 END");
     }
 }
