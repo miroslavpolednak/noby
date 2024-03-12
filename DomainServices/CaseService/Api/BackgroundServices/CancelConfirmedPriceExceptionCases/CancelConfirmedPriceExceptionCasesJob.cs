6d347c0e6e27e7802d5f6f5231ca76affee43614
filip-tuma <filip@filiptuma.com>
2024-03-12 13:38:39 +0100

    Docasene reseni bgjobu na caseservice

---
+++ DomainServices/CaseService/Api/BackgroundServices/CancelConfirmedPriceExceptionCases/CancelConfirmedPriceExceptionCasesJob.cs
@@ -0,0 +1,48 @@
+﻿using DomainServices.CaseService.Contracts;
+using Quartz;
+
+namespace DomainServices.CaseService.Api.BackgroundServices.CancelConfirmedPriceExceptionCases;
+
+internal sealed class CancelConfirmedPriceExceptionCasesJob
+    : CIS.Infrastructure.BackgroundServices.ICisBackgroundServiceJob
+{
+    public async Task ExecuteJobAsync(CancellationToken cancellationToken)
+    {
+        var d = _timeProvider.GetLocalNow().Date.AddDays(-45);
+
+        var list = await _dbContext
+            .ConfirmedPriceExceptions
+            .AsNoTracking()
+            .Where(t => t.ConfirmedDate < d)
+            .Select(t => t.CaseId)
+            .ToListAsync(cancellationToken);
+
+        foreach (long caseId in list)
+        {
+            try
+            {
+                await _mediator.Send(new CancelCaseRequest { CaseId = caseId }, cancellationToken);
+
+                await _dbContext
+                    .ConfirmedPriceExceptions
+                    .Where(t => t.CaseId == caseId)
+                    .ExecuteDeleteAsync(cancellationToken);
+            }
+            catch
+            {
+            }
+        }
+    }
+
+    private readonly TimeProvider _timeProvider;
+    private readonly Database.CaseServiceDbContext _dbContext;
+    private readonly IMediator _mediator;
+
+    public CancelConfirmedPriceExceptionCasesJob(TimeProvider timeProvider, Database.CaseServiceDbContext dbContext, IMediator mediator)
+    {
+        _timeProvider = timeProvider;
+        _dbContext = dbContext;
+        _mediator = mediator;
+    }
+}
+
