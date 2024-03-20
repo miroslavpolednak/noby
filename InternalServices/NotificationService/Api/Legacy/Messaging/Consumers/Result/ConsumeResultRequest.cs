2f5d30ef56d99d89b64610bf872b19fec1ab2042
filip-tuma <filip@filiptuma.com>
2024-03-20 11:08:26 +0100

    no message

--- InternalServices/NotificationService/Api/Legacy/Messaging/Consumers/Result/ConsumeResultRequest.cs
+++ InternalServices/NotificationService/Api/Legacy/Messaging/Consumers/Result/ConsumeResultRequest.cs
@@ -1,9 +1,6 @@
-﻿using cz.kb.osbs.mcs.notificationreport.eventapi.v3.report;
-using MediatR;
-
-namespace CIS.InternalServices.NotificationService.Api.Messaging.Consumers.Result;
+﻿namespace CIS.InternalServices.NotificationService.Api.Messaging.Consumers.Result;
 
 public class ConsumeResultRequest : IRequest<ConsumeResultResponse>
 {
-    public NotificationReport NotificationReport { get; set; } = null!;
+    public cz.kb.osbs.mcs.notificationreport.eventapi.v3.report.NotificationReport NotificationReport { get; set; } = null!;
 }
