b8fd9dc271ac1bc7e94112c669afcf835a5a1126
David Martínek <david.martinek@mpss.cz>
2024-04-02 14:53:03 +0200

    Revert "AddressSearch fix"
    This reverts commit 2d7d0e489bc193ade407845dee7ab0c423901655.

--- NOBY/NOBY.Api/Endpoints/Address/AddressSearch/AddressSearchHandler.cs
+++ NOBY/NOBY.Api/Endpoints/Address/AddressSearch/AddressSearchHandler.cs
@@ -1,7 +1,4 @@
-﻿using ExternalServices.RuianAddress.V1;
-using System.Globalization;
-
-namespace NOBY.Api.Endpoints.Address.AddressSearch;
+﻿namespace NOBY.Api.Endpoints.Address.AddressSearch;
 
 internal sealed class AddressSearchHandler
     : IRequestHandler<AddressSearchRequest, AddressSearchResponse>
@@ -9,53 +6,35 @@ internal sealed class AddressSearchHandler
     public async Task<AddressSearchResponse> Handle(AddressSearchRequest request, CancellationToken cancellationToken)
     {
         // zeme z ciselniku
-        var country = (await _codebookService.Countries(cancellationToken))
-            .FirstOrDefault(t => t.Id == request.CountryId);
-
-        var response = new AddressSearchResponse
-        {
-            PageSize = request.PageSize
-        };
-
+        string? country = null;
+        if (request.CountryId.GetValueOrDefault() > 0)
+            country = (await _codebookService.Countries(cancellationToken)).FirstOrDefault(t => t.Id == request.CountryId)?.ShortName;
         try
         {
-            if (country?.Id != 16)
-            {
-                var result = await _addressWhispererClient.GetSuggestions(request.SessionId, request.SearchText, request.PageSize, country?.ShortName, cancellationToken);
+            var result = await _addressWhispererClient.GetSuggestions(request.SessionId, request.SearchText, request.PageSize, country, cancellationToken);
 
-                response.Rows = result.Select(x => new Dto.AddressLine
+            return new AddressSearchResponse
+            {
+                PageSize = request.PageSize,
+                Rows = result.Select(x => new Dto.AddressLine
                 {
                     Id = x.AddressId,
                     Title = x.Title
-                }).ToList();
-            }
-            else
-            {
-                var result = await _ruianAddress.FindAddresses(request.SearchText, request.PageSize, cancellationToken);
-
-                response.Rows = result.Select(t => new Dto.AddressLine
-                {
-                    Id = t.Id.ToString(CultureInfo.InvariantCulture),
-                    Title = t.FullAddress ?? ""
-                }).ToList();
-            }
+                }).ToList()
+            };
         }
         catch
         {
             throw new NobyValidationException(90020);
         }
-
-        return response;
     }
 
-    private readonly IRuianAddressClient _ruianAddress;
-    private readonly ExternalServices.AddressWhisperer.V1.IAddressWhispererClient _addressWhispererClient;
-    private readonly DomainServices.CodebookService.Clients.ICodebookServiceClient _codebookService;
+    private ExternalServices.AddressWhisperer.V1.IAddressWhispererClient _addressWhispererClient;
+    private DomainServices.CodebookService.Clients.ICodebookServiceClient _codebookService;
 
-    public AddressSearchHandler(ExternalServices.AddressWhisperer.V1.IAddressWhispererClient addressWhispererClient, DomainServices.CodebookService.Clients.ICodebookServiceClient codebookService, IRuianAddressClient ruianAddress)
+    public AddressSearchHandler(ExternalServices.AddressWhisperer.V1.IAddressWhispererClient addressWhispererClient, DomainServices.CodebookService.Clients.ICodebookServiceClient codebookService)
     {
         _addressWhispererClient = addressWhispererClient;
         _codebookService = codebookService;
-        _ruianAddress = ruianAddress;
     }
 }
