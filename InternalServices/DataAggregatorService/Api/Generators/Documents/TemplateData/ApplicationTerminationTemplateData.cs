c37f66200f6fef93d443162378d067a28b19e371
David Martínek <david.martinek@mpss.cz>
2024-05-12 14:48:30 +0200

    HACH-10902

--- InternalServices/DataAggregatorService/Api/Generators/Documents/TemplateData/ApplicationTerminationTemplateData.cs
+++ InternalServices/DataAggregatorService/Api/Generators/Documents/TemplateData/ApplicationTerminationTemplateData.cs
@@ -1,9 +1,17 @@
 ﻿using CIS.InternalServices.DataAggregatorService.Api.Generators.Documents.TemplateData.Shared;
+using CIS.InternalServices.DataAggregatorService.Api.Services.DataServices.ServiceWrappers;
 
 namespace CIS.InternalServices.DataAggregatorService.Api.Generators.Documents.TemplateData;
 
 internal class ApplicationTerminationTemplateData : AggregatedData
 {
+    private readonly CaseServiceWrapper _caseServiceWrapper;
+
+    public ApplicationTerminationTemplateData(CaseServiceWrapper caseServiceWrapper)
+    {
+        _caseServiceWrapper = caseServiceWrapper;
+    }
+
     public string FullName => CustomerHelper.FullName(Customer.Source, _codebookManager.DegreesBefore);
 
     public string Street
@@ -28,12 +36,29 @@ internal class ApplicationTerminationTemplateData : AggregatedData
         }
     }
 
-    public string AnnouncementText => "dovolujeme si Vás informovat, že na základě Vašeho požadavku byla Vaše žádost o poskytnutí úvěru " +
-                                      $"č. {SalesArrangement.CaseId} ze dne {((DateTime)SalesArrangement.Created.DateTime).ToString("d", CultureProvider.GetProvider())} v našem systému ukončena.";
+    public string AnnouncementText
+    {
+        get
+        {
+            if (Custom.DocumentOnSa.DocumentsOnSa.Any(d => d.IsSigned))
+            {
+                
+                return $"dovolujeme si Vás informovat, že žádost o poskytnutí úvěru ve výši {((decimal)Case.Data.TargetAmount).ToString("C0", CultureProvider.GetProvider())} " +
+                       $"ze dne {((DateTime)SalesArrangement.Created.DateTime).ToString("d", CultureProvider.GetProvider())}, vedená pod registračním číslem {Case.Data.ContractNumber} byla ukončena na základě Vaší žádosti.";
+            }
+
+            return $"dovolujeme si Vás informovat, že na základě Vašeho požadavku bylo jednání o žádosti o poskytnutí úvěru ve výši {((decimal)Case.Data.TargetAmount).ToString("C0", CultureProvider.GetProvider())} ukončeno.";
+        }
+    }
 
     protected override void ConfigureCodebooks(ICodebookManagerConfigurator configurator)
     {
         configurator.DegreesBefore();
+    }
+
+    public override async Task LoadAdditionalData(CancellationToken cancellationToken)
+    {
+        await _caseServiceWrapper.LoadData(new InputParameters { SalesArrangementId = SalesArrangement.SalesArrangementId }, this, cancellationToken);
     }
 
     private GrpcAddress GetPermanentAddress() => Customer.Addresses.First(a => a.AddressTypeId == (int)AddressTypes.Permanent);
