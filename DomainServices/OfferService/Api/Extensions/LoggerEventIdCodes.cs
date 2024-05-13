aec91c7652b153a41d5428e123ac678e1bab2b0d
David Martínek <david.martinek@mpss.cz>
2024-05-13 23:29:06 +0200

    HACH-11505

--- DomainServices/OfferService/Api/Extensions/LoggerEventIdCodes.cs
+++ DomainServices/OfferService/Api/Extensions/LoggerEventIdCodes.cs
@@ -3,4 +3,6 @@
 public sealed class LoggerEventIdCodes
 {
     public const int BatchIdForProcessing = 10501;
+    public const int KafkaMessageCaseIdIncorrectFormat = 10502;
+    public const int KafkaMessageCurrentTaskIdIncorrectFormat = 10503;
 }
---
+++ DomainServices/OfferService/Api/LoggerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using DomainServices.OfferService.Api.Extensions;
+
+namespace DomainServices.OfferService.Api;
+
+public static class LoggerExtensions
+{
+    private static readonly Action<ILogger, string, string, Exception> _kafkaMessageCaseIdIncorrectFormat;
+    private static readonly Action<ILogger, string, string, Exception> _kafkaMessageCurrentTaskIdIncorrectFormat;
+
+    static LoggerExtensions()
+    {
+        _kafkaMessageCaseIdIncorrectFormat = LoggerMessage.Define<string, string>(
+            LogLevel.Error,
+            new EventId(LoggerEventIdCodes.KafkaMessageCaseIdIncorrectFormat, nameof(KafkaMessageCaseIdIncorrectFormat)),
+            "Kafka message processing in {Consumer}: CaseId {CaseId} is not in valid format");
+
+        _kafkaMessageCurrentTaskIdIncorrectFormat = LoggerMessage.Define<string, string>(
+            LogLevel.Error,
+            new EventId(LoggerEventIdCodes.KafkaMessageCurrentTaskIdIncorrectFormat, nameof(KafkaMessageCurrentTaskIdIncorrectFormat)),
+            "Kafka message processing in {Consumer}: CurrentTaskId {CurrentTaskId} is not in valid format");
+    }
+
+    public static void KafkaMessageCaseIdIncorrectFormat(this ILogger logger, string consumerTypeName, string caseId)
+        => _kafkaMessageCaseIdIncorrectFormat(logger, consumerTypeName, caseId, null!);
+
+    public static void KafkaMessageCurrentTaskIdIncorrectFormat(this ILogger logger, string consumerTypeName, string currentTaskId)
+        => _kafkaMessageCurrentTaskIdIncorrectFormat(logger, consumerTypeName, currentTaskId, null!);
+}
