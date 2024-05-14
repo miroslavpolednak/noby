c62629195f55eb32fcbd17f29d910aaf2f60404a
Polednak Miroslav <miroslav.polednak@mpss.cz>
2024-05-14 08:48:33 +0200

    kafka doc change

---
+++ NOBY/NOBY.Api/Messaging/DocumentOnSA/V1/DocumentStateChangedHandler.cs
@@ -0,0 +1,21 @@
+﻿using cz.mpss.api.epodpisy.digitalsigning.documentsigningevents.v1;
+using DomainServices.DocumentOnSAService.Clients;
+using KafkaFlow;
+
+namespace NOBY.Api.Messaging.DocumentOnSA.V1;
+
+public class DocumentStateChangedHandler(ILogger<DocumentStateChangedHandler> logger, IDocumentOnSAServiceClient documentOnSAService) : IMessageHandler<DocumentStateChanged>
+{
+    private readonly ILogger<DocumentStateChangedHandler> _logger = logger;
+    private readonly IDocumentOnSAServiceClient _documentOnSAService = documentOnSAService;
+
+    public async Task Handle(IMessageContext context, DocumentStateChanged message)
+    {
+        if (string.IsNullOrWhiteSpace(message.documentExternalId))
+        {
+            //ToDo log
+        }
+
+
+    }
+}
