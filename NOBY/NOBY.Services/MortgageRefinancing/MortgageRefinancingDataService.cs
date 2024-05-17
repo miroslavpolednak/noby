5fa49129ea74aa00683066de38d3c9d977dda8ea
David Martínek <david.martinek@mpss.cz>
2024-05-17 15:45:08 +0200

    HACH-11647 Refinancing - activePriceException filter changed

--- NOBY/NOBY.Services/MortgageRefinancing/MortgageRefinancingDataService.cs
+++ NOBY/NOBY.Services/MortgageRefinancing/MortgageRefinancingDataService.cs
@@ -125,7 +125,7 @@ public sealed class MortgageRefinancingDataService(
     {
         // toto je aktivni IC task!
         var activePriceExceptionTaskIdSb = tasks
-            .FirstOrDefault(t => t.TaskTypeId == (int)WorkflowTaskTypes.PriceException && !t.Cancelled && (t.PhaseTypeId == 1 || (t.DecisionId != 2 && t.PhaseTypeId == 2)))
+            .FirstOrDefault(t => t.TaskTypeId == (int)WorkflowTaskTypes.PriceException && !t.Cancelled && t.PhaseTypeId == 1)
             ?.TaskIdSb;
 
         // detail IC tasku
