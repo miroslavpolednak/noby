443cd96b5b18f2eab0713d7205c84ef40c8d38fe
David Martínek <david.martinek@mpss.cz>
2024-05-17 16:04:10 +0200

    Fixed SimulateMortgageExtraPaymentRequestValidator

--- NOBY/NOBY.Api/Endpoints/Offer/SimulateMortgageExtraPayment/SimulateMortgageExtraPaymentRequestValidator.cs
+++ NOBY/NOBY.Api/Endpoints/Offer/SimulateMortgageExtraPayment/SimulateMortgageExtraPaymentRequestValidator.cs
@@ -9,15 +9,14 @@ internal sealed class SimulateMortgageExtraPaymentRequestValidator
 {
     public SimulateMortgageExtraPaymentRequestValidator(ICodebookServiceClient codebookService, IFeatureManager featureManager)
     {
-        RuleFor(t => t.ExtraPaymentAmount)
-            .GreaterThan(0);
+        When(t => t.ExtraPaymentAmount is not null, () => RuleFor(t => t.ExtraPaymentAmount).GreaterThan(0));
 
         RuleFor(t => t.ExtraPaymentReasonId)
             .MustAsync(async (id, cancellationToken) => (await codebookService.ExtraPaymentReasons(cancellationToken)).Any(t => t.Id == id));
 
         RuleFor(t => t.ExtraPaymentDate)
             .MustAsync(async (d, cancellationToken) => (await codebookService.GetNonBankingDays(d, d, cancellationToken)).Count == 0)
-            .GreaterThan(DateOnly.FromDateTime(DateTime.Now.Date));
+            .GreaterThanOrEqualTo(DateOnly.FromDateTime(DateTime.Now.Date));
 
         RuleFor(t => t)
             .MustAsync(async (_, _) => await featureManager.IsEnabledAsync(SharedTypes.FeatureFlagsConstants.ExtraPayment))
