16a7707a3752f2b6a527220cd7ea71e9ff50d36b
filip-tuma <filip@filiptuma.com>
2024-06-05 12:16:50 +0200

    Response code validation

--- DomainServices/OfferService/Api/Endpoints/v1/CreateResponseCode/CreateResponseCodeRequestValidator.cs
+++ DomainServices/OfferService/Api/Endpoints/v1/CreateResponseCode/CreateResponseCodeRequestValidator.cs
@@ -1,4 +1,5 @@
-﻿using DomainServices.OfferService.Contracts;
+﻿using DomainServices.CodebookService.Clients;
+using DomainServices.OfferService.Contracts;
 using FluentValidation;
 
 namespace DomainServices.OfferService.Api.Endpoints.v1.CreateResponseCode;
@@ -6,10 +7,14 @@ namespace DomainServices.OfferService.Api.Endpoints.v1.CreateResponseCode;
 internal sealed class CreateResponseCodeRequestValidator
     : AbstractValidator<CreateResponseCodeRequest>
 {
-    public CreateResponseCodeRequestValidator()
+    public CreateResponseCodeRequestValidator(ICodebookServiceClient codebookService)
     {
         RuleFor(t => t.CaseId)
             .GreaterThan(0)
             .WithErrorCode(ErrorCodeMapper.CaseIdIsEmpty);
+
+        RuleFor(t => t.ResponseCodeTypeId)
+            .MustAsync(async (id, c) => (await codebookService.ResponseCodeTypes(c)).Any(t => t.Id == id))
+            .WithErrorCode(ErrorCodeMapper.ResponseCodeTypeIdNotFound);
     }
 }
