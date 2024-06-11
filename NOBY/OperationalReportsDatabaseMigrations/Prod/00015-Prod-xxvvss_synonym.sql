f64029a86eb3ed2850e3e199c70229222ed2ce5c
David Martínek <david.martinek@mpss.cz>
2024-06-11 19:30:51 +0200

    Reporting - fixed 00015 script

--- NOBY/OperationalReportsDatabaseMigrations/Prod/00015-Prod-xxvvss_synonym.sql
+++ NOBY/OperationalReportsDatabaseMigrations/Prod/00015-Prod-xxvvss_synonym.sql
@@ -1,4 +1,4 @@
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[[xxvvss_v33PMP_User_S]]') AND type in (N'SN'))
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[xxvvss_v33PMP_User_S]') AND type in (N'SN'))
 	DROP SYNONYM [dbo].[xxvvss_v33PMP_User_S]
 
 CREATE SYNONYM [dbo].[xxvvss_v33PMP_User_S] FOR [BABETA].[xxvvss].[dbo].[v33PMP_User]
