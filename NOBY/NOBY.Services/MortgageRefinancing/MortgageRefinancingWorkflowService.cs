6e8694e9a54cf84d6fad1b09802787a8e865041f
David Martínek <david.martinek@mpss.cz>
2024-06-18 15:31:22 +0200

    HACH-12108

--- NOBY/NOBY.Services/MortgageRefinancing/MortgageRefinancingWorkflowService.cs
+++ NOBY/NOBY.Services/MortgageRefinancing/MortgageRefinancingWorkflowService.cs
@@ -32,10 +32,7 @@ public sealed class MortgageRefinancingWorkflowService(
 
         var priceExceptionTasks = GetPriceExceptionTasks(taskList, processId).ToList();
 
-        if (priceExceptionTasks.Count == 0)
-            throw new NobyValidationException(90032, "Empty collection");
-
-        var priceExceptionTask = priceExceptionTasks.SingleOrDefault() ?? throw new NobyValidationException(90050);
+        var priceExceptionTask = priceExceptionTasks.Count == 1 ? priceExceptionTasks.Single() : throw new NobyValidationException(90050);
 
         if (priceExceptionTask.StateIdSb != 30 && priceExceptionTask.PhaseTypeId != 2)
             throw new NobyValidationException(90049);
