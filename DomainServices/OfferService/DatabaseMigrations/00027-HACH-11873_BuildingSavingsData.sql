bb0f18147afb8d3c452e557ec91bd935a9c2bf58
David Martínek <david.martinek@mpss.cz>
2024-07-21 19:58:34 +0200

    HACH-11873 BuildingSavingsData migration

---
+++ DomainServices/OfferService/DatabaseMigrations/00027-HACH-11873_BuildingSavingsData.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS [DDS].[BuildingSavingsData]
+GO
+CREATE TABLE [DDS].[BuildingSavingsData](
+	[DocumentDataStorageId] [int] IDENTITY(1,1) NOT NULL,
+	[DocumentDataEntityId] [varchar](50) NULL,
+	[DocumentDataVersion] [int] NOT NULL,
+	[Data] [nvarchar](max) NULL,
+	[CreatedUserId] [int] NOT NULL,
+	[CreatedTime] [datetime] NOT NULL,
+	[ModifiedUserId] [int] NULL
+ CONSTRAINT [PK_BuildingSavingsData] PRIMARY KEY CLUSTERED 
+(
+	[DocumentDataStorageId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+) ON [PRIMARY]
+GO
+
+DROP INDEX IF EXISTS Idx_EntityId ON [DDS].[BuildingSavingsData];
+CREATE NONCLUSTERED INDEX [Idx_EntityId] ON [DDS].[BuildingSavingsData]
+(
+	[DocumentDataEntityId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
