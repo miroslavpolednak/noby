8703e136011bb0eb707f078190293addf81400d0
David Martínek <david.martinek@mpss.cz>
2024-07-21 18:52:51 +0200

    HACH-11881 FE API validation

--- NOBY/NOBY.Api/Endpoints/Offer/SimulateBuildingSavings/SimulateBuildingSavingsRequestValidator.cs
+++ NOBY/NOBY.Api/Endpoints/Offer/SimulateBuildingSavings/SimulateBuildingSavingsRequestValidator.cs
@@ -1,15 +1,24 @@
-﻿using FluentValidation;
+﻿using DomainServices.CodebookService.Clients;
+using FluentValidation;
 using Microsoft.FeatureManagement;
 
 namespace NOBY.Api.Endpoints.Offer.SimulateBuildingSavings;
 
 internal sealed class SimulateBuildingSavingsRequestValidator : AbstractValidator<OfferSimulateBuildingSavingsRequest>
 {
-    public SimulateBuildingSavingsRequestValidator(IFeatureManager featureManager)
+    public SimulateBuildingSavingsRequestValidator(IFeatureManager featureManager, ICodebookServiceClient codebookService)
     {
         RuleFor(t => t)
             .MustAsync(async (_, _) => await featureManager.IsEnabledAsync(SharedTypes.FeatureFlagsConstants.BlueBang))
             .WithErrorCode(90067);
+
+        RuleFor(t => t).MustAsync(async (request, cancellationToken) =>
+        {
+            var buildingSavingsProperties = await codebookService.BuildingSavingsProperties(cancellationToken);
+
+            return buildingSavingsProperties.All(p => request.TargetAmount >= p.MinimumTargetAmount &&
+                                                      request.MinimumMonthlyDeposit >= request.TargetAmount / 100 * p.MinimumMonthlyDeposit);
+        }).WithErrorCode(90032);
 
         RuleFor(t => t.ContractStartDate).LessThan(DateOnly.FromDateTime(DateTime.Now)).WithErrorCode(90032);
         RuleFor(t => t).Must(t => !t.IsClientJuridicalPerson || !t.StateSubsidyRequired).WithErrorCode(90032);
