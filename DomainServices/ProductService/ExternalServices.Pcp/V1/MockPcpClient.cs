0601a08aa3432f79402e62e3d2bedc9d7d2ffde9
David Martínek <david.martinek@mpss.cz>
2024-07-30 13:08:11 +0200

    HACH-12332

--- DomainServices/ProductService/ExternalServices.Pcp/V1/MockPcpClient.cs
+++ DomainServices/ProductService/ExternalServices.Pcp/V1/MockPcpClient.cs
@@ -3,6 +3,11 @@
 internal sealed class MockPcpClient
     : IPcpClient
 {
+    public Task<ProductInstance3[]> GetByOtherIds(long caseId, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(Array.Empty<ProductInstance3>());
+    }
+
     public Task<string> CreateProduct(long caseId, long customerKbId, string pcpProductId, CancellationToken cancellationToken = default)
     {
         return Task.FromResult("nejake id");
