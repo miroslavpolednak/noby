5ecafc1cf7af9ecc32ece1730ba05acfdd1410d2
filip-tuma <filip@filiptuma.com>
2024-08-06 09:54:21 +0200

    HACH-12659

---
+++ DomainServices/CodebookService/DatabaseMigrations/00100-HACH-12659.sql
@@ -0,0 +1,20 @@
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (1, 1, 3, 1);
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (1, 1, 5, 1);
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (7, 1, 1, 1);
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (7, 1, 2, 1);
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (7, 1, 3, 1);
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (7, 1, 5, 1);
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (7, 1, 8, 1);
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (7, 2, 1, 1);
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (7, 2, 2, 1);
+insert into [dbo].[WorkflowConsultationMatrix] (TaskSubtypeId, ProcessTypeId, ProcessPhaseId, IsConsultation)
+	values (7, 2, 3, 1);
