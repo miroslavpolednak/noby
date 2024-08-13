d8b8f579b2fd1fc9cf0f170b4525b79c5e6a97d8
filip-tuma <filip@filiptuma.com>
2024-08-13 11:03:44 +0200

    code cleanup

--- CIS/Infrastructure.Messaging/LoggerExtensions.cs
+++ CIS/Infrastructure.Messaging/LoggerExtensions.cs
@@ -4,31 +4,28 @@ namespace CIS.Infrastructure.Messaging;
 
 internal static class LoggerExtensions
 {
-    private static readonly Action<ILogger, string, string, Exception> _consumingMessageFailed;
-    private static readonly Action<ILogger, Exception> _schemaRegistryError;
-    private static readonly Action<ILogger, string, string, string, Exception> _consumingKnownMessage;
-
-    static LoggerExtensions()
-    {
-        _consumingMessageFailed = LoggerMessage.Define<string, string>(
+    private static readonly Action<ILogger, string, string, Exception> _consumingMessageFailed =
+        LoggerMessage.Define<string, string>(
             LogLevel.Error,
             new EventId(901, nameof(ConsumingMessageFailed)),
             "Failed to consume message with ID '{MessageId}' from {Topic}"
         );
 
-        _schemaRegistryError = LoggerMessage.Define(
+    private static readonly Action<ILogger, Exception> _schemaRegistryError =
+        LoggerMessage.Define(
             LogLevel.Error,
             new EventId(902, nameof(SchemaRegistryError)),
             "Schema registry error"
         );
 
-        _consumingKnownMessage = LoggerMessage.Define<string, string, string>(
+    private static readonly Action<ILogger, string, string, string, Exception> _consumingKnownMessage =
+        LoggerMessage.Define<string, string, string>(
             LogLevel.Information,
             new EventId(903, nameof(ConsumingKnownMessage)),
             "Consuming message type {MessageType} with ID '{MessageId}' from {Topic}"
         );
-    }
 
+    // public accessor
     public static void ConsumingMessageFailed(this ILogger logger, string messageId, string topic, Exception ex) => 
         _consumingMessageFailed(logger, messageId, topic, ex);
 
