64bad739c5a050272f615da2ca6b16cfc3136bc6
filip-tuma <filip@filiptuma.com>
2024-08-22 14:47:29 +0200

    Caseservice Typo fix

--- DomainServices/CaseService/Api/Endpoints/v1/ValidateCaseId/ValidateCaseIdHandler.cs
+++ DomainServices/CaseService/Api/Endpoints/v1/ValidateCaseId/ValidateCaseIdHandler.cs
@@ -1,27 +1,13 @@
 ﻿using DomainServices.CaseService.Api.Database;
 using DomainServices.CaseService.Contracts;
-using Microsoft.Extensions.Caching.Distributed;
-using CIS.Infrastructure.Caching;
 
 namespace DomainServices.CaseService.Api.Endpoints.v1.ValidateCaseId;
 
-internal sealed class ValidateCaseIdHandler(CaseServiceDbContext _dbContext, IDistributedCache _distributedCache)
+internal sealed class ValidateCaseIdHandler(CaseServiceDbContext _dbContext)
         : IRequestHandler<ValidateCaseIdRequest, ValidateCaseIdResponse>
 {
     public async Task<ValidateCaseIdResponse> Handle(ValidateCaseIdRequest request, CancellationToken cancellationToken)
     {
-        await _distributedCache.SetObjectAsync("CaseService:CaseStateChanged_1", new SharedDto.CaseStateChangeRequestId
-            {
-                RequestId = 1,
-                CaseId = 1,
-                CreatedTime = DateTime.Now
-            }, new DistributedCacheEntryOptions
-            {
-                AbsoluteExpiration = DateTime.Now.AddHours(1),
-            },
-           CIS.Core.Configuration.ICisDistributedCacheConfiguration.SerializationTypes.Protobuf,
-           cancellationToken);
-
         var instance = await _dbContext.Cases
             .Where(t => t.CaseId == request.CaseId)
             .Select(t => new { t.State, t.OwnerUserId })
