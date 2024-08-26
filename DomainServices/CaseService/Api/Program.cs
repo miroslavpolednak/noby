a4a742272282c0679d001330fb52a3bd442e7589
David Martínek <david.martinek@mpss.cz>
2024-08-26 23:33:31 +0200

    HACH-12757

--- DomainServices/CaseService/Api/Program.cs
+++ DomainServices/CaseService/Api/Program.cs
@@ -5,73 +5,68 @@ using Ext1 = ExternalServices;
 using CIS.Infrastructure.Messaging;
 using DomainServices.CaseService.Api.Messaging.MessageHandlers;
 using DomainServices.CaseService.Api.Endpoints.v1;
+using DomainServices.CaseService.Api.Messaging.MortgageServicingBatch;
 
 SharedComponents.GrpcServiceBuilder
-    .CreateGrpcService(args, typeof(Program))
-    .AddApplicationConfiguration<DomainServices.CaseService.Api.Configuration.AppConfiguration>()
-    .AddRollbackCapability()
-    .AddDistributedCache()
-    .AddErrorCodeMapper(DomainServices.CaseService.Api.ErrorCodeMapper.Init())
-    .AddRequiredServices(services =>
-    {
-        services
-            .AddRiskIntegrationService()
-            .AddSalesArrangementService()
-            .AddDocumentOnSAService()
-            .AddCodebookService()
-            .AddHouseholdService()
-            .AddProductService()
-            .AddUserService()
-            .AddDocumentOnSAService();
-    })
-    .Build((builder, appConfiguration) =>
-    {
-        appConfiguration.Validate();
-
-        // EAS svc
-        builder.AddExternalService<Ext1.Eas.V1.IEasClient>();
-
-        // SB webapi svc
-        builder.AddExternalService<Ext1.SbWebApi.V1.ISbWebApiClient>();
-
-        // dbcontext
-        builder.AddEntityFramework<CaseServiceDbContext>();
-
-        // kafka messaging
-        builder.AddCisMessaging()
-                .AddKafkaFlow(msg =>
+                .CreateGrpcService(args, typeof(Program))
+                .AddApplicationConfiguration<DomainServices.CaseService.Api.Configuration.AppConfiguration>()
+                .AddRollbackCapability()
+                .AddDistributedCache()
+                .AddErrorCodeMapper(DomainServices.CaseService.Api.ErrorCodeMapper.Init())
+                .AddRequiredServices(services =>
                 {
-                    msg.AddConsumerAvro(
-                        appConfiguration.SbWorkflowProcessTopic!,
-                        handlers =>
-                        {
-                            handlers.AddHandler<CollateralValuationProcessChangedHandler>()
-                                    .AddHandler<ConsultationRequestProcessChangedHandler>()
-                                    .AddHandler<IndividualPricingProcessChangedHandler>()
-                                    .AddHandler<InformationRequestProcessChangedHandler>()
-                                    .AddHandler<MainLoanProcessChangedHandler>()
-                                    .AddHandler<WithdrawalProcessChangedHandler>()
-                                    .AddHandler<LoanExtraPaymentProcessChangedHandler>()
-                                    .AddHandler<LoanRetentionProcessChangedHandler>();
-                        });
+                    services
+                        .AddRiskIntegrationService()
+                        .AddSalesArrangementService()
+                        .AddDocumentOnSAService()
+                        .AddCodebookService()
+                        .AddHouseholdService()
+                        .AddProductService()
+                        .AddUserService()
+                        .AddDocumentOnSAService();
+                })
+                .Build((builder, appConfiguration) =>
+                {
+                    appConfiguration.Validate();
 
-                    msg.AddConsumerAvro<CaseStateChangedProcessingCompletedHandler>(appConfiguration.SbWorkflowInputProcessingTopic!);
+                    // EAS svc
+                    builder.AddExternalService<Ext1.Eas.V1.IEasClient>();
 
-                    msg.AddConsumerAvro(
-                        appConfiguration.MortgageServicingMortgageChangesTopic!,
-                        handlers =>
-                        {
-                            handlers.AddHandler<MortgageInstanceChangedHandler>()
-                                    .AddHandler<MortgageApplicationChangedHandler>();
-                        });
-                });
-    })
-    .MapGrpcServices((app, _) =>
-    {
-        app.MapGrpcService<CaseService>();
-        app.MapGrpcService<DomainServices.CaseService.Api.Endpoints.Maintanance.MaintananceService>();
-    })
-    .Run();
+                    // SB webapi svc
+                    builder.AddExternalService<Ext1.SbWebApi.V1.ISbWebApiClient>();
+
+                    // dbcontext
+                    builder.AddEntityFramework<CaseServiceDbContext>();
+
+                    // kafka messaging
+                    builder.AddCisMessaging()
+                           .AddKafkaFlow(msg =>
+                           {
+                               msg.AddConsumerAvro(
+                                   appConfiguration.SbWorkflowProcessTopic!,
+                                   handlers =>
+                                   {
+                                       handlers.AddHandler<CollateralValuationProcessChangedHandler>()
+                                               .AddHandler<ConsultationRequestProcessChangedHandler>()
+                                               .AddHandler<IndividualPricingProcessChangedHandler>()
+                                               .AddHandler<InformationRequestProcessChangedHandler>()
+                                               .AddHandler<MainLoanProcessChangedHandler>()
+                                               .AddHandler<WithdrawalProcessChangedHandler>()
+                                               .AddHandler<LoanExtraPaymentProcessChangedHandler>()
+                                               .AddHandler<LoanRetentionProcessChangedHandler>();
+                                   });
+
+                               msg.AddConsumerAvro<CaseStateChangedProcessingCompletedHandler>(appConfiguration.SbWorkflowInputProcessingTopic!);
+
+                               msg.AddBatchConsumerAvro<MortgageServicingBatchHandler>(appConfiguration.MortgageServicingMortgageChangesTopic!);
+                           });
+                })
+                .MapGrpcServices((app, _) =>
+                {
+                    app.MapGrpcService<CaseService>();
+                    app.MapGrpcService<DomainServices.CaseService.Api.Endpoints.Maintanance.MaintananceService>();
+                })
+                .Run();
 
 public partial class Program
 {
