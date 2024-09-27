3c9395fc1f801787763239c66a8510614e723e9b
filip-tuma <filip@filiptuma.com>
2024-09-27 10:32:54 +0200

    File log delimiter fix

--- CIS/Infrastructure.Telemetry/Constants.cs
+++ CIS/Infrastructure.Telemetry/Constants.cs
@@ -6,5 +6,5 @@ internal sealed class Constants
 
     public const string LoggerContextUserIdentPropertyName = "CisUserIdent";
 
-    public const string FileLoggerTemplate = @"{Timestamp:yyyy-MM-dd HH:mm:ss,fff zzz}␟{ThreadId}␟{Level:u}␟{TraceId}␟{SpanId}␟{ParentId}␟{CisAppKey}␟{Version}␟{Assembly}␟{SourceContext}␟{MachineName}␟{ClientIp}␟{CisUserId}␟{CisUserIdent}␟{RequestId}␟{RequestPath}␟{ConnectionId}␟{Message}␟{Exception}{Payload}]{NewLine}";
+    public const string FileLoggerTemplate = @"{Timestamp:yyyy-MM-dd HH:mm:ss,fff zzz}␟{ThreadId}␟{Level:u}␟{TraceId}␟{SpanId}␟{ParentId}␟{CisAppKey}␟{Version}␟{Assembly}␟{SourceContext}␟{MachineName}␟{ClientIp}␟{CisUserId}␟{CisUserIdent}␟{RequestId}␟{RequestPath}␟{ConnectionId}␟{Message}␟{Exception}{Payload}{NewLine}";
 }
